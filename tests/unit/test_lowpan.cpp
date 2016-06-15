a0b3e22f437b0b40ca884893e6283cf394e919ed
Martin Turon <mturon@nestlabs.com>
2016-06-14 20:43:56 -0700

    Added multicast test vectors to 6lo decompression unit tests.

--- tests/unit/test_lowpan.cpp
+++ tests/unit/test_lowpan.cpp
@@ -57,6 +57,7 @@ typedef const struct
 
 typedef const struct
 {
+    const char *mTest;
     const char *mCompressed;
     const char *mRaw;
 
@@ -73,7 +74,8 @@ typedef const struct
 
 test_lowpan_vector_t tests[] = {
   {
-    // l1_t1_AF_pass.pcap - ping request
+    // l1_t1_AF_pass.pcap
+    .mTest = "LL64 unicast ICMP ping request",
     .mCompressed = 
         "61 cc 1d ce fa 03 00 00  00 00 0a 6e 14 01 00 00 "
         "00 00 0a 6e 14 7a 33 3a  80 00 30 be 00 00 00 00 "
@@ -94,7 +96,8 @@ test_lowpan_vector_t tests[] = {
     .mHops = 64
   },
   {
-    // l1_t1_AF_pass.pcap - ping reply
+    // l1_t1_AF_pass.pcap
+    .mTest = "LL64 unicast ICMP ping reply",
     .mCompressed = 
         "61 cc 07 ce fa 01 00 00  00 00 0a 6e 14 03 00 00 "
         "00 00 0a 6e 14 7a 33 3a  81 00 2f be 00 00 00 00 "
@@ -112,7 +115,8 @@ test_lowpan_vector_t tests[] = {
     },
   },
   {
-    // l1_t2_AF_AS_pass.pcap - ping request
+    // l1_t2_AF_AS_pass.pcap
+    .mTest = "LL16 unicast ICMP ping request",
     .mCompressed =
         "61 88 13 ce fa 00 10 00  00 7a 33 3a 80 00 63 9e "
         "00 00 00 00 41 42 43 44  45 46 47 48 84 5f",
@@ -123,7 +127,8 @@ test_lowpan_vector_t tests[] = {
         "41 42 43 44 45 46 47 48"
   },
   {
-    // l1_t2_AF_AS_pass.pcap - ping reply
+    // l1_t2_AF_AS_pass.pcap
+    .mTest = "LL16 unicast ICMP ping reply",
     .mCompressed =
         "61 88 0f ce fa 00 00 00 10 7a 33 3a 81 00 62 9e "
         "00 00 00 00 41 42 43 44 45 46 47 48 e0 35",
@@ -133,6 +138,63 @@ test_lowpan_vector_t tests[] = {
         "00 00 00 ff fe 00 00 00  81 00 62 9e 00 00 00 00 "
         "41 42 43 44 45 46 47 48"
   },
+  {
+    // l1_t3_SF_pass.pcap
+    .mTest = "LL64 multicast FF02::1 ICMP ping request",
+    .mCompressed =
+       "41 c8 99 ce fa ff ff 01 00 00 00 00 0a 6e 14 7a "
+       "3b 3a 01 80 00 54 b4 40 41 42 43 44 45 46 47 68 "
+       "44",
+    .mRaw =
+        "60 00 00 00 00 0c 3a 40  fe 80 00 00 00 00 00 00 "
+        "16 6e 0a 00 00 00 00 01  ff 02 00 00 00 00 00 00 "
+        "00 00 00 00 00 00 00 01  80 00 54 b4 40 41 42 43 "
+        "44 45 46 47 "
+  },
+  {
+    // l1_t3_SF_pass.pcap
+    .mTest = "LL64 multicast FF02::1 ICMP ping reply",
+    .mCompressed =
+       "61 cc fc ce fa 01 00 00 00 00 0a 6e 14 02 00 00 "
+       "00 00 0a 6e 14 7a 33 3a 81 00 33 c7 40 41 42 43 "
+       "44 45 46 47 1a 80",
+    .mRaw =
+        "60 00 00 00 00 0c 3a 40  fe 80 00 00 00 00 00 00 "
+        "16 6e 0a 00 00 00 00 02  fe 80 00 00 00 00 00 00 "
+        "16 6e 0a 00 00 00 00 01  81 00 33 c7 40 41 42 43 "
+        "44 45 46 47"
+  },
+  {
+    // l1_t4_FS_pass.pcap
+    .mTest = "LL16 multicast FF02::1 ICMP ping request",
+    .mCompressed =
+       "41 88 df ce fa ff ff 00   08 7a 3b 3a 01 80 00 76 "
+       "0e 00 01 00 04 50 50 50   50 50 50 50 50 50 50 50 "
+       "50 50 50 50 50 50 50 50   50 50 50 50 50 50 50 50 "
+       "50 50 50 50 50 a7 d2",
+    .mRaw =
+        "60 00 00 00 00 28 3a 40  fe 80 00 00 00 00 00 00 "
+        "00 00 00 ff fe 00 08 00  ff 02 00 00 00 00 00 00 "
+        "00 00 00 00 00 00 00 01  80 00 76 0e 00 01 00 04 "
+        "50 50 50 50 50 50 50 50  50 50 50 50 50 50 50 50 "
+        "50 50 50 50 50 50 50 50  50 50 50 50 50 50 50 50 "
+  },
+  {
+    // l1_t4_FS_pass.pcap
+    .mTest = "LL16 multicast FF02::1 ICMP ping reply",
+    .mCompressed =
+       "61 c8 41 ce fa 00 08 03 00 00 00 00 0a 6e 14 7a "
+       "33 3a 81 00 55 20 00 01 00 04 50 50 50 50 50 50 "
+       "50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 50 "
+       "50 50 50 50 50 50 50 50 50 50 ab 56",
+    .mRaw =
+        "60 00 00 00 00 28 3a 40  fe 80 00 00 00 00 00 00 "
+        "16 6e 0a 00 00 00 00 03  fe 80 00 00 00 00 00 00 "
+        "00 00 00 ff fe 00 08 00  81 00 55 20 00 01 00 04 "
+        "50 50 50 50 50 50 50 50  50 50 50 50 50 50 50 50 "
+        "50 50 50 50 50 50 50 50  50 50 50 50 50 50 50 50 "
+  },
+
 };
 
 
@@ -157,10 +219,11 @@ void TestLowpanIphc(void)
         std::vector<uint8_t> iphcVector;       
 	otTestHexToVector(iphcString, iphcVector);
 
-	printf("iphcString:\n");
+	printf("=== Packet #%d: %s ===\n", i, tests[i].mTest);
+	printf("6lo Packet:\n");
 	otTestPrintHex(iphcVector);
 
-	printf("ipString:\n");
+	printf("Decompressed Reference:\n");
 	otTestPrintHex(ipVector);
 
 	// Test decompression
@@ -192,13 +255,15 @@ void TestLowpanIphc(void)
 	resultLength = message->GetLength();
 	message->Read(0, resultLength, result);
 
-	printf("Decompressed [%d]:\n", i);
+	printf("Decompressed OpenThread:\n");
 	otTestPrintHex(result, resultLength);
 
 	SuccessOrQuit(memcmp(ipVector.data(), result, resultLength),
 		      "6lo: Lowpan::Decompress failed");
 	
 	SuccessOrQuit(Message::Free(*message), "6lo: Message:Free failed");
+
+	printf("PASS\n\n");
     }
 }
 
