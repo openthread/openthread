e8ed43a9efa4b75c90fb332a58225ce5deb6bcce
Martin Turon <mturon@nestlabs.com>
2016-06-30 22:58:31 -0700

    IAR builds working NCP firmware now for cc2538. Performed basic verification with wpantund: connect over uart, status, attach (start).

---
+++ examples/platforms/cc2538/proj/iar/openthread-config.h
@@ -0,0 +1,8 @@
+/* include/openthread-config.h.  Generated from openthread-config.h.in by configure.  */
+/* include/openthread-config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "OPENTHREAD"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "g4779fb2-iar"
