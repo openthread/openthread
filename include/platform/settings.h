d52367fc398bf6c556d70859738e8fcff7041fc3
Robert Quattlebaum <rquattlebaum@nestlabs.com>
2016-08-01 11:29:11 -0700

    otPlatform: New non-volatile settings API
    This is the new API for the storage and retrieval of non-volatile
    settings. Platforms would implement this API in order to allow various
    types of information to be preserved across resets or power loss.

---
+++ include/platform/settings.h
@@ -0,0 +1,172 @@
+/*
+ *  Copyright (c) 2016, Nest Labs, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * @brief
+ *   This file includes platform abstractions for non-volatile storage of settings.
+ */
+
+#ifndef OT_PLATFORM_SETTINGS_H
+#define OT_PLATFORM_SETTINGS_H 1
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Performs any initialization for the settings subsystem, if necessary.
+ */
+void otPlatSettingsInit(void);
+
+/// Begin atomic change set
+/** This function is called at the start of a sequence of changes
+ *  that should be made atomically.
+ *
+ *  This function, along with `otPlatSettingsCommitChange()` are designed
+ *  to ensure atomicity of changes to multiple settings. Once
+ *  this function has been called, any changes made to the settings
+ *  store are only committed when `otPlatSettingsCommitChange()` is
+ *  called.
+ *
+ *  The implementation of this function is optional. If not
+ *  implemented, it should return kThreadError_None.
+ *
+ *  @retval kThreadError_None     The settings commit lock has been set.
+ *  @retval kThreadError_Already  The commit lock is already set.
+ *
+ * @sa otPlatSettingsCommitChange(), otPlatSettingsAbandonChange()
+ */
+ThreadError otPlatSettingsBeginChange(void);
+
+/// Commit all settings changes since previous call to otPlatSettingsBeginChange()
+/** This function is called at the end of a sequence of changes.
+ *  The implementation of this function is optional. If not
+ *  implemented, it should return kThreadError_NotImplemented.
+ *
+ *  @retval kThreadError_None     The changes made since the last call
+ *                                to otPlatSettingsBeginChange() have been
+ *                                successfully committed.
+ *  @retval kThreadError_InvalidState    otPlatSettingsBeginChange() has not
+ *                                       been called.
+ *  @retval kThreadError_NotImplemented  This function is not implemented
+ *                                       on this platform.
+ *
+ *  @sa otPlatSettingsBeginChange(), otPlatSettingsAbandonChange()
+ */
+ThreadError otPlatSettingsCommitChange(void);
+
+/// Abandon all settings changes since previous call to otPlatSettingsBeginChange()
+/** This function may be called at the end of a sequence of changes.
+ *  instead of otPlatSettingsCommitChange(). If implemented, it
+ *  causes all changes made since otPlatSettingsBeginChange() to be
+ *  rolled back and abandoned.
+ *
+ *  The implementation of this function is optional. If not
+ *  implemented, it should return kThreadError_NotImplemented..
+ *
+ *  @retval kThreadError_None     The changes made since the last call
+ *                                to otPlatSettingsBeginChange() have been
+ *                                successfully rolled back.
+ *  @retval kThreadError_InvalidState    otPlatSettingsBeginChange() has not
+ *                                       been called.
+ *  @retval kThreadError_NotImplemented  This function is not implemented
+ *                                       on this platform.
+ *
+ *  @sa otPlatSettingsBeginChange(), otPlatSettingsCommitChange()
+ */
+ThreadError otPlatSettingsAbandonChange(void);
+
+/// Fetches the value of a setting
+/** This function fetches the value of the setting identified
+ *  by aKey and write it to the memory pointed to by aValue.
+ *  It then writes the length to the integer pointed to by
+ *  aValueLength. The initial value of aValueLength is the
+ *  maximum number of bytes to be written to aValue.
+ *
+ *  This function can be used to check for the existance of
+ *  a key without fetching the value by setting aValue and
+ *  aValueLength to NULL. You can also check the length of
+ *  the setting without fetching it by setting only aValue
+ *  to NULL.
+ *
+ * @param[in]     aKey         The key associated with the requested setting.
+ * @param[out]    aValue       A pointer to where the value of the setting
+ *                             should be written. May be set to NULL if just
+ *                             testing for the presence or length of a setting.
+ * @param[in/out] aValueLength A pointer to the length of the value. When called,
+ *                             this pointer should point to an integer containing the
+ *                             maximum value size that can be written to aValue.
+ *                             At return, the actual length of the setting is written.
+ *                             This may be set to NULL if performing a presence check.
+ *
+ * @retval kThreadError_None     The given setting was found and fetched successfully.
+ * @retval kThreadError_NotFound The given setting was not found in the setting store.
+ * @retval kThreadError_NotImplemented  This function is not implemented on this platform.
+ */
+ThreadError otPlatSettingsGet(uint16_t aKey, uint8_t *aValue, int *aValueLength);
+
+/// Sets or replaces the value of a setting
+/** This function sets or replaces the value of a setting
+ *  identified by aKey.
+ *
+ * @param[in]     aKey         The key associated with the setting to change.
+ * @param[out]    aValue       A pointer to where the new value of the setting
+ *                             should be read from. MUST NOT be NULL if aValueLength
+ *                             is non-zero.
+ * @param[in/out] aValueLength The length of the data pointed to by aValue. May be zero.
+ *
+ * @retval kThreadError_None     The given setting was changed or staged.
+ * @retval kThreadError_NotImplemented  This function is not implemented on this platform.
+ */
+ThreadError otPlatSettingsSet(uint16_t aKey, const uint8_t *aValue, int aValueLength);
+
+/// Removes a setting from the setting store
+/** This function deletes the the setting identified
+ *  by aKey from the settings store.
+ *
+ * @param[in]     aKey         The key associated with the requested setting.
+ *
+ * @retval kThreadError_None     The given setting was found and removed successfully.
+ * @retval kThreadError_NotFound The given setting was not found in the setting store.
+ */
+ThreadError otPlatSettingsDelete(uint16_t aKey);
+
+/// Removes all settings from the setting store
+/** This function deletes all settings from the settings
+ *  store, resetting it to its initial factory state.
+ */
+void otPlatSettingsWipe(void);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // OT_PLATFORM_SETTINGS_H
