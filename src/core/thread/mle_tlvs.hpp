2baff05f0dd44e566e6bd5a17d1b4d90a38d4d37
Jonathan Hui <jonhui@nestlabs.com>
2016-08-01 09:35:44 -0700

    Update Connectivity TLV. (#310)
    * Remove Max Child Count, and Child Count fields.
    * Add Parent Priority, Active Routers, SED Buffer Size, and SED Datagram Size fields.
    * Update parent selection logic.

--- src/core/thread/mle_tlvs.hpp
+++ src/core/thread/mle_tlvs.hpp
@@ -982,36 +982,20 @@ public:
     bool IsValid(void) const { return GetLength() == sizeof(*this) - sizeof(Tlv); }
 
     /**
-     * This method returns the Max Child Count value.
+     * This method returns the Parent Priority value.
      *
-     * @returns The Max Child Count value.
+     * @returns The Parent Priority value.
      *
      */
-    uint8_t GetMaxChildCount(void) const { return mMaxChildCount; }
+    int8_t GetParentPriority(void) const { return mParentPriority >> kParentPriorityOffset; }
 
     /**
-     * This method sets the Max Child Count value.
+     * This method sets the Parent Priority value.
      *
-     * @param[in]  aCount  The Max Child Count value.
-     *
-     */
-    void SetMaxChildCount(uint8_t aCount) { mMaxChildCount = aCount; }
-
-    /**
-     * This method returns the Child Count value.
-     *
-     * @returns The Child Count value.
+     * @param[in] aParentPriority  The Parent Priority value.
      *
      */
-    uint8_t GetChildCount(void) const { return mChildCount; }
-
-    /**
-     * This method sets the Child Count value.
-     *
-     * @param[in]  aCount  The Child Count value.
-     *
-     */
-    void SetChildCount(uint8_t aCount) { mChildCount = aCount; }
+    void SetParentPriority(int8_t aParentPriority) { mParentPriority = aParentPriority << kParentPriorityOffset; }
 
     /**
      * This method returns the Link Quality 3 value.
@@ -1062,6 +1046,22 @@ public:
     void SetLinkQuality1(uint8_t aLinkQuality) { mLinkQuality1 = aLinkQuality; }
 
     /**
+     * This method sets the Active Routers value.
+     *
+     * @returns The Active Routers value.
+     *
+     */
+    uint8_t GetActiveRouters(void) const { return mActiveRouters; }
+
+    /**
+     * This method sets the Active Routers value.
+     *
+     * @param[in]  aActiveRouters  The Active Routers value.
+     *
+     */
+    void SetActiveRouters(uint8_t aActiveRouters) { mActiveRouters = aActiveRouters; }
+
+    /**
      * This method returns the Leader Cost value.
      *
      * @returns The Leader Cost value.
@@ -1093,14 +1093,53 @@ public:
      */
     void SetIdSequence(uint8_t aSequence) { mIdSequence = aSequence; }
 
+    /**
+     * This method returns the SED Buffer Size value.
+     *
+     * @returns The SED Buffer Size value.
+     *
+     */
+    uint16_t GetSedBufferSize(void) const { return HostSwap16(mSedBufferSize); }
+
+    /**
+     * This method sets the SED Buffer Size value.
+     *
+     * @param[in]  aSedBufferSize  The SED Buffer Size value.
+     *
+     */
+    void SetSedBufferSize(uint16_t aSedBufferSize) { mSedBufferSize = HostSwap16(aSedBufferSize); }
+
+    /**
+     * This method returns the SED Datagram Count value.
+     *
+     * @returns The SED Datagram Count value.
+     *
+     */
+    uint8_t GetSedDatagramCount(void) const { return mSedDatagramCount; }
+
+    /**
+     * This method sets the SED Datagram Count value.
+     *
+     * @param[in]  aSedDatagramCount  The SED Datagram Count value.
+     *
+     */
+    void SetSedDatagramCount(uint8_t aSedDatagramCount) { mSedDatagramCount = aSedDatagramCount; }
+
 private:
-    uint8_t mMaxChildCount;
-    uint8_t mChildCount;
-    uint8_t mLinkQuality3;
-    uint8_t mLinkQuality2;
-    uint8_t mLinkQuality1;
-    uint8_t mLeaderCost;
-    uint8_t mIdSequence;
+    enum
+    {
+        kParentPriorityOffset = 6,
+    };
+
+    int8_t   mParentPriority;
+    uint8_t  mLinkQuality3;
+    uint8_t  mLinkQuality2;
+    uint8_t  mLinkQuality1;
+    uint8_t  mLeaderCost;
+    uint8_t  mIdSequence;
+    uint8_t  mActiveRouters;
+    uint16_t mSedBufferSize;
+    uint8_t  mSedDatagramCount;
 }  OT_TOOL_PACKED_END;
 
 /**
