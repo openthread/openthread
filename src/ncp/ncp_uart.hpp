c09e1cc05bf0f92ad12b2b6a7a54cad9a67b0eb6
Jonathan Hui <jonhui@nestlabs.com>
2016-09-13 11:34:55 -0700

    Change copyright to The OpenThread Authors.
    - Add a top-level file named AUTHORS which identifies the copyright
    holders of the project.

--- src/ncp/ncp_uart.hpp
+++ src/ncp/ncp_uart.hpp
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2016, Nest Labs, Inc.
+ *    Copyright (c) 2016, The OpenThread Authors.
  *    All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
