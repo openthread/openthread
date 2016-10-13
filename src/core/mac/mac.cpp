62a4ae49fe40a8db7f20ad6043f53c2f34ebe505
Nick Banks <nibanks@microsoft.com>
2016-10-13 10:20:44 -0700

    Remove the group bit check for setting extended mac addresses.

--- src/core/mac/mac.cpp
+++ src/core/mac/mac.cpp
@@ -349,12 +349,9 @@ const ExtAddress *Mac::GetExtAddress(void) const
     return &mExtAddress;
 }
 
-ThreadError Mac::SetExtAddress(const ExtAddress &aExtAddress)
+void Mac::SetExtAddress(const ExtAddress &aExtAddress)
 {
-    ThreadError error = kThreadError_None;
     uint8_t buf[sizeof(aExtAddress)];
-
-    VerifyOrExit(!aExtAddress.IsGroup(), error = kThreadError_InvalidArgs);
 
     for (size_t i = 0; i < sizeof(buf); i++)
     {
@@ -363,9 +360,6 @@ ThreadError Mac::SetExtAddress(const ExtAddress &aExtAddress)
 
     otPlatRadioSetExtendedAddress(mNetif.GetInstance(), buf);
     mExtAddress = aExtAddress;
-
-exit:
-    return error;
 }
 
 void Mac::GetHashMacAddress(ExtAddress *aHashMacAddress)
