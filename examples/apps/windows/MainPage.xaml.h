e3f7375672b0d49975c77a80ca0be7b8b500bb63
Nick Banks <nibanks@microsoft.com>
2016-11-08 09:11:59 -0800

    Add an initial Windows Universal App for OpenThread

---
+++ examples/apps/windows/MainPage.xaml.h
@@ -0,0 +1,57 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace Thread
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public ref class MainPage sealed
+    {
+    public:
+        MainPage();
+
+        void OnResuming();
+
+        void BuildInterfaceList();
+
+        void ConnectNetwork(Platform::Guid InterfaceGuid);
+        void ShowInterfaceDetails(Platform::Guid InterfaceGuid);
+        void DisconnectNetwork(Platform::Guid InterfaceGuid);
+
+    protected:
+        virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+
+    private:
+
+        void OnLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void OnUnloaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+        void OnWindowSizeChanged(Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ args);
+        void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ coreWindow, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+
+        UIElement^ CreateNewInterface(Platform::Guid InterfaceGuid); 
+
+        bool _isVisible;
+        bool _isFullScreen;
+
+        Windows::Foundation::Size _windowSize;
+        
+        void *_apiInstance;
+        #define ApiInstance ((otApiInstance*)_apiInstance)
+
+        std::vector<void*> _devices;
+
+        Platform::Guid _currentInterface;
+
+    internal:
+        static MainPage^ Current;
+
+    };
+}
---
+++ examples/apps/windows/pch.h
@@ -0,0 +1,23 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <collection.h>
+#include <ppltasks.h>
+
+#include <ws2def.h>
+#include <ws2ipdef.h>
+#include <mstcpip.h>
+#define OTDLL 1
+#include <openthread.h>
+#include <commissioning/commissioner.h>
+#include <commissioning/joiner.h>
+
+#include "App.xaml.h"
