00d21b4412412dbe8877ac772620a125db4dabad
KaSroka <kamil.sroka@nordicsemi.no>
2017-01-26 09:39:27 +0100

    Add custom diagnostic commands to nrf52840

--- examples/platforms/nrf52840/diag.c
+++ examples/platforms/nrf52840/diag.c
@@ -28,24 +28,74 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <platform/diag.h>
+#include <platform/logging.h>
+#include <platform/alarm.h>
+
+#include <common/code_utils.hpp>
+
+static void otPlatDiagProcessListen(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen);
+static void otPlatDiagProcessID(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen);
+static void otPlatDiagProcessTransmit(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen);
+static ThreadError otPlatDiagParseLong(char *argv, long *value);
+static void otPlatDiagAppendErrorResult(ThreadError aError, char *aOutput, size_t aOutputMaxLen);
+
+struct DiagCommand
+{
+    const char *mName;
+    void (*mCommand)(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen);
+};
+
+struct DiagMessage
+{
+    const char messageDescriptor[11];
+    uint8_t channel;
+    int16_t id;
+    uint64_t cnt;
+};
+
+const struct DiagCommand sCommands[] =
+{
+        {"listen", &otPlatDiagProcessListen },
+        {"transmit", &otPlatDiagProcessTransmit },
+        {"id", &otPlatDiagProcessID }
+};
 
 /**
  * Diagnostics mode variables.
  *
  */
 static bool sDiagMode = false;
+static bool sListen = false;
+static bool sTransmitActive = false;
 static uint8_t sChannel = 20;
 static int8_t sTxPower = 0;
+static uint32_t sTxPeriod = 1;
+static int32_t sTxCount = 1;
+static int32_t sTxRequestedCount = 1;
+static int16_t sID = -1;
+static struct DiagMessage sDiagMessage = {.messageDescriptor = "DiagMessage", .channel = 0, .id = 0, .cnt = 0};
 
 void otPlatDiagProcess(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen)
 {
-    // Add more plarform specific diagnostics features here.
-    snprintf(aOutput, aOutputMaxLen, "diag feature '%s' is not supported\r\n", argv[0]);
-    (void) argc;
-    (void) aInstance;
+    uint32_t i;
+
+    for (i = 0; i < sizeof(sCommands) / sizeof(sCommands[0]); i++)
+    {
+        if (strcmp(argv[0], sCommands[i].mName) == 0)
+        {
+            sCommands[i].mCommand(aInstance, argc - 1, argc > 1 ? &argv[1] : NULL, aOutput, aOutputMaxLen);
+            break;
+        }
+    }
+
+    if (i == sizeof(sCommands) / sizeof(sCommands[0]))
+    {
+        snprintf(aOutput, aOutputMaxLen, "diag feature '%s' is not supported\r\n", argv[0]);
+    }
 }
 
 void otPlatDiagModeSet(bool aMode)
@@ -68,10 +118,183 @@ void otPlatDiagTxPowerSet(int8_t aTxPower){
 
 void otPlatDiagRadioReceived(otInstance *aInstance, RadioPacket *aFrame, ThreadError aError){
     (void) aInstance;
-    (void) aFrame;
-    (void) aError;
+
+    if(sListen && (aError == kThreadError_None))
+    {
+        if(aFrame->mLength == sizeof(struct DiagMessage))
+        {
+            struct DiagMessage *message = (struct DiagMessage*)aFrame->mPsdu;
+            if(strncmp(message->messageDescriptor, "DiagMessage", 11) == 0)
+            {
+                otPlatLog(kLogLevelDebg, kLogRegionNetDiag, "{\"Frame\": {"
+                            "\"LocalChannel\": %d, "
+                            "\"RemoteChannel\": %d, "
+                            "\"CNT\": %d, \"LocalID\": %d, "
+                            "\"RemoteID\": %d, "
+                            "\"RSSI\": %d"
+                            "}}\r\n",
+                            (int)(aFrame->mChannel),
+                            (int)(message->channel),
+                            (int)(message->cnt),
+                            (int)(sID),
+                            (int)(message->id),
+                            (int)(aFrame->mPower)
+                        );
+            }
+        }
+    }
 }
 
 void otPlatDiagAlarmCallback(otInstance *aInstance){
+    if(sTransmitActive)
+    {
+        if((sTxCount > 0) || (sTxCount == -1))
+        {
+            RadioPacket *sTxPacket = otPlatRadioGetTransmitBuffer(aInstance);
+
+            sTxPacket->mLength = sizeof(struct DiagMessage);
+            sTxPacket->mChannel = sChannel;
+            sTxPacket->mPower = sTxPower;
+
+            sDiagMessage.channel = sTxPacket->mChannel;
+            sDiagMessage.id = sID;
+
+            memcpy(sTxPacket->mPsdu, &sDiagMessage, sizeof(struct DiagMessage));
+            otPlatRadioTransmit(aInstance, sTxPacket);
+
+            sDiagMessage.cnt++;
+
+            if(sTxCount != -1)
+            {
+                sTxCount--;
+            }
+
+            uint32_t now = otPlatAlarmGetNow();
+            otPlatAlarmStartAt(aInstance, now, sTxPeriod);
+        }
+        else
+        {
+            sTransmitActive = false;
+            otPlatAlarmStop(aInstance);
+            otPlatLog(kLogLevelDebg, kLogRegionNetDiag, "Transmit done");
+        }
+    }
+}
+
+static ThreadError otPlatDiagParseLong(char *argv, long *value)
+{
+    char *endptr;
+    *value = strtol(argv, &endptr, 0);
+    return (*endptr == '\0') ? kThreadError_None : kThreadError_Parse;
+}
+
+static void otPlatDiagAppendErrorResult(ThreadError aError, char *aOutput, size_t aOutputMaxLen)
+{
+    if (aError != kThreadError_None)
+    {
+        snprintf(aOutput, aOutputMaxLen, "failed\r\nstatus %#x\r\n", aError);
+    }
+}
+
+static void otPlatDiagProcessListen(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen)
+{
     (void) aInstance;
+    ThreadError error = kThreadError_None;
+
+    VerifyOrExit(otPlatDiagModeGet(), error = kThreadError_InvalidState);
+
+    if (argc == 0)
+    {
+        snprintf(aOutput, aOutputMaxLen, "listen: %s\r\n", sListen==true?"yes":"no");
+    }
+    else
+    {
+        long value;
+
+        SuccessOrExit(error = otPlatDiagParseLong(argv[0], &value));
+        sListen = (bool)(value);
+        snprintf(aOutput, aOutputMaxLen, "set listen to %s\r\nstatus 0x%02x\r\n", sListen==true?"yes":"no", error);
+    }
+
+exit:
+    otPlatDiagAppendErrorResult(error, aOutput, aOutputMaxLen);
+}
+
+static void otPlatDiagProcessID(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen)
+{
+    (void) aInstance;
+
+    ThreadError error = kThreadError_None;
+
+    VerifyOrExit(otPlatDiagModeGet(), error = kThreadError_InvalidState);
+
+    if (argc == 0)
+    {
+        snprintf(aOutput, aOutputMaxLen, "ID: %d\r\n", (int)sID);
+    }
+    else
+    {
+        long value;
+
+        SuccessOrExit(error = otPlatDiagParseLong(argv[0], &value));
+        VerifyOrExit(value>=0, error = kThreadError_InvalidArgs);
+        sID = (int16_t)(value);
+        snprintf(aOutput, aOutputMaxLen, "set ID to %d\r\nstatus 0x%02x\r\n", (int)sID, error);
+    }
+
+exit:
+    otPlatDiagAppendErrorResult(error, aOutput, aOutputMaxLen);
+}
+
+static void otPlatDiagProcessTransmit(otInstance *aInstance, int argc, char *argv[], char *aOutput, size_t aOutputMaxLen)
+{
+    ThreadError error = kThreadError_None;
+
+    VerifyOrExit(otPlatDiagModeGet(), error = kThreadError_InvalidState);
+
+    if (argc == 0)
+    {
+        snprintf(aOutput, aOutputMaxLen, "transmit will send %d diagnostic messages with %d ms interval\r\nstatus 0x%02x\r\n",
+                (int)(sTxRequestedCount), (int)(sTxPeriod), error);
+    }
+    else if (strcmp(argv[0], "stop") == 0)
+    {
+        otPlatAlarmStop(aInstance);
+        snprintf(aOutput, aOutputMaxLen, "diagnostic message transmission is stopped\r\nstatus 0x%02x\r\n", error);
+        sTransmitActive = false;
+    }
+    else if (strcmp(argv[0], "start") == 0)
+    {
+        otPlatAlarmStop(aInstance);
+        sTransmitActive = true;
+        sTxCount = sTxRequestedCount;
+        uint32_t now = otPlatAlarmGetNow();
+        otPlatAlarmStartAt(aInstance, now, sTxPeriod);
+        snprintf(aOutput, aOutputMaxLen, "sending %d diagnostic messages with %d ms interval\r\nstatus 0x%02x\r\n",
+                (int)(sTxRequestedCount), (int)(sTxPeriod), error);
+    }
+    else if (strcmp(argv[0], "interval") == 0)
+    {
+        long value;
+
+        VerifyOrExit(argc == 2, error = kThreadError_InvalidArgs);
+
+        SuccessOrExit(error = otPlatDiagParseLong(argv[1], &value));
+        VerifyOrExit(value>0, error = kThreadError_InvalidArgs);
+        sTxPeriod = (uint32_t)(value);
+        snprintf(aOutput, aOutputMaxLen, "set diagnostic messages interval to %d ms\r\nstatus 0x%02x\r\n", (int)(sTxPeriod), error);
+    }
+    else if (strcmp(argv[0], "count") == 0)
+    {
+        long value;
+
+        VerifyOrExit(argc == 2, error = kThreadError_InvalidArgs);
+
+        SuccessOrExit(error = otPlatDiagParseLong(argv[1], &value));
+        VerifyOrExit((value > 0) || (value == -1), error = kThreadError_InvalidArgs);
+        sTxRequestedCount = (uint32_t)(value);
+        snprintf(aOutput, aOutputMaxLen, "set diagnostic messages count to %d\r\nstatus 0x%02x\r\n", (int)(sTxRequestedCount), error);
+    }
+exit:
+    otPlatDiagAppendErrorResult(error, aOutput, aOutputMaxLen);
 }
