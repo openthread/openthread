a7fd9922c9136576deb24d8a21af99b362b615f1
Seth Rickard <s-rickard@ti.com>
2017-02-10 12:48:11 -0800

    Add constructors and destructors to crypto objects
    Ensures mbedtls init and free functions are called when crypto objects
    enter and fall out of scope.

--- src/core/crypto/aes_ecb.cpp
+++ src/core/crypto/aes_ecb.cpp
@@ -36,15 +36,24 @@
 namespace Thread {
 namespace Crypto {
 
-void AesEcb::SetKey(const uint8_t *aKey, uint16_t aKeyLength)
+AesEcb::AesEcb()
 {
     mbedtls_aes_init(&mContext);
+}
+
+void AesEcb::SetKey(const uint8_t *aKey, uint16_t aKeyLength)
+{
     mbedtls_aes_setkey_enc(&mContext, aKey, aKeyLength);
 }
 
 void AesEcb::Encrypt(const uint8_t aInput[kBlockSize], uint8_t aOutput[kBlockSize])
 {
     mbedtls_aes_crypt_ecb(&mContext, MBEDTLS_AES_ENCRYPT, aInput, aOutput);
+}
+
+AesEcb::~AesEcb()
+{
+    mbedtls_aes_free(&mContext);
 }
 
 }  // namespace Crypto
