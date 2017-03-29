8be1844ac6b8fa3badf97e8585866597c06866d4
Buke Po <bukepo@gmail.com>
2017-03-29 08:33:51 +0800

    make sure sock addr is unicast

--- src/core/coap/secure_coap_server.cpp
+++ src/core/coap/secure_coap_server.cpp
@@ -117,7 +117,10 @@ void SecureServer::Receive(Message &aMessage, const Ip6::MessageInfo &aMessageIn
     {
         mPeerAddress.SetPeerAddr(aMessageInfo.GetPeerAddr());
         mPeerAddress.SetPeerPort(aMessageInfo.GetPeerPort());
-        mPeerAddress.SetSockAddr(aMessageInfo.GetSockAddr());
+        if (mNetif.IsUnicastAddress(aMessageInfo.GetSockAddr()))
+        {
+            mPeerAddress.SetSockAddr(aMessageInfo.GetSockAddr());
+        }
         mPeerAddress.SetSockPort(aMessageInfo.GetSockPort());
 
         mNetif.GetDtls().Start(false, HandleDtlsConnected, HandleDtlsReceive, HandleDtlsSend, this);
