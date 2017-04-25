5baf82298c3507df3ff22371696b4be13dc58cc5
Abtin Keshavarzian <abtink@nestlabs.com>
2017-04-25 14:35:46 -0700

    Adding `topology.cpp`
    This commit adds `topology.cpp` corresponding to `topology.hpp`. It
    also moves some of the `Child` or `Neighbor` method implementations
    to the `cpp` file.

---
+++ src/core/thread/topology.cpp
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2016-2017, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ *   This file includes definitions for maintaining Thread network topologies.
+ */
+
+#define WPP_NAME "topology.tmh"
+
+#include <common/code_utils.hpp>
+#include <common/debug.hpp>
+#include <common/logging.hpp>
+#include <thread/topology.hpp>
+
+namespace Thread {
+
+void Neighbor::GenerateChallenge(void)
+{
+    for (uint8_t i = 0; i < sizeof(mValidPending.mPending.mChallenge); i++)
+    {
+        mValidPending.mPending.mChallenge[i] = static_cast<uint8_t>(otPlatRandomGet());
+    }
+}
+
+void Child::GenerateChallenge(void)
+{
+    for (uint8_t i = 0; i < sizeof(mAttachChallenge); i++)
+    {
+        mAttachChallenge[i] = static_cast<uint8_t>(otPlatRandomGet());
+    }
+}
+
+const Mac::Address &Child::GetMacAddress(Mac::Address &aMacAddress) const
+{
+    if (mUseShortAddress)
+    {
+        aMacAddress.mShortAddress = GetRloc16();
+        aMacAddress.mLength = sizeof(aMacAddress.mShortAddress);
+    }
+    else
+    {
+        aMacAddress.mExtAddress = GetExtAddress();
+        aMacAddress.mLength = sizeof(aMacAddress.mExtAddress);
+    }
+
+    return aMacAddress;
+}
+
+}  // namespace Thread
