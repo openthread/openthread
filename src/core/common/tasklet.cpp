00eeb3fabbdde889ccee9b35198608bae65d84ab
Duane Ellis <d-ellis@ti.com>
2017-04-26 13:39:35 -0700

    Add include openthread-config.h

--- src/core/common/tasklet.cpp
+++ src/core/common/tasklet.cpp
@@ -31,6 +31,12 @@
  *   This file implements the tasklet scheduler.
  */
 
+ #ifdef OPENTHREAD_CONFIG_FILE
+#include OPENTHREAD_CONFIG_FILE
+#else
+#include <openthread-config.h>
+#endif
+
 #include "openthread/openthread.h"
 
 #include <common/code_utils.hpp>
--- src/core/common/trickle_timer.cpp
+++ src/core/common/trickle_timer.cpp
@@ -31,6 +31,12 @@
  *   This file implements the trickle timer logic.
  */
 
+#ifdef OPENTHREAD_CONFIG_FILE
+#include OPENTHREAD_CONFIG_FILE
+#else
+#include <openthread-config.h>
+#endif
+
 #include "openthread/platform/random.h"
 
 #include <common/code_utils.hpp>
