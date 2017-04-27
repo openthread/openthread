7bfa72141d3eaaf58942da159521274044b4b317
Duane Ellis <d-ellis@ti.com>
2017-04-27 16:19:34 -0700

    Include opehtread-config.h fixes (for unit test code)

--- tests/unit/test_platform.cpp
+++ tests/unit/test_platform.cpp
@@ -26,6 +26,12 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef OPENTHREAD_CONFIG_FILE
+#include OPENTHREAD_CONFIG_FILE
+#else
+#include <openthread-config.h>
+#endif
+
 #include "test_platform.h"
 
 #if _WIN32
