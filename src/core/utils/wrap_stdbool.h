4f69f406f5cfdefb60dd06bebee2706585498822
Duane Ellis <d-ellis@ti.com>
2017-04-26 17:02:46 -0700

    Another varient for win vrs stdbool.h

--- src/core/utils/wrap_stdbool.h
+++ src/core/utils/wrap_stdbool.h
@@ -43,16 +43,20 @@
 #if __cplusplus
 /* c++ has a built in bool */
 #else
-#if !defined(_MSC_VER)
+
+#if defined(_MSC_VER)
+#define bool _Bool
+#else
 typedef _Bool bool;
 #endif // visual studio has a bool
-#endif
 
 #if !defined(__bool_true_false_are_defined)
 #define __bool_true_false_are_defined 1
 #define false 0
 #define true 1
 #endif // bool defined
+
+#endif // __cplusplus
 
 #endif // HAVE_STDBOOL_H
 
