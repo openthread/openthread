41c1dbf9ef0cbba809cc3adebf792a1e728eda1c
DuaneEllis-TI <d-ellis@ti.com>
2017-04-28 11:06:30 -0700

    Add include openthread-config.h to missing_str*.c files (#1677)
    * Add include openthread-config.h to missing_str*.c files
    
    * Enable _BSD_SOURCE and _DEFAULT_SOURCE

--- src/core/utils/missing_strlcpy.c
+++ src/core/utils/missing_strlcpy.c
@@ -25,6 +25,12 @@
  *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef OPENTHREAD_CONFIG_FILE
+#include OPENTHREAD_CONFIG_FILE
+#else
+#include <openthread-config.h>
+#endif
+
 #include "utils/wrap_string.h"
 
 size_t missing_strlcpy(char *dest, const char *src, size_t size)
