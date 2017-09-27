7dbc20d2ddecc65d84622c7f2c9ff7e1c18e77bd
Jonathan Hui <jonhui@nestlabs.com>
2017-09-27 12:18:41 -0700

    [mle] fix MLE Link Accept timeout on resync after boot (#2230)

--- src/core/thread/mle_router.cpp
+++ src/core/thread/mle_router.cpp
@@ -1793,8 +1793,13 @@ void MleRouter::HandleStateUpdateTimer(void)
         break;
 
     case OT_DEVICE_ROLE_DETACHED:
-        BecomeDetached();
-        ExitNow();
+        if (mChallengeTimeout == 0)
+        {
+            BecomeDetached();
+            ExitNow();
+        }
+
+        break;
 
     case OT_DEVICE_ROLE_CHILD:
         if (routerStateUpdate)
