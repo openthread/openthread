1a4985e44dd4274145a08a64869f55d63652ddfd
Abtin Keshavarzian <abtink@google.com>
2018-01-15 19:59:10 -0800

    [types] remove unused CONTAINING_RECORD macro

--- include/openthread/types.h
+++ include/openthread/types.h
@@ -54,18 +54,6 @@ extern "C" {
 #else
 #include <windows.h>
 #endif
-#else
-#ifndef CONTAINING_RECORD
-/*#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Winvalid-offsetof"
-#define CONTAINING_RECORD(address, type, field) \
-    ((type *)((uint8_t*)(address) - offsetof(type, field)))
-#pragma GCC diagnostic pop*/
-#define BASE 0x1
-#define myoffsetof(s,m) (((size_t)&(((s*)BASE)->m))-BASE)
-#define CONTAINING_RECORD(address, type, field) \
-    ((type *)((uint8_t*)(address) - myoffsetof(type, field)))
-#endif /* CONTAINING_RECORD */
 #endif /* _WIN32 */
 
 /**
