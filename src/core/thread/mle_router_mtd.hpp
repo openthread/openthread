1f4b082f2b6b74708b70c606486540ab47875812
Jonathan Hui <jonhui@nestlabs.com>
2018-01-16 17:00:02 +0000

    [mle] abort Address Solicit on partition change (#2481)
    This commit aborts the Address Solicit transaction on a partition change.
    Doing so avoids having Address Solicit messages being sent to the wrong
    Leader address and delays caused by waiting for the previous Address Solicit
    transaction to time out.

--- src/core/thread/mle_router_mtd.hpp
+++ src/core/thread/mle_router_mtd.hpp
@@ -151,6 +151,7 @@ private:
     otError HandleDataRequest(const Message &, const Ip6::MessageInfo &) { return OT_ERROR_DROP; }
     otError HandleNetworkDataUpdateRouter(void) { return OT_ERROR_NONE; }
     otError HandleDiscoveryRequest(const Message &, const Ip6::MessageInfo &) { return OT_ERROR_DROP; }
+    void HandlePartitionChange(void) { }
 
     void StopAdvertiseTimer(void) { }
     otError ProcessRouteTlv(const RouteTlv &aRoute) { OT_UNUSED_VARIABLE(aRoute); return OT_ERROR_NONE; }
