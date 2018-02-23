1c6714712b3e56b5d4d1e151f80c8904cb59a0fd
Jonathan Hui <jonhui@nestlabs.com>
2018-02-23 17:04:56 +0000

    [tests] fix occasional failures in Cert_6_1_03_RouterAttachConnectivity.py (#2580)
    This commit adds necessary delay to allow the router topology to establish
    links with neighboring routers.

--- tests/scripts/thread-cert/Cert_6_1_03_RouterAttachConnectivity.py
+++ tests/scripts/thread-cert/Cert_6_1_03_RouterAttachConnectivity.py
@@ -95,8 +95,13 @@ class Cert_6_1_3_RouterAttachConnectivity(unittest.TestCase):
 
         for i in range(2, 5):
             self.nodes[i].start()
-            self.simulator.go(5)
+
+        self.simulator.go(5)
+
+        for i in range(2, 5):
             self.assertEqual(self.nodes[i].get_state(), 'router')
+
+        self.simulator.go(config.MAX_ADVERTISEMENT_INTERVAL)
 
         self.nodes[ED].start()
         self.simulator.go(5)
