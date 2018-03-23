f64d51f9fe614e3de7a389fc5dc1573896e3f7fd
Abtin Keshavarzian <abtink@google.com>
2018-03-23 15:12:15 -0700

    [key-manager] set default key from KeyManager (#2636)
    This commit moves the setting/initialization of default key from
    `ThreadNetif` to `KeyManager` class.

--- src/core/thread/thread_netif.cpp
+++ src/core/thread/thread_netif.cpp
@@ -49,25 +49,6 @@ using ot::Encoding::BigEndian::HostSwap16;
 
 namespace ot {
 
-static const otMasterKey kThreadMasterKey = {{
-    0x00,
-    0x11,
-    0x22,
-    0x33,
-    0x44,
-    0x55,
-    0x66,
-    0x77,
-    0x88,
-    0x99,
-    0xaa,
-    0xbb,
-    0xcc,
-    0xdd,
-    0xee,
-    0xff,
-}};
-
 ThreadNetif::ThreadNetif(Instance &aInstance)
     : Netif(aInstance, OT_NETIF_INTERFACE_ID_THREAD)
     , mCoap(aInstance)
@@ -123,7 +104,6 @@ ThreadNetif::ThreadNetif(Instance &aInstance)
     , mEnergyScan(aInstance)
 
 {
-    mKeyManager.SetMasterKey(kThreadMasterKey);
     mCoap.SetInterceptor(&ThreadNetif::TmfFilter, this);
 }
 
