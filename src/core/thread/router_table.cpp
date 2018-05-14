8492c46c3ee6dd739f7cf8d78dfcdf5341291553
Jonathan Hui <jonhui@nestlabs.com>
2018-05-14 12:50:51 -0700

    [router-table] validate router id before allocating router
    Credit to OSS-Fuzz.

--- src/core/thread/router_table.cpp
+++ src/core/thread/router_table.cpp
@@ -241,7 +241,7 @@ Router *RouterTable::Allocate(uint8_t aRouterId)
 {
     Router *rval = NULL;
 
-    VerifyOrExit(!IsAllocated(aRouterId) && mRouterIdReuseDelay[aRouterId] == 0);
+    VerifyOrExit(aRouterId <= Mle::kMaxRouterId && !IsAllocated(aRouterId) && mRouterIdReuseDelay[aRouterId] == 0);
 
     mAllocatedRouterIds[aRouterId / 8] |= 1 << (aRouterId % 8);
     UpdateAllocation();
