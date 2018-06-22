d5797546e1bad8e0674630fb6183e77413de2524
Kamil Sroka <kamilsroka92@gmail.com>
2018-06-22 18:23:50 +0200

    [link-raw] handle mIsCcaEnabled flag with software retransmit (#2821)

--- src/core/api/link_raw_api.cpp
+++ src/core/api/link_raw_api.cpp
@@ -218,16 +218,21 @@ otError LinkRaw::Transmit(otRadioFrame *aFrame, otLinkRawTransmitDone aCallback)
         mTransmitDoneCallback = aCallback;
 
 #if OPENTHREAD_CONFIG_ENABLE_SOFTWARE_RETRANSMIT
-        OT_UNUSED_VARIABLE(aFrame);
-        mTransmitAttempts = 0;
-        mCsmaAttempts     = 0;
+        if (aFrame->mInfo.mTxInfo.mIsCcaEnabled)
+        {
+            mTransmitAttempts = 0;
+            mCsmaAttempts     = 0;
 
-        // Start the transmission backlog logic
-        StartCsmaBackoff();
-        error = OT_ERROR_NONE;
-#else
-        // Let the hardware do the transmission logic
-        error = otPlatRadioTransmit(&mInstance, aFrame);
+            // Start the transmission backoff logic
+            StartCsmaBackoff();
+            error = OT_ERROR_NONE;
+        }
+        else
+        {
+#endif
+            error = otPlatRadioTransmit(&mInstance, aFrame);
+#if OPENTHREAD_CONFIG_ENABLE_SOFTWARE_RETRANSMIT
+        }
 #endif
     }
 
