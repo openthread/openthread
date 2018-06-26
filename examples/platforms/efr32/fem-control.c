13d308e3c282636b855027bed207d8058c1f4d43
Tomas Cerskus <tomas@amberbox.com>
2018-06-26 12:57:56 +0100

    [efr32] create fem-control.c that includes the one from Gecko SDK

---
+++ examples/platforms/efr32/fem-control.c
@@ -0,0 +1,5 @@
+#include "hal-config.h"
+
+#if (HAL_FEM_ENABLE)
+#include "../../../third_party/silabs/gecko_sdk_suite/v2.2/util/plugin/plugin-common/fem-control/fem-control.c"
+#endif
--- examples/platforms/efr32/platform.c
+++ examples/platforms/efr32/platform.c
@@ -48,7 +48,7 @@
 #include "hal-config.h"
 
 #if (HAL_FEM_ENABLE)
-#include "../../../third_party/silabs/gecko_sdk_suite/v2.2/util/plugin/plugin-common/fem-control/fem-control.c"
+#include "fem-control.h"
 #endif
 
 void halInitChipSpecific(void);
