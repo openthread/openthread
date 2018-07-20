0a4fd214201bf288ed9433754a7c9b046d5e0e79
Jonathan Hui <jonhui@nestlabs.com>
2018-07-20 09:24:19 -0700

    [efr32] update to Gecko SDK v2.3

--- examples/platforms/efr32/fem-control.c
+++ examples/platforms/efr32/fem-control.c
@@ -29,5 +29,5 @@
 #include "hal-config.h"
 
 #if (HAL_FEM_ENABLE)
-#include "../../../third_party/silabs/gecko_sdk_suite/v2.2/util/plugin/plugin-common/fem-control/fem-control.c"
+#include "../../../third_party/silabs/gecko_sdk_suite/v2.3/util/plugin/plugin-common/fem-control/fem-control.c"
 #endif
