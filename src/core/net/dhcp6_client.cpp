7be61df6ecfc7ca48af927c0ce9153bff1c56fc6
Jonathan Hui <jonhui@nestlabs.com>
2018-07-31 14:10:02 -0500

    [message-info] remove unnecessary memset initialization (#2937)

--- src/core/net/dhcp6_client.cpp
+++ src/core/net/dhcp6_client.cpp
@@ -387,7 +387,6 @@ otError Dhcp6Client::Solicit(uint16_t aRloc16)
     SuccessOrExit(error = AppendIaAddress(*message, aRloc16));
     SuccessOrExit(error = AppendRapidCommit(*message));
 
-    memset(&messageInfo, 0, sizeof(messageInfo));
     memcpy(messageInfo.GetPeerAddr().mFields.m8, netif.GetMle().GetMeshLocalPrefix(), 8);
     messageInfo.GetPeerAddr().mFields.m16[4] = HostSwap16(0x0000);
     messageInfo.GetPeerAddr().mFields.m16[5] = HostSwap16(0x00ff);
