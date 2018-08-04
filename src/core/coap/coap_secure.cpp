2914f7a3576d38965c5e8b4965509c346dbddd46
Christian <chrigi@staufi.ch>
2018-08-04 12:48:28 +0200

    [coaps] rm unused var from initializer list

--- src/core/coap/coap_secure.cpp
+++ src/core/coap/coap_secure.cpp
@@ -55,7 +55,6 @@ CoapSecure::CoapSecure(Instance &aInstance)
     , mTransmitMessage(NULL)
     , mTransmitTask(aInstance, &CoapSecure::HandleUdpTransmit, this)
     , mLayerTwoSecurity(false)
-    , mApplicationCoapSecure(false)
 {
 }
 
@@ -72,7 +71,6 @@ CoapSecure::CoapSecure(Instance &       aInstance,
     , mTransmitMessage(NULL)
     , mTransmitTask(aInstance, aUdpTransmitHandle, this)
     , mLayerTwoSecurity(true)
-    , mApplicationCoapSecure(true)
 {
 }
 #endif // OPENTHREAD_ENABLE_APPLICATION_COAP_SECURE
