c953c4f757f39f72e5a257c1eb17d1effa10d5b4
Yakun Xu <xyk@google.com>
2018-09-19 00:31:40 +0800

    [posix] flash file permission to 600 (#3085)

--- src/posix/platform/flash.c
+++ src/posix/platform/flash.c
@@ -80,7 +80,7 @@ otError utilsFlashInit(void)
         create = true;
     }
 
-    sFlashFd = open(fileName, O_RDWR | O_CREAT, 0666);
+    sFlashFd = open(fileName, O_RDWR | O_CREAT, 0600);
     lseek(sFlashFd, 0, SEEK_SET);
 
     otEXPECT_ACTION(sFlashFd >= 0, error = OT_ERROR_FAILED);
