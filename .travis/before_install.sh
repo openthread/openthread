d64b889f5217b2af985c75accc52d2f97858a4c0
Jonathan Hui <jonhui@nestlabs.com>
2018-10-04 00:28:54 -0700

    [travis] add python3 to continuous integration (#3146)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -40,11 +40,11 @@ cd /tmp || die
     sudo apt-get update || die
 
     [ $BUILD_TARGET != posix-distcheck -a $BUILD_TARGET != posix-32-bit -a $BUILD_TARGET != posix-app-cli -a $BUILD_TARGET != posix-mtd -a $BUILD_TARGET != posix-ncp -a $BUILD_TARGET != posix-app-ncp ] || {
-        pip install --user --upgrade pip || die
-        pip install --user -r $TRAVIS_BUILD_DIR/tests/scripts/thread-cert/requirements.txt || die
+        pip install --upgrade pip || die
+        pip install -r $TRAVIS_BUILD_DIR/tests/scripts/thread-cert/requirements.txt || die
         [ $BUILD_TARGET != posix-ncp -a $BUILD_TARGET != posix-app-ncp ] || {
             # Packages used by ncp tools.
-            pip install --user git+https://github.com/openthread/pyspinel || die
+            pip install git+https://github.com/openthread/pyspinel || die
         }
     }
 
