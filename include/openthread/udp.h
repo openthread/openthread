70abcdb034eba9e9536ca0659ff6b47505318575
Yakun Xu <xyk@google.com>
2018-11-01 23:21:59 +0800

    Update udp.h

--- include/openthread/udp.h
+++ include/openthread/udp.h
@@ -183,7 +183,7 @@ otError otUdpConnect(otUdpSocket *aSocket, otSockAddr *aSockName);
  * @param[in]  aMessage      A pointer to a message buffer.
  * @param[in]  aMessageInfo  A pointer to a message info structure.
  *
- * @note Caller is responsible freeing @p aMessage if this call fails.
+ * @note Caller is responsible for freeing @p aMessage if this call fails.
  *
  * @retval OT_ERROR_NONE            The message is successfully scheduled for sending.
  * @retval OT_ERROR_INVALID_ARGS    Invalid arguments are given.
