f58b8627b68acde1e6a11c92923e99879f908b60
Matt Smith <smides@nestlabs.com>
2018-12-03 09:10:33 -0800

    [logging] add initial log level config option (#3337)
    Add OPENTHREAD_CONFIG_INTIIAL_LOG_LEVEL config setting to enable
    starting OpenThread with a lower log level that the maximum compile-time
    setting. Useful when OPENTHREAD_CONFIG_ENABLE_DYNAMIC_LOG_LEVEL is
    defined to 1.

--- src/core/common/instance.cpp
+++ src/core/common/instance.cpp
@@ -85,7 +85,7 @@ Instance::Instance(void)
     , mLinkRaw(*this)
 #endif // OPENTHREAD_RADIO || OPENTHREAD_ENABLE_RAW_LINK_API
 #if OPENTHREAD_CONFIG_ENABLE_DYNAMIC_LOG_LEVEL
-    , mLogLevel(static_cast<otLogLevel>(OPENTHREAD_CONFIG_LOG_LEVEL))
+    , mLogLevel(static_cast<otLogLevel>(OPENTHREAD_CONFIG_INITIAL_LOG_LEVEL))
 #endif
 #if OPENTHREAD_ENABLE_VENDOR_EXTENSION
     , mExtension(Extension::ExtensionBase::Init(*this))
