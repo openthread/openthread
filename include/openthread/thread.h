b99836924ca413af8577f21e6b29c8944b1d5a7d
Jonathan Hui <jonhui@nestlabs.com>
2019-01-07 09:03:55 -0800

    [api] fix typos (#3431)

--- include/openthread/thread.h
+++ include/openthread/thread.h
@@ -166,7 +166,7 @@ typedef struct otMleCounters
     uint16_t mRouterRole;                    ///< Number of times device entered OT_DEVICE_ROLE_ROUTER role.
     uint16_t mLeaderRole;                    ///< Number of times device entered OT_DEVICE_ROLE_LEADER role.
     uint16_t mAttachAttempts;                ///< Number of attach attempts while device was detached.
-    uint16_t mParitionIdChanges;             ///< Number of changes to partition ID.
+    uint16_t mPartitionIdChanges;            ///< Number of changes to partition ID.
     uint16_t mBetterPartitionAttachAttempts; ///< Number of attempts to attach to a better partition.
 
     /**
