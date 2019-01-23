5a6d7eedac5c7ea49c13c84c9d834f2accbd603c
Jeff Bumgardner <jbumgardner@google.com>
2019-01-23 10:00:52 -0800

    [docs] Doxygen tag fixes

--- include/openthread/ip6.h
+++ include/openthread/ip6.h
@@ -593,6 +593,7 @@ bool otIp6IsAddressUnspecified(const otIp6Address *aAddress);
 /**
  * This function perform OpenThread source address selection.
  *
+ * @param[in]     aInstance     A pointer to an OpenThread instance.
  * @param[inout]  aMessageInfo  A pointer to the message information.
  *
  * @retval  OT_ERROR_NONE       Found a source address and is filled into mSockAddr of @p aMessageInfo.
