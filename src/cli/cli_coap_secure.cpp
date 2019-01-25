ac8ae1f251f85377505b47f568907df3eada48b1
Jonathan Hui <jonhui@nestlabs.com>
2019-01-25 15:36:50 -0800

    [cli] add length checks for "coaps set psk" (#3518)

--- src/cli/cli_coap_secure.cpp
+++ src/cli/cli_coap_secure.cpp
@@ -168,10 +168,16 @@ otError CoapSecure::Process(int argc, char *argv[])
             {
                 if (argc > 3)
                 {
-                    mPskLength   = (uint8_t)strlen(argv[2]);
-                    mPskIdLength = (uint8_t)strlen(argv[3]);
+                    size_t length;
 
+                    length = strlen(argv[2]);
+                    VerifyOrExit(length <= sizeof(mPsk), error = OT_ERROR_INVALID_ARGS);
+                    mPskLength = static_cast<uint8_t>(length);
                     memcpy(mPsk, argv[2], mPskLength);
+
+                    length = strlen(argv[3]);
+                    VerifyOrExit(length <= sizeof(mPskId), error = OT_ERROR_INVALID_ARGS);
+                    mPskIdLength = static_cast<uint8_t>(length);
                     memcpy(mPskId, argv[3], mPskIdLength);
 
                     SuccessOrExit(
