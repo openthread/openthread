e7c9f2e1b7197eb3b621ea7ac6f974f14bae641a
Abtin Keshavarzian <abtink@google.com>
2019-02-07 15:25:34 -0800

    [toranj] add test-case for data poll transmission and poll interval

--- tests/toranj/start.sh
+++ tests/toranj/start.sh
@@ -131,6 +131,7 @@ run test-025-network-data-timeout.py
 run test-026-slaac-address.py
 run test-027-child-mode-change.py
 run test-028-router-leader-reset-recovery.py
+run test-029-data-poll-interval.py
 run test-100-mcu-power-state.py
 run test-600-channel-manager-properties.py
 run test-601-channel-manager-channel-change.py
