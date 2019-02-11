2c756c01e7ab77656c69050d9616c4a05ec17561
Kamil Sroka <kamil.sroka@nordicsemi.no>
2019-02-11 14:48:56 +0100

    [build] don't define UINT32_MAX for C

--- include/openthread/platform/toolchain.h
+++ include/openthread/platform/toolchain.h
@@ -303,12 +303,14 @@ extern "C" {
 /*
  * Keil and IAR compiler doesn't provide type limits for C++.
  */
+#ifdef __cplusplus
 #if defined(__CC_ARM) || defined(__ICCARM__)
 
 #ifndef UINT32_MAX
 #define UINT32_MAX 0xffffffff
 #endif
 
+#endif
 #endif
 
 /**
