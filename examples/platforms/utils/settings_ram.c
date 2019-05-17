7983fd40ae7817270f8cc2d45e20e994f3259e9d
Joseph Newman <joseph.newman@amberbox.com>
2019-05-17 10:29:32 +0100

    remove disused functions

--- examples/platforms/utils/settings_ram.c
+++ examples/platforms/utils/settings_ram.c
@@ -61,27 +61,6 @@ void otPlatSettingsInit(otInstance *aInstance)
     sSettingsBufLength = 0;
 }
 
-otError otPlatSettingsBeginChange(otInstance *aInstance)
-{
-    OT_UNUSED_VARIABLE(aInstance);
-
-    return OT_ERROR_NONE;
-}
-
-otError otPlatSettingsCommitChange(otInstance *aInstance)
-{
-    OT_UNUSED_VARIABLE(aInstance);
-
-    return OT_ERROR_NONE;
-}
-
-otError otPlatSettingsAbandonChange(otInstance *aInstance)
-{
-    OT_UNUSED_VARIABLE(aInstance);
-
-    return OT_ERROR_NONE;
-}
-
 otError otPlatSettingsGet(otInstance *aInstance, uint16_t aKey, int aIndex, uint8_t *aValue, uint16_t *aValueLength)
 {
     OT_UNUSED_VARIABLE(aInstance);
