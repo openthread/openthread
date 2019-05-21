8bb50d4ccde667295f892f976ee5ff90ba4a4a2f
Jonathan Hui <jonhui@nestlabs.com>
2019-05-20 20:37:19 -0700

    [ip6] add additional parse validation to Ip6::Address::FromString (#3843)

--- src/core/net/ip6_address.cpp
+++ src/core/net/ip6_address.cpp
@@ -316,6 +316,9 @@ otError Address::FromString(const char *aBuf)
 
             // Do not count bytes of the embedded IPv4 address.
             endp -= kIp4AddressSize;
+
+            VerifyOrExit(dst <= endp, error = OT_ERROR_PARSE);
+
             break;
         }
         else
