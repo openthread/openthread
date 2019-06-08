openthread/openthread

commit 607fa67f27a6f176c14b0da661f49550f33b1731

[thci] match meshlocalprefix accurately to classify the addresses correctly (#3639)

tools/harness-thci/OpenThread.py
@@ -254,7 +254,7 @@ class OpenThread(IThci):
                 # link local address
                 if ip6Addr.split(':')[4] != '0':
                     linkLocal64Addr = ip6Addr
-            elif ip6AddrPrefix == 'fd00':
+            elif ip6Addr.startswith(self.meshLocalPrefix):
                 # mesh local address
                 if ip6Addr.split(':')[4] == '0':
                     # rloc
@@ -1257,7 +1257,7 @@ class OpenThread(IThci):
         self.channelMask = "0x7fff800" #(0xffff << 11)
         self.panId = ModuleHelper.Default_PanId
         self.xpanId = ModuleHelper.Default_XpanId
-        self.localprefix = ModuleHelper.Default_MLPrefix
+        self.meshLocalPrefix = ModuleHelper.Default_MLPrefix
         self.pskc = "00000000000000000000000000000000"  # OT only accept hex format PSKc for now
         self.securityPolicySecs = ModuleHelper.Default_SecurityPolicy
         self.securityPolicyFlags = "onrcb"
@@ -1287,7 +1287,7 @@ class OpenThread(IThci):
             self.setXpanId(self.xpanId)
             self.setNetworkName(self.networkName)
             self.setNetworkKey(self.networkKey)
-            self.setMLPrefix(self.localprefix)
+            self.setMLPrefix(self.meshLocalPrefix)
             self.setPSKc(self.pskc)
             self.setActiveTimestamp(self.activetimestamp)
         except Exception, e:

tools/harness-thci/OpenThread_WpanCtl.py
@@ -332,7 +332,7 @@ class OpenThread_WpanCtl(IThci):
                 # link local address
                 if ip6Addr.split(':')[4] != '0':
                     linkLocal64Addr = ip6Addr
-            elif ip6AddrPrefix == 'fd00':
+            elif ip6Addr.startswith(self.meshLocalPrefix):
                 # mesh local address
                 if ip6Addr.split(':')[4] == '0':
                     # rloc
@@ -1451,7 +1451,7 @@ class OpenThread_WpanCtl(IThci):
         self.channelMask = "0x7fff800" #(0xffff << 11)
         self.panId = ModuleHelper.Default_PanId
         self.xpanId = ModuleHelper.Default_XpanId
-        self.localprefix = ModuleHelper.Default_MLPrefix
+        self.meshLocalPrefix = ModuleHelper.Default_MLPrefix
         self.pskc = "00000000000000000000000000000000"  # OT only accept hex format PSKc for now
         self.securityPolicySecs = ModuleHelper.Default_SecurityPolicy
         self.securityPolicyFlags = "onrcb"
@@ -1480,7 +1480,7 @@ class OpenThread_WpanCtl(IThci):
             self.setXpanId(self.xpanId)
             self.setNetworkName(self.networkName)
             self.setNetworkKey(self.networkKey)
-            self.setMLPrefix(self.localprefix)
+            self.setMLPrefix(self.meshLocalPrefix)
             self.setPSKc(self.pskc)
             self.setActiveTimestamp(self.activetimestamp)
         except Exception, e:

commit 225a9937f47cd043ffaaba48196f56ca493af2d3

[thci] update address classifciation
This commit fixes the issue introduced in #3639 as the default value
(`ModuleHelper.Default_MLPrefix` of `self.meshLocalPrefix` should be
uppercase).

tools/harness-thci/OpenThread.py
@@ -248,24 +248,34 @@ class OpenThread(IThci):
         for ip6Addr in addrs:
             if ip6Addr == 'Done':
                 break
+            fullIp = ModuleHelper.GetFullIpv6Address(ip6Addr).lower()
 
-            ip6AddrPrefix = ip6Addr.split(':')[0]
-            if ip6AddrPrefix == 'fe80':
+            print 'fullip ', fullIp
+
+            if fullIp.startswith('fe80'):
                 # link local address
-                if ip6Addr.split(':')[4] != '0':
-                    linkLocal64Addr = ip6Addr
-            elif ip6Addr.startswith(self.meshLocalPrefix):
+                linkLocal64Addr = fullIp
+                print 'link local'
+            elif fullIp.startswith(self.meshLocalPrefix.lower()[0:19]):
                 # mesh local address
-                if ip6Addr.split(':')[4] == '0':
+                print "mesh local"
+                if fullIp.startswith('0000:00ff:fe00:', 20):
                     # rloc
-                    rlocAddr = ip6Addr
+                    if fullIp.startswith('fc', 35):
+                        alocAddr = fullIp
+                        print 'aloc'
+                    else:
+                        rlocAddr = fullIp
+                        print 'rloc'
                 else:
                     # mesh EID
-                    meshEIDAddr = ip6Addr
+                    meshEIDAddr = fullIp
+                    print 'mleid'
             else:
                 # global ipv6 address
-                if ip6Addr != None:
-                    globalAddr.append(ip6Addr)
+                if fullIp != None:
+                    globalAddr.append(fullIp)
+                    print 'global'
                 else:
                     pass
 

tools/harness-thci/OpenThread_WpanCtl.py
@@ -327,25 +327,33 @@ class OpenThread_WpanCtl(IThci):
                 break
             ip6AddrItem = ip6AddrItem.strip()
             ip6Addr = self.__stripValue(ip6AddrItem).split(' ')[0]
-            ip6AddrPrefix = ip6Addr.split(':')[0]
-            if ip6AddrPrefix == 'fe80':
+
+            fullIp = ModuleHelper.GetFullIpv6Address(ip6Addr).lower()
+
+            if fullIp.startswith('fe80'):
                 # link local address
-                if ip6Addr.split(':')[4] != '0':
-                    linkLocal64Addr = ip6Addr
-            elif ip6Addr.startswith(self.meshLocalPrefix):
+                linkLocal64Addr = fullIp
+                print 'link local'
+            elif fullIp.startswith(self.meshLocalPrefix.lower()[0:19]):
                 # mesh local address
-                if ip6Addr.split(':')[4] == '0':
+                print "mesh local"
+                if fullIp.startswith('0000:00ff:fe00:', 20):
                     # rloc
-                    rlocAddr = ip6Addr
+                    if fullIp.startswith('fc', 35):
+                        alocAddr = fullIp
+                        print 'aloc'
+                    else:
+                        rlocAddr = fullIp
+                        print 'rloc'
                 else:
                     # mesh EID
-                    meshEIDAddr = ip6Addr
-                    print 'meshEIDAddr:' + meshEIDAddr
+                    meshEIDAddr = fullIp
+                    print 'mleid'
             else:
                 # global ipv6 address
-                if ip6Addr:
-                    print 'globalAddr: ' + ip6Addr
-                    globalAddr.append(ip6Addr)
+                if fullIp != None:
+                    globalAddr.append(fullIp)
+                    print 'global'
                 else:
                     pass
 