07fdbf6d99c7be6c4a553d00c237a24fdbd70980
Yakun Xu <xyk@google.com>
2019-07-30 12:16:58 +0800

    revert deleting TIME_SYNC check

--- examples/platforms/efr32mg21/openthread-core-efr32-config-check.h
+++ examples/platforms/efr32mg21/openthread-core-efr32-config-check.h
@@ -29,6 +29,10 @@
 #ifndef OPENTHREAD_CORE_EFR32_CONFIG_CHECK_H_
 #define OPENTHREAD_CORE_EFR32_CONFIG_CHECK_H_
 
+#if OPENTHREAD_CONFIG_ENABLE_TIME_SYNC
+#error "Platform efr32mg21 doesn't support configuration option: OPENTHREAD_CONFIG_ENABLE_TIME_SYNC"
+#endif
+
 #if OPENTHREAD_CONFIG_RADIO_915MHZ_OQPSK_SUPPORT
 #error "Platform efr32mg21 doesn't support configuration option: OPENTHREAD_CONFIG_RADIO_915MHZ_OQPSK_SUPPORT"
 #endif
