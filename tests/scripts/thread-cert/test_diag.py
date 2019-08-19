b8ea1a98a942f543ec140e00c3321a59812c601c
Yakun Xu <xyk@google.com>
2019-08-19 17:44:18 +0800

    add invalid command test

--- tests/scripts/thread-cert/test_diag.py
+++ tests/scripts/thread-cert/test_diag.py
@@ -51,6 +51,8 @@ class TestDiag(unittest.TestCase):
              'failed\r\nstatus 0xd\r\n'),
             ('diag start\n',
              'start diagnostics mode\r\nstatus 0x00\r\n'),
+            ('diag invalid test\n',
+             'diag feature \'invalid\' is not supported'),
             ('diag',
              'diagnostics mode is enabled\r\n'),
             ('diag channel 10\n',
