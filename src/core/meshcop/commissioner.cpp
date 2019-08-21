1f8407b30b6b752326685501886b141a5da34c32
Simon Lin <simonlin@google.com>
2019-08-21 11:35:19 +0800

    [commissioner] make start fail early if device role is invalid or detached

--- src/core/meshcop/commissioner.cpp
+++ src/core/meshcop/commissioner.cpp
@@ -144,6 +144,9 @@ otError Commissioner::Start(otCommissionerStateCallback  aStateCallback,
 {
     otError error = OT_ERROR_NONE;
 
+    otDeviceRole deviceRole = Get<Mle::MleRouter>().GetRole();
+    VerifyOrExit(deviceRole != OT_DEVICE_ROLE_DISABLED && deviceRole != OT_DEVICE_ROLE_DETACHED,
+            error = OT_ERROR_INVALID_STATE);
     VerifyOrExit(mState == OT_COMMISSIONER_STATE_DISABLED, error = OT_ERROR_INVALID_STATE);
 
     SuccessOrExit(error = Get<Coap::CoapSecure>().Start(SendRelayTransmit, this));
