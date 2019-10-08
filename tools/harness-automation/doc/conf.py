22c6237630513eeba2b7a3a14c9b926287f3eb16
FiveDimensions <mjing@google.com>
2019-10-08 16:05:27 +0800

    [harness-automation] update to support Test Harness v50.x

--- tools/harness-automation/doc/conf.py
+++ tools/harness-automation/doc/conf.py
@@ -49,7 +49,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
