fcde3ffa18e55d2efe27729129bed31ee96a92d9
FiveDimensions <mjing@google.com>
2019-10-22 12:29:58 +0800

    [harness-automation] save case failure times

--- tools/harness-automation/autothreadharness/runner.py
+++ tools/harness-automation/autothreadharness/runner.py
@@ -106,11 +106,19 @@ class SimpleTestResult(unittest.TestResult):
             test (TestCase): The test just run
             passed (bool): Whether the case is passed
         """
+        if str(test.__class__.__name__) in self.result and 'fails' in self.result[str(test.__class__.__name__)]:
+            fails = int(self.result[str(test.__class__.__name__)]['fails'])
+        else:
+            fails = 0
+        if passed is False:
+            fails += 1
+
         self.result[str(test.__class__.__name__)] = {
             'started': self.started,
             'stopped': time.strftime('%Y-%m-%dT%H:%M:%S'),
             'passed': passed,
             'error': error,
+            'fails': fails,
             'executions': SimpleTestResult.executions,
         }
         if self.auto_reboot_args:
