edc439de0341203debf5262b8219bff67b5c72cb
Yakun Xu <xyk@google.com>
2019-10-25 08:57:55 +0800

    output hex number

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -452,11 +452,11 @@ void Interpreter::ProcessChannel(int argc, char *argv[])
     }
     else if (strcmp(argv[0], "supported") == 0)
     {
-        mServer->OutputFormat("%u\r\n", otPlatRadioGetSupportedChannelMask(mInstance));
+        mServer->OutputFormat("0x%x\r\n", otPlatRadioGetSupportedChannelMask(mInstance));
     }
     else if (strcmp(argv[0], "preferred") == 0)
     {
-        mServer->OutputFormat("%u\r\n", otPlatRadioGetPreferredChannelMask(mInstance));
+        mServer->OutputFormat("0x%x\r\n", otPlatRadioGetPreferredChannelMask(mInstance));
     }
 #if OPENTHREAD_CONFIG_CHANNEL_MONITOR_ENABLE
     else if (strcmp(argv[0], "monitor") == 0)
