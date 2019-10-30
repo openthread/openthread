5fffc45d967e856d63d9883899023e447551f937
FiveDimensions <mjing@google.com>
2019-10-30 19:51:51 +0800

    [harness-automation] save case failure times

--- tools/harness-automation/autothreadharness/runner.py
+++ tools/harness-automation/autothreadharness/runner.py
@@ -106,10 +106,7 @@ class SimpleTestResult(unittest.TestResult):
             test (TestCase): The test just run
             passed (bool): Whether the case is passed
         """
-        if str(test.__class__.__name__) in self.result and 'fails' in self.result[str(test.__class__.__name__)]:
-            fails = int(self.result[str(test.__class__.__name__)]['fails'])
-        else:
-            fails = 0
+        fails = self.result.get(test.__class__.__name__, {}).get('fails', 0)
         if passed is False:
             fails += 1
         self.result[str(test.__class__.__name__)] = {
