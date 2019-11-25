f7569538199ccef5afa671cfd0db7c24515a14d2
Łukasz Duda <lukasz.duda@nordicsemi.no>
2019-11-25 19:44:22 +0100

    [nrf528xx] handle overrun error on high baudrates correctly (#4350)

--- examples/platforms/nrf528xx/src/uart.c
+++ examples/platforms/nrf528xx/src/uart.c
@@ -284,9 +284,7 @@ void UARTE0_UART0_IRQHandler(void)
     // Check if any error has been detected.
     if (nrf_uart_event_check(UART_INSTANCE, NRF_UART_EVENT_ERROR))
     {
-        // Clear error event and ignore erronous byte in RXD register.
         nrf_uart_event_clear(UART_INSTANCE, NRF_UART_EVENT_ERROR);
-        nrf_uart_event_clear(UART_INSTANCE, NRF_UART_EVENT_RXDRDY);
     }
     else if (nrf_uart_event_check(UART_INSTANCE, NRF_UART_EVENT_RXDRDY))
     {
