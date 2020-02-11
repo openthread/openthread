6e51f319e802c1a6a5c983846257bb91aafaee34
Yakun Xu <xyk@google.com>
2020-02-11 17:08:45 +0800

    client.c to client.cpp

--- src/posix/console_cli.h
+++ src/posix/console_cli.h
@@ -35,6 +35,10 @@
 
 #include "openthread-system.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * This function initializes CLI console.
  *
@@ -64,5 +68,9 @@ void otxConsoleUpdate(otSysMainloopContext *aMainloop);
  *
  */
 void otxConsoleProcess(const otSysMainloopContext *aMainloop);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // OPENTHREAD_CONSOLE_CLI_H_
