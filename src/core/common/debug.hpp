40acc14367bd01c0ed999a0e75f129b300a81b2e
Doru Gucea <doru-cristian.gucea@nxp.com>
2020-03-04 13:23:28 +0200

    [jn5189] Guard assert definition
    Guarded assert definition as both OT and NXP drivers define it.
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

--- src/core/common/debug.hpp
+++ src/core/common/debug.hpp
@@ -77,6 +77,7 @@
 #undef assert
 #endif
 
+#ifndef assert
 #define assert(cond)  \
     do                \
     {                 \
@@ -88,6 +89,7 @@
         }             \
     } while (0)
 
+#endif
 #endif
 
 #endif // DEBUG_HPP_
