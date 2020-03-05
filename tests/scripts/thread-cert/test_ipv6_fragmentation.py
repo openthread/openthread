ac7906f1189af00c5d05a5012303d5874c7f57b4
Yakun Xu <xyk@google.com>
2020-03-05 20:04:50 +0800

    add test for frame pending

--- tests/scripts/thread-cert/test_ipv6_fragmentation.py
+++ tests/scripts/thread-cert/test_ipv6_fragmentation.py
@@ -76,18 +76,19 @@ class TestIPv6Fragmentation(unittest.TestCase):
         mleid_router = self.nodes[ROUTER].get_ip6_address(
             config.ADDRESS_TYPE.ML_EID)
 
-        self.nodes[LEADER].udp_start("::", 12345)
-        self.nodes[ROUTER].udp_start("::", 12345)
+        self.nodes[LEADER].udp_start("::", config.UDP_TEST_PORT)
+        self.nodes[ROUTER].udp_start("::", config.UDP_TEST_PORT)
 
-        self.nodes[LEADER].udp_send(1952, mleid_router, 12345)
+        self.nodes[LEADER].udp_send(1952, mleid_router, config.UDP_TEST_PORT)
         self.simulator.go(5)
         self.nodes[ROUTER].udp_check_rx(1952)
 
-        self.nodes[ROUTER].udp_send(1831, mleid_leader, 12345)
+        self.nodes[ROUTER].udp_send(1831, mleid_leader, config.UDP_TEST_PORT)
         self.simulator.go(5)
         self.nodes[LEADER].udp_check_rx(1831)
 
-        self.nodes[ROUTER].udp_send(1953, mleid_leader, 12345, False)
+        self.nodes[ROUTER].udp_send(1953, mleid_leader, config.UDP_TEST_PORT,
+                                    False)
 
         self.nodes[ROUTER].udp_stop()
         self.nodes[LEADER].udp_stop()
