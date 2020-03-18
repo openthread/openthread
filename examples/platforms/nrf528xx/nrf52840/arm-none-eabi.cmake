815aa766cd34c563111a74ba75d0e4cbcba2e27f
FiveDimensions <mjing@google.com>
2020-03-18 11:59:34 +0800

    switch nrf52840 build to cmake

--- examples/platforms/nrf528xx/nrf52840/arm-none-eabi.cmake
+++ examples/platforms/nrf528xx/nrf52840/arm-none-eabi.cmake
@@ -26,25 +26,27 @@
 #  POSSIBILITY OF SUCH DAMAGE.
 #
 
-# Get a list of the available platforms and output as a list to the 'arg_platforms' argument
-function(ot_get_platforms arg_platforms)
-    list(APPEND result "NO" "posix" "external")
-    set(platforms_dir "${PROJECT_SOURCE_DIR}/examples/platforms")
-    file(GLOB platforms RELATIVE "${platforms_dir}" "${platforms_dir}/*")
-    foreach(platform IN LISTS platforms)
-        if(IS_DIRECTORY "${platforms_dir}/${platform}")
-            list(APPEND result "${platform}")
-        endif()
-    endforeach()
-    set(platforms_dir "${PROJECT_SOURCE_DIR}/examples/platforms/nrf528xx")
-    file(GLOB platforms RELATIVE "${platforms_dir}" "${platforms_dir}/nrf*")
-    foreach(platform IN LISTS platforms)
-        if(IS_DIRECTORY "${platforms_dir}/${platform}")
-            list(APPEND result "${platform}")
-        endif()
-    endforeach()
+set(CMAKE_SYSTEM_NAME              Generic)
+set(CMAKE_SYSTEM_PROCESSOR         ARM)
 
-    list(REMOVE_ITEM result utils)
-    list(SORT result)
-    set(${arg_platforms} "${result}" PARENT_SCOPE)
-endfunction()
+set(CMAKE_C_COMPILER               arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER             arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER             arm-none-eabi-as)
+set(CMAKE_RANLIB                   arm-none-eabi-ranlib)
+
+set(COMMON_C_FLAGS                 "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb -mabi=aapcs -fdata-sections -ffunction-sections -Os -g")
+
+set(CMAKE_C_FLAGS                  "${COMMON_C_FLAGS} -std=gnu99 -Wno-pedantic")
+set(CMAKE_CXX_FLAGS                "${COMMON_C_FLAGS} -fno-exceptions -fno-rtti")
+set(CMAKE_ASM_FLAGS                "${COMMON_C_FLAGS} -x assembler-with-cpp")
+set(CMAKE_EXE_LINKER_FLAGS         "${COMMON_C_FLAGS} -specs=nano.specs -specs=nosys.specs -Wl,--gc-sections")
+
+set(CMAKE_C_FLAGS_DEBUG            "-Os -g")
+set(CMAKE_CXX_FLAGS_DEBUG          "-Os -g")
+set(CMAKE_ASM_FLAGS_DEBUG          "-Os -g")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG   "-Os -g")
+
+set(CMAKE_C_FLAGS_RELEASE          "-Os")
+set(CMAKE_CXX_FLAGS_RELEASE        "-Os")
+set(CMAKE_ASM_FLAGS_RELEASE        "")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
