64e03b834741d025aa575f46f692225869031b4d
Jing Ma <mjing@google.com>
2020-04-02 14:53:34 +0800

    [cmake] add support for cc1352 (#4690)

--- examples/platforms/cc1352/arm-none-eabi.cmake
+++ examples/platforms/cc1352/arm-none-eabi.cmake
@@ -1,4 +1,3 @@
-#!/bin/bash
 #
 #  Copyright (c) 2020, The OpenThread Authors.
 #  All rights reserved.
@@ -27,63 +26,27 @@
 #  POSSIBILITY OF SUCH DAMAGE.
 #
 
-set -euxo pipefail
+set(CMAKE_SYSTEM_NAME              Generic)
+set(CMAKE_SYSTEM_PROCESSOR         ARM)
 
-readonly OT_COMMON_OPTIONS=(
-    "-DOT_COMPILE_WARNING_AS_ERROR=ON"
-)
+set(CMAKE_C_COMPILER               arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER             arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER             arm-none-eabi-as)
+set(CMAKE_RANLIB                   arm-none-eabi-ranlib)
 
-reset_source()
-{
-    git checkout -- .
-    git clean -xfd
-}
+set(COMMON_C_FLAGS                 "-mcpu=cortex-m4 -march=armv7e-m -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb -fdata-sections -ffunction-sections")
 
-build_cc1352()
-{
-    local options=("${OT_COMMON_OPTIONS[@]}")
+set(CMAKE_C_FLAGS                  "${COMMON_C_FLAGS} -std=gnu99")
+set(CMAKE_CXX_FLAGS                "${COMMON_C_FLAGS} -fno-exceptions -fno-rtti")
+set(CMAKE_ASM_FLAGS                "${COMMON_C_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS         "${COMMON_C_FLAGS} -nostartfiles -specs=nano.specs -specs=nosys.specs -Wl,--gc-sections -Wl,-Map=map.map")
 
-    options+=(
-        "-DOT_PLATFORM=cc1352"
-        "-DCMAKE_TOOLCHAIN_FILE=examples/platforms/cc1352/arm-none-eabi.cmake"
-        "-DOT_COMMISSIONER=on"
-        "-DOT_DHCP6_CLIENT=on"
-        "-DOT_DHCP6_SERVER=on"
-        "-DOT_DNS_CLIENT=on"
-        "-DOT_JOINER=on"
-        "-DOT_SLAAC=on"
-        "-DCMAKE_BUILD_TYPE=Release"
-    )
+set(CMAKE_C_FLAGS_DEBUG            "-Og -g")
+set(CMAKE_CXX_FLAGS_DEBUG          "-Og -g")
+set(CMAKE_ASM_FLAGS_DEBUG          "-g")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG   "")
 
-    reset_source
-    mkdir build
-    cd build
-    cmake -GNinja "${options[@]}" ..
-    ninja
-    cd ..
-}
-
-build_cc2538()
-{
-    local options=("${OT_COMMON_OPTIONS[@]}")
-
-    options+=(
-        "-DOT_PLATFORM=cc2538"
-        "-DCMAKE_TOOLCHAIN_FILE=examples/platforms/cc2538/arm-none-eabi.cmake"
-    )
-
-    reset_source
-    mkdir build
-    cd build
-    cmake -GNinja "${options[@]}" ..
-    ninja
-    cd ..
-}
-
-main()
-{
-    build_cc1352
-    build_cc2538
-}
-
-main "$@"
+set(CMAKE_C_FLAGS_RELEASE          "-Os")
+set(CMAKE_CXX_FLAGS_RELEASE        "-Os")
+set(CMAKE_ASM_FLAGS_RELEASE        "")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
