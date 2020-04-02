11ce2e3439bff47adf8002873ac79454a67427b7
FiveDimensions <mjing@google.com>
2020-04-02 11:04:52 +0800

    refine, add cc2652 cmake check in check-arm-build-cmake, restore .travis/script.sh

--- .travis/script.sh
+++ .travis/script.sh
@@ -146,24 +146,14 @@ build_cc2650() {
 }
 
 build_cc2652() {
-    CMAKE_FLAGS="                        \
-        -DOT_COMPILE_WARNING_AS_ERROR=on \
-        -DOT_COMMISSIONER=on             \
-        -DOT_DHCP6_CLIENT=on             \
-        -DOT_DHCP6_SERVER=on             \
-        -DOT_DNS_CLIENT=on               \
-        -DOT_JOINER=on                   \
-        -DOT_SLAAC=on"
     git checkout -- . || die
     git clean -xfd || die
-    mkdir build && cd build || die
-    cmake -GNinja -DOT_PLATFORM=cc2652 -DCMAKE_TOOLCHAIN_FILE=examples/platforms/cc2652/arm-none-eabi.cmake ${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=Debug .. || die
-    ninja || die
-    arm-none-eabi-size  examples/apps/cli/ot-cli-ftd || die
-    arm-none-eabi-size  examples/apps/cli/ot-cli-mtd || die
-    arm-none-eabi-size  examples/apps/ncp/ot-ncp-ftd || die
-    arm-none-eabi-size  examples/apps/ncp/ot-ncp-mtd || die
-    cd .. || die
+    ./bootstrap || die
+    COMMISSIONER=1 JOINER=1 SLAAC=1 DHCP6_CLIENT=1 DHCP6_SERVER=1 DNS_CLIENT=1 make -f examples/Makefile-cc2652 || die
+    arm-none-eabi-size  output/cc2652/bin/ot-cli-ftd || die
+    arm-none-eabi-size  output/cc2652/bin/ot-cli-mtd || die
+    arm-none-eabi-size  output/cc2652/bin/ot-ncp-ftd || die
+    arm-none-eabi-size  output/cc2652/bin/ot-ncp-mtd || die
 }
 
 build_kw41z() {
