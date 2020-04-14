cb474f298687db5133debd43616f294d2c01893b
Jonathan Hui <jonhui@google.com>
2020-04-13 22:26:04 -0700

    Apply suggestions from code review

--- src/core/mac/mac_types.hpp
+++ src/core/mac/mac_types.hpp
@@ -540,10 +540,10 @@ public:
     /**
      * This method copies the name data into a given char buffer with a given size.
      *
-     * The given buffer is cleared (`memset` to zero) before copying the Name into it. The copied string
+     * The given buffer is cleared (`memset` to zero) before copying the name into it. The copied string
      * in @p aBuffer is NOT necessarily null terminated.
      *
-     * @param[out] aBuffer   A pointer to a buffer where to copy the Name into.
+     * @param[out] aBuffer   A pointer to a buffer where to copy the name into.
      * @param[in]  aMaxSize  Size of @p aBuffer (maximum number of chars to write into @p aBuffer).
      *
      * @returns The actual number of chars copied into @p aBuffer.
