5d9e80bfe5cdf1c24c97c9c91e66ae44f13d5ce0
Yakun Xu <xyk@google.com>
2020-04-27 13:20:50 +0800

    move --gc-sections

--- examples/platforms/cc1352/arm-none-eabi.cmake
+++ examples/platforms/cc1352/arm-none-eabi.cmake
@@ -39,14 +39,12 @@ set(COMMON_C_FLAGS                 "-mcpu=cortex-m4 -march=armv7e-m -mfloat-abi=
 set(CMAKE_C_FLAGS                  "${COMMON_C_FLAGS} -std=gnu99")
 set(CMAKE_CXX_FLAGS                "${COMMON_C_FLAGS} -fno-exceptions -fno-rtti")
 set(CMAKE_ASM_FLAGS                "${COMMON_C_FLAGS}")
-set(CMAKE_EXE_LINKER_FLAGS         "${COMMON_C_FLAGS} -nostartfiles -specs=nano.specs -specs=nosys.specs -Wl,--gc-sections -Wl,-Map=map.map")
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "${COMMON_C_FLAGS} -nostartfiles -specs=nano.specs -specs=nosys.specs")
 
 set(CMAKE_C_FLAGS_DEBUG            "-Og -g")
 set(CMAKE_CXX_FLAGS_DEBUG          "-Og -g")
 set(CMAKE_ASM_FLAGS_DEBUG          "-g")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG   "")
 
 set(CMAKE_C_FLAGS_RELEASE          "-Os")
 set(CMAKE_CXX_FLAGS_RELEASE        "-Os")
 set(CMAKE_ASM_FLAGS_RELEASE        "")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
