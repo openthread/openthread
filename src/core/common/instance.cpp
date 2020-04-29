dace2407fee40a56aca0757bf6617502d2004255
kangping <wgtdkp@google.com>
2020-04-29 17:39:39 +0800

    [instance] fix multiple instance compilation

--- src/core/common/instance.cpp
+++ src/core/common/instance.cpp
@@ -143,12 +143,12 @@ Instance *Instance::Init(void *aBuffer, size_t *aBufferSize)
 {
     Instance *instance = NULL;
 
-    VerifyOrExit(aBufferSize != NULL);
+    VerifyOrExit(aBufferSize != NULL, OT_NOOP);
 
     // Make sure the input buffer is big enough
     VerifyOrExit(sizeof(Instance) <= *aBufferSize, *aBufferSize = sizeof(Instance));
 
-    VerifyOrExit(aBuffer != NULL);
+    VerifyOrExit(aBuffer != NULL, OT_NOOP);
 
     instance = new (aBuffer) Instance();
 
