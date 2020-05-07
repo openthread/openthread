95496cf9cd551996209b5e8232a2f9c58420ec59
Jiacheng Guo <gjc@google.com>
2020-05-07 18:31:27 +0800

    [posix] add radio url
    Radio url provides a unified interface for users to specify the radio
    peripheral device and arguments to the OpenThread core stack.

--- Android.mk
+++ Android.mk
@@ -281,6 +281,7 @@ LOCAL_SRC_FILES                                          := \
     src/posix/platform/misc.cpp                             \
     src/posix/platform/netif.cpp                            \
     src/posix/platform/radio.cpp                            \
+    src/posix/platform/radio_url.cpp                        \
     src/posix/platform/settings.cpp                         \
     src/posix/platform/spi_interface.cpp                    \
     src/posix/platform/system.cpp                           \
