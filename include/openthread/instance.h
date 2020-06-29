fa6ae9653ebe60c31c3da07f02ae3d94339febd8
Abtin Keshavarzian <abtink@google.com>
2020-06-29 10:30:27 -0700

    [meshcop] fix typos in joiner/commissioner method documentation (#5154)

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (10)
+#define OPENTHREAD_API_VERSION (11)
 
 /**
  * @addtogroup api-instance
