37177edf894746cfa15a866f64f4bebc7c747d82
Yakun Xu <xyk@google.com>
2020-07-01 13:31:50 +0800

    Revert "autostart netif"
    This reverts commit 0d37351ba58244797c50112fa4f899917b33c2a1.

--- src/core/meshcop/joiner.cpp
+++ src/core/meshcop/joiner.cpp
@@ -143,14 +143,9 @@ otError Joiner::Start(const char *     aPskd,
     otLogInfoMeshCoP("Joiner starting");
 
     VerifyOrExit(mState == OT_JOINER_STATE_IDLE, error = OT_ERROR_BUSY);
-
-    if (!Get<ThreadNetif>().IsUp())
-    {
-        Get<ThreadNetif>().Up();
-    }
-
-    // Verify Thread stack is not fully enabled.
-    VerifyOrExit(Get<Mle::Mle>().GetRole() == Mle::kRoleDisabled, error = OT_ERROR_INVALID_STATE);
+    // Verify IPv6 stack is down and Thread stack is not fully enabled.
+    VerifyOrExit(Get<ThreadNetif>().IsUp() && Get<Mle::Mle>().GetRole() == Mle::kRoleDisabled,
+                 error = OT_ERROR_INVALID_STATE);
 
     SuccessOrExit(error = joinerPskd.SetFrom(aPskd));
 
