f574a6a5d101cc63e2dfcb9898192b5d060faf44
Simon Lin <simonlin@google.com>
2020-07-10 14:24:23 +0800

    increase API version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (15)
+#define OPENTHREAD_API_VERSION (14)
 
 /**
  * @addtogroup api-instance
