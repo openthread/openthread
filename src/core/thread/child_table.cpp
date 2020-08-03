55cd8e18a9ab8854f6e34ecb595cd23994fd7560
Abtin Keshavarzian <abtink@google.com>
2020-08-03 10:29:40 -0700

    [child-table] move child related methods to 'ChildTable'
    This commit moves `GetChildInfoById()`, `GetChildInfoByIndex()`, and
    `HasSleepyChildWithAddress()`, along with methods for saving/restoring
    child entries in/from non-volatile settings (e.g., `Restore()`,
    `RemoveStoredChild()`, etc.) from `Mle` module to `ChildTable`. It also
    removes/inlines the `MleRouter::IsSleepyChildSubscribed()` method.

--- src/core/thread/child_table.cpp
+++ src/core/thread/child_table.cpp
@@ -225,6 +225,155 @@ exit:
     return error;
 }
 
+otError ChildTable::GetChildInfoById(uint16_t aChildId, Child::Info &aChildInfo)
+{
+    otError  error = OT_ERROR_NONE;
+    Child *  child;
+    uint16_t rloc16;
+
+    if ((aChildId & ~Mle::kMaxChildId) != 0)
+    {
+        aChildId = Mle::Mle::ChildIdFromRloc16(aChildId);
+    }
+
+    rloc16 = Get<Mac::Mac>().GetShortAddress() | aChildId;
+    child  = FindChild(rloc16, Child::kInStateValidOrRestoring);
+    VerifyOrExit(child != nullptr, error = OT_ERROR_NOT_FOUND);
+
+    aChildInfo.SetFrom(*child);
+
+exit:
+    return error;
+}
+
+otError ChildTable::GetChildInfoByIndex(uint16_t aChildIndex, Child::Info &aChildInfo)
+{
+    otError error = OT_ERROR_NONE;
+    Child * child = nullptr;
+
+    child = GetChildAtIndex(aChildIndex);
+    VerifyOrExit((child != nullptr) && child->IsStateValidOrRestoring(), error = OT_ERROR_NOT_FOUND);
+
+    aChildInfo.SetFrom(*child);
+
+exit:
+    return error;
+}
+
+void ChildTable::Restore(void)
+{
+    otError  error          = OT_ERROR_NONE;
+    bool     foundDuplicate = false;
+    uint16_t numChildren    = 0;
+
+    for (const Settings::ChildInfo &childInfo : Get<Settings>().IterateChildInfo())
+    {
+        Child *child;
+
+        child = FindChild(childInfo.GetExtAddress(), Child::kInStateAnyExceptInvalid);
+
+        if (child == nullptr)
+        {
+            VerifyOrExit((child = GetNewChild()) != nullptr, error = OT_ERROR_NO_BUFS);
+        }
+        else
+        {
+            foundDuplicate = true;
+        }
+
+        child->Clear();
+
+        child->SetExtAddress(childInfo.GetExtAddress());
+        child->GetLinkInfo().Clear();
+        child->SetRloc16(childInfo.GetRloc16());
+        child->SetTimeout(childInfo.GetTimeout());
+        child->SetDeviceMode(Mle::DeviceMode(childInfo.GetMode()));
+        child->SetState(Neighbor::kStateRestored);
+        child->SetLastHeard(TimerMilli::GetNow());
+        child->SetVersion(static_cast<uint8_t>(childInfo.GetVersion()));
+        Get<IndirectSender>().SetChildUseShortAddress(*child, true);
+        numChildren++;
+    }
+
+exit:
+
+    if (foundDuplicate || (numChildren > GetMaxChildren()) || (error != OT_ERROR_NONE))
+    {
+        // If there is any error, e.g., there are more saved children
+        // in non-volatile settings than could be restored or there are
+        // duplicate entries with same extended address, refresh the stored
+        // children info to ensure that the non-volatile settings remain
+        // consistent with the child table.
+
+        RefreshStoredChildren();
+    }
+}
+
+void ChildTable::RemoveStoredChild(const Child &aChild)
+{
+    for (Settings::ChildInfoIterator iter(GetInstance()); !iter.IsDone(); iter++)
+    {
+        if (iter.GetChildInfo().GetRloc16() == aChild.GetRloc16())
+        {
+            IgnoreError(iter.Delete());
+            break;
+        }
+    }
+}
+
+otError ChildTable::StoreChild(const Child &aChild)
+{
+    Settings::ChildInfo childInfo;
+
+    RemoveStoredChild(aChild);
+
+    childInfo.Init();
+    childInfo.SetExtAddress(aChild.GetExtAddress());
+    childInfo.SetTimeout(aChild.GetTimeout());
+    childInfo.SetRloc16(aChild.GetRloc16());
+    childInfo.SetMode(aChild.GetDeviceMode().Get());
+    childInfo.SetVersion(aChild.GetVersion());
+
+    return Get<Settings>().AddChildInfo(childInfo);
+}
+
+void ChildTable::RefreshStoredChildren(void)
+{
+    const Child *child = &mChildren[0];
+
+    SuccessOrExit(Get<Settings>().DeleteAllChildInfo());
+
+    for (uint16_t num = mMaxChildrenAllowed; num != 0; num--, child++)
+    {
+        if (child->IsStateInvalid())
+        {
+            continue;
+        }
+
+        SuccessOrExit(StoreChild(*child));
+    }
+
+exit:
+    return;
+}
+
+bool ChildTable::HasSleepyChildWithAddress(const Ip6::Address &aIp6Address) const
+{
+    bool         hasChild = false;
+    const Child *child    = &mChildren[0];
+
+    for (uint16_t num = mMaxChildrenAllowed; num != 0; num--, child++)
+    {
+        if (child->IsStateValidOrRestoring() && !child->IsRxOnWhenIdle() && child->HasIp6Address(aIp6Address))
+        {
+            hasChild = true;
+            break;
+        }
+    }
+
+    return hasChild;
+}
+
 #endif // OPENTHREAD_FTD
 
 } // namespace ot
