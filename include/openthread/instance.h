659cf55a5a2d3517419da1f1b4a2fe15e1625262
Jonathan Hui <jonhui@google.com>
2020-08-05 21:06:15 -0700

    [coap] reduce OT_COAP_MAX_RETRANSMIT to 20 (#5338)
    Also add a note to clarify the set of valid CoAP transmission
    parameters.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (21)
+#define OPENTHREAD_API_VERSION (22)
 
 /**
  * @addtogroup api-instance
