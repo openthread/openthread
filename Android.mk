abd444202f8e941c426e790d3d7c74e7bd86f318
Moandor-y <reimu@google.com>
2020-08-18 18:15:37 +0800

    move to OPENTHREAD_PROJECT_CFLAGS

--- Android.mk
+++ Android.mk
@@ -30,12 +30,14 @@ LOCAL_PATH := $(call my-dir)
 
 OPENTHREAD_DEFAULT_VERSION := $(shell cat $(LOCAL_PATH)/.default-version)
 OPENTHREAD_SOURCE_VERSION := $(shell git -C $(LOCAL_PATH) describe --always --match "[0-9].*" 2> /dev/null)
-OPENTHREAD_PROJECT_CFLAGS ?= -DOPENTHREAD_PROJECT_CORE_CONFIG_FILE=\"openthread-core-posix-config.h\"
-OPENTHREAD_PROJECT_CONFIG_FILE ?= \<openthread-config-android.h\>
+
+OPENTHREAD_PROJECT_CFLAGS                                                 ?= \
+    -DOPENTHREAD_PROJECT_CORE_CONFIG_FILE=\"openthread-core-posix-config.h\" \
+    -DOPENTHREAD_CONFIG_FILE=\<openthread-config-android.h\>                 \
+    $(NULL)
 
 OPENTHREAD_PUBLIC_CFLAGS                                         := \
     -DOPENTHREAD_CONFIG_COMMISSIONER_ENABLE=1                       \
-    -DOPENTHREAD_CONFIG_FILE=$(OPENTHREAD_PROJECT_CONFIG_FILE)      \
     -DOPENTHREAD_CONFIG_LOG_LEVEL_DYNAMIC_ENABLE=1                  \
     -DOPENTHREAD_CONFIG_MAC_FILTER_ENABLE=1                         \
     -DOPENTHREAD_POSIX_CONFIG_RCP_PTY_ENABLE=1                      \
@@ -499,8 +501,10 @@ LOCAL_CPPFLAGS                                                              := \
     -pedantic-errors                                                           \
     $(NULL)
 
-LOCAL_CFLAGS                                                := \
-    -DOPENTHREAD_CONFIG_FILE=$(OPENTHREAD_PROJECT_CONFIG_FILE) \
+LOCAL_CFLAGS                                                                := \
+    $(OPENTHREAD_PUBLIC_CFLAGS)                                                \
+    $(OPENTHREAD_PRIVATE_CFLAGS)                                               \
+    $(OPENTHREAD_PROJECT_CFLAGS)                                               \
     $(NULL)
 
 LOCAL_C_INCLUDES                                         := \
