8705e91cde67660a8b4faf0654bb63d3fe0ac3ba
Li Cao <irvingcl@google.com>
2020-09-14 09:34:31 +0800

    [thread-cert] update as review suggests

--- tests/scripts/thread-cert/v1_2_LowPower_5_3_01_SSEDAttachment.py
+++ tests/scripts/thread-cert/v1_2_LowPower_5_3_01_SSEDAttachment.py
@@ -30,6 +30,7 @@
 import unittest
 
 from config import ADDRESS_TYPE
+from mac802154 import MacHeader
 from pktverify import consts
 from pktverify.packet_verifier import PacketVerifier
 
@@ -38,9 +39,6 @@ import thread_cert
 LEADER = 1
 ROUTER = 2
 SSED_1 = 3
-
-# 0.5s
-CSL_PERIOD = 3125
 
 
 class LowPower_5_3_01_SSEDAttachment(thread_cert.TestCase):
@@ -58,14 +56,14 @@ class LowPower_5_3_01_SSEDAttachment(thread_cert.TestCase):
             'mode': 'rsdn',
             'panid': 0xface,
             'router_selection_jitter': 1,
-            'whitelist': [LEADER],
+            'allowlist': [LEADER],
         },
         SSED_1: {
             'version': '1.2',
             'name': 'SSED_1',
             'mode': 's',
             'panid': 0xface,
-            'whitelist': [LEADER],
+            'allowlist': [LEADER],
         },
     }
 
@@ -78,56 +76,70 @@ class LowPower_5_3_01_SSEDAttachment(thread_cert.TestCase):
         self.simulator.go(5)
         self.assertEqual(self.nodes[ROUTER].get_state(), 'router')
 
-        self.nodes[SSED_1].set_csl_period(CSL_PERIOD)
+        self.nodes[SSED_1].set_csl_period(consts.CSL_DEFAULT_PERIOD)
         self.nodes[SSED_1].start()
         self.simulator.go(5)
         self.assertEqual(self.nodes[SSED_1].get_state(), 'child')
 
         self.simulator.go(5)
 
+        self.collect_rloc16s()
+        self.collect_rlocs()
+
         # Verify connectivity by sending an ICMP Echo Request from the Router to SSED_1 mesh-local address via the DUT
         # Set ping timeout as two CSL periods.
-        timeout = 2 * CSL_PERIOD * 160 / 1000000
+        timeout = 2 * consts.CSL_DEFAULT_PERIOD_IN_SECOND
         self.assertTrue(self.nodes[ROUTER].ping(self.nodes[SSED_1].get_ip6_address(ADDRESS_TYPE.RLOC),
                                                 timeout=timeout))
 
         # Verify fragmented CSL transmission
         self.assertTrue(self.nodes[ROUTER].ping(self.nodes[SSED_1].get_ip6_address(ADDRESS_TYPE.RLOC),
                                                 size=128,
-                                                timeout=timeout))
+                                                timeout=timeout * 2))
 
     def verify(self, pv):
         pkts = pv.pkts
         pv.summary.show()
-        LEADER = self.vars['LEADER']
-        ROUTER = self.vars['ROUTER']
-        SSED_1 = self.vars['SSED_1']
+        LEADER = pv.vars['LEADER']
+        ROUTER = pv.vars['ROUTER']
+        SSED_1 = pv.vars['SSED_1']
 
         # Step 1: ensure the Leader is sending MLE Advertisements
         pkts.filter_wpan_src64(LEADER) \
-            .filter_mle_cmd(pvconsts.MLE_ADVERTISEMENT) \
+            .filter_mle_cmd(consts.MLE_ADVERTISEMENT) \
             .must_next()
 
         # Step 2: Verify that the frame version is IEEE Std 802.15.4-2015 of the MLE Child Update Response from the
         # DUT to SSED_1
         pkts.filter_wpan_src64(LEADER) \
-            .filter_mle_cmd(pvconsts.MLE_CHILD_UPDATE_RESPONSE) \
-            .filter(lambda p: p.wpan.version == 2) \
+            .filter_wpan_dst64(SSED_1) \
+            .filter_mle_cmd(consts.MLE_CHILD_UPDATE_RESPONSE) \
+            .filter_wpan_version(consts.MAC_FRAME_VERSION_2015) \
             .must_next()
 
         # Step 3:
         # Verify that SSED_1 does not send a MAC Data Request prior to receiving the ICMP Echo Request from the Leader.
         # Verify that the frame version is IEEE Std 802.15.4-2015 of the ICMP Echo Request sent to SSED_1 from the DUT
         # Verify that the Router successfully receives an Echo Response from SSED_1
+        ssed_rloc16 = pv.vars['SSED_1_RLOC16']
+        ssed_rloc = pv.vars['SSED_1_RLOC']
+        leader_rloc16 = pv.vars['LEADER_RLOC16']
+        router_rloc16 = pv.vars['ROUTER_RLOC16']
+        router_rloc = pv.vars['ROUTER_RLOC']
         pkts.filter_wpan_src64(LEADER) \
+            .filter('wpan.dst16 == {rloc16}', rloc16 = ssed_rloc16) \
+            .filter_ipv6_dst(ssed_rloc) \
             .filter_ping_request() \
-            .filter(lambda p : p.wpan.version == 2) \
+            .filter_wpan_version(consts.MAC_FRAME_VERSION_2015) \
             .must_next()
         idx1 = pkts.index
         pkts.filter_wpan_src64(SSED_1) \
+            .filter('wpan.dst16 == {rloc16}', rloc16 = leader_rloc16) \
+            .filter_ipv6_dst(router_rloc) \
             .filter_ping_reply() \
-            .filter(lambda p : p.wpan.version == 2) \
-            .must_next()
+            .filter_wpan_version(consts.MAC_FRAME_VERSION_2015) \
+            .must_next() \
+            .must_verify(lambda p: (p.wpan.ie_present == 1) and (consts.CSL_IE_ID in p.wpan.header_ie.id))
         idx2 = pkts.index
         pkts.range(idx1, idx2).filter(lambda p: p.wpan.cmd == MacHeader.CommandIdentifier.DATA_REQUEST) \
             .must_not_next()
