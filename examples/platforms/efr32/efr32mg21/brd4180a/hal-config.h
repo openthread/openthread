f8ae23264a761f413ae292dff53066b39f1f60ea
Mason Tran <Mason.Tran@silabs.com>
2020-10-14 17:21:58 -0400

    OPENTHREAD-465: Part1 of regenerated hal-config.h. These changes have no functional effect

--- examples/platforms/efr32/efr32mg21/brd4180a/hal-config.h
+++ examples/platforms/efr32/efr32mg21/brd4180a/hal-config.h
@@ -14,40 +14,30 @@
 // $[ACMP1]
 // [ACMP1]$
 
-// $[ADC0]
-// [ADC0]$
-
 // $[ANTDIV]
 // [ANTDIV]$
-
-// $[BATTERYMON]
-// [BATTERYMON]$
 
 // $[BTL_BUTTON]
 // [BTL_BUTTON]$
 
-// $[BULBPWM]
-// [BULBPWM]$
 
-// $[BULBPWM_COLOR]
-// [BULBPWM_COLOR]$
 
 // $[BUTTON]
 // [BUTTON]$
 
 // $[CMU]
 #define HAL_CLK_HFCLK_SOURCE (HAL_CLK_HFCLK_SOURCE_HFXO)
-#define BSP_CLK_LFXO_PRESENT (1)
-#define HAL_CLK_EM4CLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
 #define HAL_CLK_EM23CLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
-#define BSP_CLK_HFXO_PRESENT (1)
-#define BSP_CLK_LFXO_INIT CMU_LFXOINIT_DEFAULT
-#define BSP_CLK_LFXO_CTUNE (79U)
+#define HAL_CLK_EM4CLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
 #define HAL_CLK_RTCCCLK_SOURCE (HAL_CLK_LFCLK_SOURCE_LFRCO)
-#define BSP_CLK_LFXO_FREQ (32768U)
+#define BSP_CLK_HFXO_PRESENT (1)
 #define BSP_CLK_HFXO_FREQ (38400000UL)
-#define BSP_CLK_HFXO_CTUNE (129)
 #define BSP_CLK_HFXO_INIT CMU_HFXOINIT_DEFAULT
+#define BSP_CLK_HFXO_CTUNE (129)
+#define BSP_CLK_LFXO_PRESENT (1)
+#define BSP_CLK_LFXO_INIT CMU_LFXOINIT_DEFAULT
+#define BSP_CLK_LFXO_FREQ (32768U)
+#define BSP_CLK_LFXO_CTUNE (79U)
 // [CMU]$
 
 // $[COEX]
@@ -61,6 +51,9 @@
 
 // $[EZRADIOPRO]
 // [EZRADIOPRO]$
+
+// $[FEM]
+// [FEM]$
 
 // $[GPIO]
 #define PORTIO_GPIO_SWV_PIN (3U)
@@ -95,17 +88,17 @@
 #define BSP_LED1_PIN (1U)
 #define BSP_LED1_PORT (gpioPortB)
 
-#define HAL_LED_ENABLE \
-    {                  \
-        0, 1           \
-    }
-#define BSP_LED_POLARITY (1)
-#define HAL_LED_COUNT (2U)
 #define BSP_LED_COUNT (2U)
 #define BSP_LED_INIT                                                   \
     {                                                                  \
         {BSP_LED0_PORT, BSP_LED0_PIN}, { BSP_LED1_PORT, BSP_LED1_PIN } \
     }
+#define HAL_LED_COUNT (2U)
+#define HAL_LED_ENABLE \
+    {                  \
+        0, 1           \
+    }
+#define BSP_LED_POLARITY (1)
 // [LED]$
 
 // $[LETIMER0]
@@ -120,11 +113,11 @@
 // $[PA]
 #define HAL_PA_ENABLE (1)
 
-#define HAL_PA_RAMP (10UL)
-#define HAL_PA_SELECTION (HAL_PA_SELECTION_2P4_HP)
-#define HAL_PA_POWER (252U)
-#define HAL_PA_VOLTAGE (3300U)
 #define HAL_PA_CURVE_HEADER "pa_curves_efr32.h"
+#define HAL_PA_POWER (252U)
+#define HAL_PA_RAMP (10UL)
+#define BSP_PA_VOLTAGE (3300U)
+#define HAL_PA_SELECTION (HAL_PA_SELECTION_2P4_HP)
 // [PA]$
 
 // $[PORTIO]
@@ -159,14 +152,15 @@
 #define HAL_SERIAL_USART2_ENABLE (0)
 #define HAL_SERIAL_USART3_ENABLE (0)
 #define HAL_SERIAL_RXWAKE_ENABLE (0)
-#define BSP_SERIAL_APP_CTS_PIN (4U)
-#define BSP_SERIAL_APP_CTS_PORT (gpioPortA)
+
+#define BSP_SERIAL_APP_TX_PIN (5U)
+#define BSP_SERIAL_APP_TX_PORT (gpioPortA)
 
 #define BSP_SERIAL_APP_RX_PIN (6U)
 #define BSP_SERIAL_APP_RX_PORT (gpioPortA)
 
-#define BSP_SERIAL_APP_TX_PIN (5U)
-#define BSP_SERIAL_APP_TX_PORT (gpioPortA)
+#define BSP_SERIAL_APP_CTS_PIN (4U)
+#define BSP_SERIAL_APP_CTS_PORT (gpioPortA)
 
 #define BSP_SERIAL_APP_RTS_PIN (1U)
 #define BSP_SERIAL_APP_RTS_PORT (gpioPortC)
@@ -213,26 +207,26 @@
 #define PORTIO_USART0_TX_PIN (5U)
 #define PORTIO_USART0_TX_PORT (gpioPortA)
 
-#define HAL_USART0_ENABLE (1)
+#define BSP_USART0_TX_PIN (5U)
+#define BSP_USART0_TX_PORT (gpioPortA)
 
-#define BSP_USART0_CTS_PIN (4U)
-#define BSP_USART0_CTS_PORT (gpioPortA)
+#define HAL_USART0_ENABLE (1)
 
 #define BSP_USART0_RX_PIN (6U)
 #define BSP_USART0_RX_PORT (gpioPortA)
 
-#define BSP_USART0_TX_PIN (5U)
-#define BSP_USART0_TX_PORT (gpioPortA)
+#define BSP_USART0_CTS_PIN (4U)
+#define BSP_USART0_CTS_PORT (gpioPortA)
 
 #define BSP_USART0_RTS_PIN (1U)
 #define BSP_USART0_RTS_PORT (gpioPortC)
 
-#define HAL_USART0_RX_QUEUE_SIZE (128UL)
 #define HAL_USART0_BAUD_RATE (115200UL)
+#define HAL_USART0_FLOW_CONTROL (HAL_USART_FLOW_CONTROL_HWUART)
 #define HAL_USART0_RXSTOP (16UL)
 #define HAL_USART0_RXSTART (16UL)
 #define HAL_USART0_TX_QUEUE_SIZE (128UL)
-#define HAL_USART0_FLOW_CONTROL (HAL_USART_FLOW_CONTROL_HWUART)
+#define HAL_USART0_RX_QUEUE_SIZE (128UL)
 // [USART0]$
 
 // $[USART1]
