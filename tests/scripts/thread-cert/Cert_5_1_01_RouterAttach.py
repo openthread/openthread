b2ab5e61d14dd9f424f10644577b18ce59084ddd
FiveDimensions <mjing@google.com>
2020-10-22 10:00:10 +0800

    [thread-cert] refactor case 5.1.1 using pktverify

--- tests/scripts/thread-cert/Cert_5_1_01_RouterAttach.py
+++ tests/scripts/thread-cert/Cert_5_1_01_RouterAttach.py
@@ -32,19 +32,28 @@ import unittest
 import mle
 import network_layer
 import thread_cert
+from pktverify.consts import MLE_ADVERTISEMENT, MLE_PARENT_REQUEST, MLE_PARENT_RESPONSE, MLE_CHILD_UPDATE_RESPONSE, MLE_CHILD_ID_REQUEST, MLE_CHILD_ID_RESPONSE, MLE_LINK_REQUEST, MLE_LINK_ACCEPT_AND_REQUEST, ADDR_SOL_URI, SOURCE_ADDRESS_TLV, MODE_TLV, TIMEOUT_TLV, CHALLENGE_TLV, RESPONSE_TLV, LINK_LAYER_FRAME_COUNTER_TLV, MLE_FRAME_COUNTER_TLV, ROUTE64_TLV, ADDRESS16_TLV, LEADER_DATA_TLV, NETWORK_DATA_TLV, TLV_REQUEST_TLV, SCAN_MASK_TLV, CONNECTIVITY_TLV, LINK_MARGIN_TLV, VERSION_TLV, ADDRESS_REGISTRATION_TLV, NL_MAC_EXTENDED_ADDRESS_TLV, NL_RLOC16_TLV, NL_STATUS_TLV, NL_ROUTER_MASK_TLV, COAP_CODE_ACK
+from pktverify.packet_verifier import PacketVerifier
 
 LEADER = 1
 ROUTER = 2
+
+# Test Purpose and Description:
+# -----------------------------
+# The purpose of this test case is to show that the Leader is able to form
+# a network and the Router attaches to it with proper steps.
 
 
 class Cert_5_1_01_RouterAttach(thread_cert.TestCase):
     TOPOLOGY = {
         LEADER: {
+            'name': 'LEADER',
             'mode': 'rdn',
             'panid': 0xface,
             'allowlist': [ROUTER]
         },
         ROUTER: {
+            'name': 'ROUTER',
             'mode': 'rdn',
             'panid': 0xface,
             'router_selection_jitter': 1,
@@ -60,6 +69,8 @@ class Cert_5_1_01_RouterAttach(thread_cert.TestCase):
         self.nodes[ROUTER].start()
         self.simulator.go(7)
         self.assertEqual(self.nodes[ROUTER].get_state(), 'router')
+
+        self.collect_rloc16s()
 
         leader_messages = self.simulator.get_messages_sent_by(LEADER)
         router_messages = self.simulator.get_messages_sent_by(ROUTER)
@@ -167,6 +178,317 @@ class Cert_5_1_01_RouterAttach(thread_cert.TestCase):
         # 11 - Leader, Router1
         for addr in self.nodes[LEADER].get_addrs():
             self.assertTrue(self.nodes[ROUTER].ping(addr))
+
+    def verify(self, pv):
+        pkts = pv.pkts
+        pv.summary.show()
+
+        LEADER = pv.vars['LEADER']
+        LEADER_RLOC16 = pv.vars['LEADER_RLOC16']
+        ROUTER = pv.vars['ROUTER']
+
+        # Step 1: Leader is sending properly formatted MLE Advertisements.
+        #         Advertisements MUST be sent with an IP hop limit of 255 to
+        #         the Link-Local All Nodes multicast address (FF02::1).
+        #         The following TLVs MUST be present in the MLE Advertisements:
+        #             - Leader Data TLV
+        #             - Route64 TLV
+        #             - Source Address TLV
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_LLANMA().\
+            filter_mle_cmd(MLE_ADVERTISEMENT).\
+            filter(lambda p: {\
+                              LEADER_DATA_TLV,\
+                              ROUTE64_TLV,\
+                              SOURCE_ADDRESS_TLV\
+                              } <= set(p.mle.tlv.type) and\
+                   p.ipv6.hlim == 255).\
+            must_next()
+
+        # Step 2: Router sends a MLE Parent Request with an IP hop limit of
+        #         255 to the Link-Local All Routers multicast address (FF02::2).
+        #         The following TLVs MUST be present in the MLE Parent Request:
+        #            - Challenge TLV
+        #            - Mode TLV
+        #            - Scan Mask TLV
+        #                If the DUT sends multiple MLE Parent Requests
+        #                    - The first one MUST be sent only to all Routers
+        #                    - Subsequent ones MAY be sent to all Routers and REEDS
+        #            -  Version TLV
+        #         If the first MLE Parent Request was sent to all Routers and
+        #         REEDS, the test fails.
+
+        pkts.filter_wpan_src64(ROUTER).\
+            filter_LLARMA().\
+            filter_mle_cmd(MLE_PARENT_REQUEST).\
+            filter(lambda p: {\
+                              CHALLENGE_TLV,\
+                              MODE_TLV,\
+                              SCAN_MASK_TLV,\
+                              VERSION_TLV\
+                              } <= set(p.mle.tlv.type) and\
+                   p.ipv6.hlim == 255 and\
+                   p.mle.tlv.scan_mask.r == 1 and\
+                   p.mle.tlv.scan_mask.e == 0).\
+            must_next()
+        pkts_copy = pkts.copy()
+        start_index = pkts.index
+
+        # Step 3: Leader responds with a MLE Parent Response.
+        #         The following TLVs MUST be present in the MLE Parent Response:
+        #             - Challenge TLV
+        #             - Connectivity TLV
+        #             - Leader Data TLV
+        #             - Link-layer Frame Counter TLV
+        #             - Link Margin TLV
+        #             - Response TLV
+        #             - Source Address
+        #             - Version TLV
+        #             - MLE Frame Counter TLV (optional)
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_wpan_dst64(ROUTER).\
+            filter_mle_cmd(MLE_PARENT_RESPONSE).\
+            filter(lambda p: {\
+                              CHALLENGE_TLV,\
+                              CONNECTIVITY_TLV,\
+                              LEADER_DATA_TLV,\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              LINK_MARGIN_TLV,\
+                              RESPONSE_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              VERSION_TLV,\
+                              MLE_FRAME_COUNTER_TLV\
+                              } <= set(p.mle.tlv.type) or\
+                             {\
+                              CHALLENGE_TLV,\
+                              CONNECTIVITY_TLV,\
+                              LEADER_DATA_TLV,\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              LINK_MARGIN_TLV,\
+                              RESPONSE_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              VERSION_TLV\
+                               } <= set(p.mle.tlv.type)).\
+                   must_next()
+        end_index = pkts.index
+
+        # verify sub step in Step 2 : Subsequent ones MAY be sent to all Routers and REEDS
+
+        _pkt = pkts_copy.range(start_index, end_index).\
+            filter_wpan_src64(ROUTER).\
+            filter_LLARMA().\
+            filter_mle_cmd(MLE_PARENT_REQUEST).\
+            filter(lambda p: {\
+                              CHALLENGE_TLV,\
+                              MODE_TLV,\
+                              SCAN_MASK_TLV,\
+                              VERSION_TLV\
+                              } <= set(p.mle.tlv.type) and\
+                   p.ipv6.hlim == 255)\
+            .next()
+
+        if _pkt is not None:
+            _pkt.must_verify(lambda p:
+                    p.mle.tlv.scan_mask.r == 1 and\
+                    p.mle.tlv.scan_mask.e == 1)
+
+        # Step 4: Router sends a MLE Child ID Request.
+        #         The following TLVs MUST be present in the MLE Child ID Request:
+        #             - Link-layer Frame Counter TLV
+        #             - Mode TLV
+        #             - Response TLV
+        #             - Timeout TLV
+        #             - TLV Request TLV
+        #             - Address16 TLV
+        #             - Network Data TLV
+        #             - Route64 TLV (optional)
+        #             - Version TLV
+        #             - MLE Frame Counter TLV (optional)
+        #         The following TLV MUST NOT be present in the MLE Child ID Request:
+        #             - Address Registration TLV
+
+        _pkt = pkts.filter_wpan_src64(ROUTER).\
+            filter_wpan_dst64(LEADER).\
+            filter_mle_cmd(MLE_CHILD_ID_REQUEST).\
+            filter(lambda p: {\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              MODE_TLV,\
+                              RESPONSE_TLV,\
+                              TIMEOUT_TLV,\
+                              TLV_REQUEST_TLV,\
+                              ADDRESS16_TLV,\
+                              NETWORK_DATA_TLV,\
+                              ROUTE64_TLV,\
+                              VERSION_TLV,\
+                              MLE_FRAME_COUNTER_TLV\
+                              } <= set(p.mle.tlv.type) or\
+                             {\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              MODE_TLV,\
+                              RESPONSE_TLV,\
+                              TIMEOUT_TLV,\
+                              TLV_REQUEST_TLV,\
+                              ADDRESS16_TLV,\
+                              NETWORK_DATA_TLV,\
+                              VERSION_TLV\
+                               } <= set(p.mle.tlv.type)).\
+                   must_next()
+        _pkt.must_not_verify(lambda p: (ADDRESS_REGISTRATION_TLV) in p.mle.tlv.type)
+
+        # Step 5: Leader responds with a Child ID Response.
+        #         The following TLVs MUST be present in the Child ID Response:
+        #             - Address16 TLV
+        #             - Leader Data TLV
+        #             - Network Data TLV
+        #             - Source Address TLV
+        #             - Route64 TLV (if requested)
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_wpan_dst64(ROUTER).\
+            filter_mle_cmd(MLE_CHILD_ID_RESPONSE).\
+            filter(lambda p: {\
+                              ADDRESS16_TLV,\
+                              LEADER_DATA_TLV,\
+                              NETWORK_DATA_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              ROUTE64_TLV\
+                              } <= set(p.mle.tlv.type) or\
+                             {\
+                              ADDRESS16_TLV,\
+                              LEADER_DATA_TLV,\
+                              NETWORK_DATA_TLV,\
+                              SOURCE_ADDRESS_TLV\
+                               } <= set(p.mle.tlv.type)).\
+                   must_next()
+
+        # Step 6: Router sends an Address Solicit Request.
+        #         Ensure the Address Solicit Request is properly formatted:
+        #         CoAP Request URI
+        #             coap://<leader address>:MM/a/as
+        #         CoAP Payload
+        #             - MAC Extended Address TLV
+        #             - Status TLV
+
+        _pkt = pkts.filter_wpan_src64(ROUTER).\
+            filter_wpan_dst16(LEADER_RLOC16).\
+            filter_coap_request(ADDR_SOL_URI).\
+            filter(lambda p: {\
+                              NL_MAC_EXTENDED_ADDRESS_TLV,
+                              NL_STATUS_TLV\
+                              } == set(p.coap.tlv.type)\
+                   ).\
+           must_next()
+
+        # Step 7: Leader sends an Address Solicit Response.
+        #         Ensure the Address Solicit Response is properly formatted:
+        #         CoAP Response Code
+        #             2.04 Changed
+        #         CoAP Payload
+        #             - Status TLV (value = Success)
+        #             - RLOC16 TLV
+        #             - Router Mask TLV
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_wpan_dst16(_pkt.wpan.src16).\
+            filter_coap_ack(ADDR_SOL_URI).\
+            filter(lambda p: {\
+                              NL_STATUS_TLV,\
+                              NL_RLOC16_TLV,\
+                              NL_ROUTER_MASK_TLV\
+                              } == set(p.coap.tlv.type) and\
+                   p.coap.code == COAP_CODE_ACK and\
+                   p.thread_address.tlv.status == 0\
+                   ).\
+            must_next()
+
+        # Step 8: Router Sends a Link Request Message.
+        #         The Link Request Message MUST be multicast and contain
+        #         the following TLVs:
+        #             - Challenge TLV
+        #             - Leader Data TLV
+        #             - Source Address TLV
+        #             - Version TLV
+        #             - TLV Request TLV: Link Margin
+
+        pkts.filter_wpan_src64(ROUTER).\
+            filter_LLARMA().\
+            filter_mle_cmd(MLE_LINK_REQUEST).\
+            filter(lambda p: {\
+                              CHALLENGE_TLV,\
+                              LEADER_DATA_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              VERSION_TLV,\
+                              TLV_REQUEST_TLV,\
+                              LINK_MARGIN_TLV\
+                              } <= set(p.mle.tlv.type)\
+                   ).\
+            must_next()
+
+        # Step 9: Leader sends a Unicast Link Accept and Request Message.
+        #         The Message MUST be unicast to Router
+        #         The Message MUST contain the following TLVs:
+        #             - Leader Data TLV
+        #             - Link-layer Frame Counter TLV
+        #             - Link Margin TLV
+        #             - Response TLV
+        #             - Source Address TLV
+        #             - Version TLV
+        #             - Challenge TLV (optional)
+        #             - MLE Frame Counter TLV (optional)
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_wpan_dst64(ROUTER).\
+            filter_mle_cmd(MLE_LINK_ACCEPT_AND_REQUEST).\
+            filter(lambda p: {\
+                              LEADER_DATA_TLV,\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              LINK_MARGIN_TLV,\
+                              RESPONSE_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              VERSION_TLV,\
+                              CHALLENGE_TLV,\
+                              MLE_FRAME_COUNTER_TLV\
+                              } <= set(p.mle.tlv.type) or\
+                             {\
+                              LEADER_DATA_TLV,\
+                              LINK_LAYER_FRAME_COUNTER_TLV,\
+                              LINK_MARGIN_TLV,\
+                              RESPONSE_TLV,\
+                              SOURCE_ADDRESS_TLV,\
+                              VERSION_TLV\
+                               } <= set(p.mle.tlv.type)).\
+                   must_next()
+
+        # Step 10: Router is sending properly formatted MLE Advertisements.
+        #          MLE Advertisements MUST be sent with an IP Hop Limit of
+        #          255 to the Link-Local All Nodes multicast address (FF02::1).
+        #          The following TLVs MUST be present in the MLE Advertisements:
+        #              - Leader Data TLV
+        #              - Route64 TLV
+        #              - Source Address TLV
+
+        pkts.filter_wpan_src64(LEADER).\
+            filter_LLANMA().\
+            filter_mle_cmd(MLE_ADVERTISEMENT).\
+            filter(lambda p: {\
+                              LEADER_DATA_TLV,\
+                              ROUTE64_TLV,\
+                              SOURCE_ADDRESS_TLV\
+                              } == set(p.mle.tlv.type) and\
+                   p.ipv6.hlim == 255).\
+            must_next()
+
+        # Step 11: DUT responds with ICMPv6 Echo Reply
+        _pkt = pkts.filter_ping_request().\
+            filter_wpan_src64(ROUTER).\
+            filter_wpan_dst64(LEADER).\
+            must_next()
+        pkts.filter_ping_reply(identifier=_pkt.icmpv6.echo.identifier).\
+            filter_wpan_src64(LEADER).\
+            filter_wpan_dst64(ROUTER).\
+            must_next()
 
 
 if __name__ == '__main__':
