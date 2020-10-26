0c331a6fa77fa2ae3ff242ed7e372de81a6074a1
kangping <wgtdkp@google.com>
2020-10-26 15:54:38 +0800

    update API version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (41)
+#define OPENTHREAD_API_VERSION (42)
 
 /**
  * @addtogroup api-instance
