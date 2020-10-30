80e0494731d22fba0572cd1594940a028ea4b80b
Simon Lin <simonlin@google.com>
2020-10-30 14:11:16 +0800

    Update tests/scripts/thread-cert/wpan.py

--- tests/scripts/thread-cert/wpan.py
+++ tests/scripts/thread-cert/wpan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#  Copyright (c) 2019, The OpenThread Authors.
+#  Copyright (c) 2020, The OpenThread Authors.
 #  All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
