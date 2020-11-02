230184ebac6c695f1698c28132f83ea9286b8a7c
FiveDimensions <mjing@google.com>
2020-11-02 11:58:02 +0800

    [thread-cert] refactor case 5.1.10 using pktverify

--- tests/scripts/thread-cert/Cert_5_1_10_RouterAttachLinkQuality.py
+++ tests/scripts/thread-cert/Cert_5_1_10_RouterAttachLinkQuality.py
@@ -31,33 +31,59 @@ import unittest
 
 import mle
 import thread_cert
+from pktverify.consts import MLE_ADVERTISEMENT, MLE_PARENT_REQUEST, MLE_PARENT_RESPONSE, MLE_CHILD_ID_REQUEST, SOURCE_ADDRESS_TLV, MODE_TLV, TIMEOUT_TLV, CHALLENGE_TLV, RESPONSE_TLV, LINK_LAYER_FRAME_COUNTER_TLV, ADDRESS16_TLV, LEADER_DATA_TLV, NETWORK_DATA_TLV, TLV_REQUEST_TLV, SCAN_MASK_TLV, CONNECTIVITY_TLV, LINK_MARGIN_TLV, VERSION_TLV, ADDRESS_REGISTRATION_TLV
+from pktverify.packet_verifier import PacketVerifier
+from pktverify.null_field import nullField
 
 LEADER = 1
 ROUTER1 = 2
 ROUTER2 = 3
 ROUTER3 = 4
 
+# Test Purpose and Description:
+# -----------------------------
+# The purpose of this test case is to validate that the DUT
+# will choose a router with better link quality as its parent.
+#
+# Test Topology:
+# -------------
+#       Leader
+#       /  \
+# ROUTER2   ROUTER1
+#       \  /(better link_quality)
+#      Router3[DUT]
+#
+# DUT Types:
+# ----------
+#  Router
+
 
 class Cert_5_1_10_RouterAttachLinkQuality(thread_cert.TestCase):
+    USE_MESSAGE_FACTORY = False
+
     TOPOLOGY = {
         LEADER: {
+            'name': 'LEADER',
             'mode': 'rdn',
             'panid': 0xface,
             'allowlist': [ROUTER1, ROUTER2]
         },
         ROUTER1: {
+            'name': 'ROUTER_1',
             'mode': 'rdn',
             'panid': 0xface,
             'router_selection_jitter': 1,
             'allowlist': [LEADER, ROUTER3]
         },
         ROUTER2: {
+            'name': 'ROUTER_2',
             'mode': 'rdn',
             'panid': 0xface,
             'router_selection_jitter': 1,
             'allowlist': [LEADER, (ROUTER3, -85)]
         },
         ROUTER3: {
+            'name': 'ROUTER_3',
             'mode': 'rdn',
             'panid': 0xface,
             'router_selection_jitter': 1,
@@ -82,66 +108,112 @@ class Cert_5_1_10_RouterAttachLinkQuality(thread_cert.TestCase):
         self.simulator.go(5)
         self.assertEqual(self.nodes[ROUTER3].get_state(), 'router')
 
-        leader_messages = self.simulator.get_messages_sent_by(LEADER)
-        router1_messages = self.simulator.get_messages_sent_by(ROUTER1)
-        router2_messages = self.simulator.get_messages_sent_by(ROUTER2)
-        router3_messages = self.simulator.get_messages_sent_by(ROUTER3)
+    def verify(self, pv):
+        pkts = pv.pkts
+        pv.summary.show()
 
-        # 1 - Leader, Router1, Router2
-        leader_messages.next_mle_message(mle.CommandType.ADVERTISEMENT)
+        LEADER = pv.vars['LEADER']
+        ROUTER_1 = pv.vars['ROUTER_1']
+        ROUTER_2 = pv.vars['ROUTER_2']
+        ROUTER_3 = pv.vars['ROUTER_3']
 
-        router1_messages.next_mle_message(mle.CommandType.PARENT_REQUEST)
-        leader_messages.next_mle_message(mle.CommandType.PARENT_RESPONSE)
+        # Step 1: Verify ROUTER_1, Router2 and Leader are sending MLE advertisements.
+        pkts.filter_wpan_src64(LEADER).\
+                   filter_mle_cmd(MLE_ADVERTISEMENT).\
+                   must_next()
+        pv.verify_attached('ROUTER_1')
+        pv.verify_attached('ROUTER_2')
 
-        router1_messages.next_mle_message(mle.CommandType.CHILD_ID_REQUEST)
-        leader_messages.next_mle_message(mle.CommandType.CHILD_ID_RESPONSE)
+        # Step 3: The DUT sends a MLE Parent Request with an IP hop limit of
+        #         255 to the Link-Local All Routers multicast address (FF02::2).
+        #         The following TLVs MUST be present in the MLE Parent Request:
+        #            - Challenge TLV
+        #            - Mode TLV
+        #            - Scan Mask TLV
+        #                If the DUT sends multiple MLE Parent Requests
+        #                    - The first one MUST be sent only to all Routers
+        #                    - Subsequent ones MAY be sent to all Routers and REEDS
+        #            -  Version TLV
 
-        msg = router1_messages.next_coap_message("0.02")
-        msg.assertCoapMessageRequestUriPath("/a/as")
+        pkts.filter_wpan_src64(ROUTER_3).\
+            filter_LLARMA().\
+            filter_mle_cmd(MLE_PARENT_REQUEST).\
+            filter(lambda p: {
+                              CHALLENGE_TLV,
+                              MODE_TLV,
+                              SCAN_MASK_TLV,
+                              VERSION_TLV
+                              } <= set(p.mle.tlv.type) and\
+                   p.ipv6.hlim == 255 and\
+                   p.mle.tlv.scan_mask.r == 1 and\
+                   p.mle.tlv.scan_mask.e == 0
+                   ).\
+            must_next()
 
-        msg = leader_messages.next_coap_message("2.04")
+        # Step 4: ROUTER_1 and ROUTER_2 respond with a MLE Parent Response.
+        #         The following TLVs MUST be present in the MLE Parent Response:
+        #             - Challenge TLV
+        #             - Connectivity TLV
+        #             - Leader Data TLV
+        #             - Link-layer Frame Counter TLV
+        #             - Link Margin TLV
+        #             - Response TLV
+        #             - Source Address
+        #             - Version TLV
+        #             - MLE Frame Counter TLV (optional)
 
-        router2_messages.next_mle_message(mle.CommandType.PARENT_REQUEST)
-        leader_messages.next_mle_message(mle.CommandType.PARENT_RESPONSE)
+        for i in range(1, 3):
+            with pkts.save_index():
+                pkts.filter_wpan_src64(pv.vars['ROUTER_%d' % i]).\
+                    filter_wpan_dst64(ROUTER_3).\
+                    filter_mle_cmd(MLE_PARENT_RESPONSE).\
+                    filter(lambda p: {
+                                      CHALLENGE_TLV,
+                                      CONNECTIVITY_TLV,
+                                      LEADER_DATA_TLV,
+                                      LINK_LAYER_FRAME_COUNTER_TLV,
+                                      LINK_MARGIN_TLV,
+                                      RESPONSE_TLV,
+                                      SOURCE_ADDRESS_TLV,
+                                      VERSION_TLV
+                                    } <= set(p.mle.tlv.type)
+                           ).\
+                    must_next()
 
-        router2_messages.next_mle_message(mle.CommandType.CHILD_ID_REQUEST)
-        leader_messages.next_mle_message(mle.CommandType.CHILD_ID_RESPONSE)
+        # Step 5: The DUT MUST initiate the attach process with Router_1 by
+        #         sending an MLE Child ID Request; if not, the test fails.
+        #         The following TLVs MUST be present in the MLE Child ID Request:
+        #             - Link-layer Frame Counter TLV
+        #             - Mode TLV
+        #             - Response TLV
+        #             - Timeout TLV
+        #             - TLV Request TLV
+        #                 - Address16 TLV
+        #                 - Network Data TLV
+        #                 - Route64 TLV (optional)
+        #             - Version TLV
+        #             - MLE Frame Counter TLV (optional)
+        #         The following TLV MUST NOT be present in the MLE Child ID Request:
+        #             - Address Registration TLV
 
-        msg = router2_messages.next_coap_message("0.02")
-        msg.assertCoapMessageRequestUriPath("/a/as")
-
-        msg = leader_messages.next_coap_message("2.04")
-
-        router1_messages.next_mle_message(mle.CommandType.ADVERTISEMENT)
-        router2_messages.next_mle_message(mle.CommandType.ADVERTISEMENT)
-
-        # 3 - Router3
-        msg = router3_messages.next_mle_message(mle.CommandType.PARENT_REQUEST)
-        msg.assertSentWithHopLimit(255)
-        msg.assertSentToDestinationAddress("ff02::2")
-        msg.assertMleMessageContainsTlv(mle.Mode)
-        msg.assertMleMessageContainsTlv(mle.Challenge)
-        msg.assertMleMessageContainsTlv(mle.ScanMask)
-        msg.assertMleMessageContainsTlv(mle.Version)
-
-        # 4 - Router1, Router2
-        msg = router1_messages.next_mle_message(mle.CommandType.PARENT_RESPONSE)
-        msg.assertSentToNode(self.nodes[ROUTER3])
-
-        msg = router2_messages.next_mle_message(mle.CommandType.PARENT_RESPONSE)
-        msg.assertSentToNode(self.nodes[ROUTER3])
-
-        # 5 - Router3
-        msg = router3_messages.next_mle_message(mle.CommandType.CHILD_ID_REQUEST)
-        msg.assertSentToNode(self.nodes[ROUTER1])
-        msg.assertMleMessageContainsTlv(mle.Response)
-        msg.assertMleMessageContainsTlv(mle.LinkLayerFrameCounter)
-        msg.assertMleMessageContainsOptionalTlv(mle.MleFrameCounter)
-        msg.assertMleMessageContainsTlv(mle.Mode)
-        msg.assertMleMessageContainsTlv(mle.Timeout)
-        msg.assertMleMessageContainsTlv(mle.Version)
-        msg.assertMleMessageContainsTlv(mle.TlvRequest)
-        msg.assertMleMessageDoesNotContainTlv(mle.AddressRegistration)
+        _pkt = pkts.filter_wpan_src64(ROUTER_3).\
+            filter_wpan_dst64(ROUTER_1).\
+            filter_mle_cmd(MLE_CHILD_ID_REQUEST).\
+            filter(lambda p: {
+                              LINK_LAYER_FRAME_COUNTER_TLV,
+                              MODE_TLV,
+                              RESPONSE_TLV,
+                              TIMEOUT_TLV,
+                              TLV_REQUEST_TLV,
+                              ADDRESS16_TLV,
+                              NETWORK_DATA_TLV,
+                              VERSION_TLV
+                    } <= set(p.mle.tlv.type) and\
+                   p.mle.tlv.addr16 is nullField and\
+                   p.thread_nwd.tlv.type is nullField
+                   ).\
+            must_next()
+        _pkt.must_not_verify(lambda p: (ADDRESS_REGISTRATION_TLV) in p.mle.tlv.type)
 
 
 if __name__ == '__main__':
