8b6c1b84821f0558d6d4e125f0584c1e5c07f607
Jintao Lin <jintaolin@google.com>
2020-11-24 08:33:57 +0800

    address review comments

--- include/openthread/thread_ftd.h
+++ include/openthread/thread_ftd.h
@@ -67,7 +67,7 @@ typedef struct
     int8_t       mLastRssi;             ///< Last observed RSSI
     uint16_t     mFrameErrorRate;       ///< Frame error rate (0xffff->100%). Requires error tracking feature.
     uint16_t     mMessageErrorRate;     ///< (IPv6) msg error rate (0xffff->100%). Requires error tracking feature.
-    uint16_t     mQueuedMessageCnt;     ///< Number of queued indirect messages for the child.
+    uint16_t     mQueuedMessageCnt;     ///< Number of queued messages for the child.
     uint8_t      mVersion;              ///< MLE version
     bool         mRxOnWhenIdle : 1;     ///< rx-on-when-idle
     bool         mFullThreadDevice : 1; ///< Full Thread Device
