0750e4b1d888a90d9a328c50d9016acdff23703d
Jonathan Hui <jonhui@google.com>
2020-12-08 22:24:52 -0800

    [meshcop-tlvs] fix ChannelMaskBaseTlv::IsValid()

--- src/core/meshcop/meshcop_tlvs.cpp
+++ src/core/meshcop/meshcop_tlvs.cpp
@@ -189,7 +189,7 @@ bool ChannelMaskBaseTlv::IsValid(void) const
 
     VerifyOrExit(entry != nullptr);
 
-    while (reinterpret_cast<const uint8_t *>(entry) < end)
+    while (reinterpret_cast<const uint8_t *>(entry) + sizeof(ChannelMaskEntryBase) <= end)
     {
         entry = entry->GetNext();
         VerifyOrExit(reinterpret_cast<const uint8_t *>(entry) <= end);
