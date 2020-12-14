142f36345b450ccfcca6945ba1e840b7c0f45cc8
Jing Ma <mjing@google.com>
2020-12-14 12:42:01 +0800

    [thread-cert] add case 9.2.19 PendingDatasetGet using pktverify

--- tests/scripts/thread-cert/pktverify/layer_fields.py
+++ tests/scripts/thread-cert/pktverify/layer_fields.py
@@ -594,6 +594,7 @@ _LAYER_FIELDS = {
     'thread_meshcop.tlv.udp_port': _list(_auto),
     'thread_meshcop.tlv.ba_locator': _auto,
     'thread_meshcop.tlv.active_tstamp': _auto,
+    'thread_meshcop.tlv.delay_timer': _auto,
     'thread_meshcop.tlv.ipv6_addr': _list(_ipv6_addr),
 
     # THREAD NWD
