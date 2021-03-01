9265876f1532f268b5c535553d960ebfc195b491
Eduardo Montoya <edmont@users.noreply.github.com>
2021-03-01 17:02:59 +0100

    [harness-thci] remove `pylint` issues from THCI file (#6219)
    Remove some `pylint` issue and fix `ValidateDeviceFirmware` method.

--- tools/harness-thci/OpenThread.py
+++ tools/harness-thci/OpenThread.py
@@ -2827,7 +2827,7 @@ class OpenThreadTHCI(object):
     @API
     def ValidateDeviceFirmware(self):
         print('%s call ValidateDeviceFirmware' % self)
-        return 'OPENTHREAD' in self.UIStatusMsge
+        return 'OPENTHREAD' in self.UIStatusMsg
 
     # Low power THCI
     @API
@@ -2859,7 +2859,8 @@ class OpenThreadTHCI(object):
         print(cmd)
         return self.__executeCommand(cmd)[-1] == 'Done'
 
-    def getForwardSeriesFlagsFromHexStr(self, flags):
+    @staticmethod
+    def getForwardSeriesFlagsFromHexStr(flags):
         hexFlags = int(flags, 16)
         strFlags = ''
         if hexFlags == 0:
@@ -2876,7 +2877,8 @@ class OpenThreadTHCI(object):
 
         return strFlags
 
-    def mapMetricsHexToChar(self, metrics):
+    @staticmethod
+    def mapMetricsHexToChar(metrics):
         metricsFlagMap = {
             0x40: 'p',
             0x09: 'q',
@@ -2885,21 +2887,22 @@ class OpenThreadTHCI(object):
         }
         return metricsFlagMap.get(metrics, '?')
 
-    def getMetricsFlagsFromHexStr(self, metrics):
+    @staticmethod
+    def getMetricsFlagsFromHexStr(metrics):
         if metrics.startswith('0x'):
             metrics = metrics[2:]
         hexMetricsArray = bytearray.fromhex(metrics)
 
         strMetrics = ''
         for metric in hexMetricsArray:
-            strMetrics += mapMetricsHexToChar(metric)
+            strMetrics += OpenThreadTHCI.mapMetricsHexToChar(metric)
 
         return strMetrics
 
     @API
     def LinkMetricsSingleReq(self, dst_addr, metrics):
         self.log('call LinkMetricsSingleReq')
-        cmd = 'linkmetrics query %s single %s' % (dst_addr, getMetricsFlagsFromHexStr(metrics))
+        cmd = 'linkmetrics query %s single %s' % (dst_addr, self.getMetricsFlagsFromHexStr(metrics))
         print(cmd)
         return self.__executeCommand(cmd)[-1] == 'Done'
 
@@ -2908,14 +2911,14 @@ class OpenThreadTHCI(object):
         self.log('call LinkMetricsMgmtReq')
         cmd = 'linkmetrics mgmt %s ' % dst_addr
         if type_ == 'FWD':
-            cmd += 'forward %d %s' % (series_id, getForwardSeriesFlagsFromHexStr(flags))
+            cmd += 'forward %d %s' % (series_id, self.getForwardSeriesFlagsFromHexStr(flags))
             if flags != 0:
-                cmd += ' %s' % (getMetricsFlagsFromHexStr(metrics))
+                cmd += ' %s' % (self.getMetricsFlagsFromHexStr(metrics))
         elif type_ == 'ENH':
             cmd += 'enhanced-ack'
             if flags != 0:
                 cmd += ' register'
-                metricsFlags = getMetricsFlagsFromHexStr(metrics)
+                metricsFlags = self.getMetricsFlagsFromHexStr(metrics)
                 if '?' in metricsFlags:
                     cmd += ' %s r' % metricsFlags.replace('?', '')
                 else:
@@ -2963,7 +2966,7 @@ class OpenThreadTHCI(object):
         print(cmd1)
         cmd2 = 'udp send %s %d %s' % (destination, port, payload)
         print(cmd2)
-        return (self.__executeCommand(cmd1)[-1] == 'Done' and self.__executeCommand(cmd2)[-1] == 'Done')
+        return self.__executeCommand(cmd1)[-1] == 'Done' and self.__executeCommand(cmd2)[-1] == 'Done'
 
     @API
     def send_udp(self, interface, destination, port, payload='12ABcd'):
@@ -2975,7 +2978,7 @@ class OpenThreadTHCI(object):
         print(cmd1)
         cmd2 = 'udp send %s %s -x %s' % (destination, port, payload)
         print(cmd2)
-        return (self.__executeCommand(cmd1)[-1] == 'Done' and self.__executeCommand(cmd2)[-1] == 'Done')
+        return self.__executeCommand(cmd1)[-1] == 'Done' and self.__executeCommand(cmd2)[-1] == 'Done'
 
     @API
     def sendMACcmd(self, enh=False):
