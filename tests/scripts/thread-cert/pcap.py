8e8dee06d9dd31e014c02abaeac292abe56be2c8
Jintao Lin <jintaolin@google.com>
2021-03-12 17:20:25 +0800

    [tests] add radio metadata in sniffer log using TAP TLVs

--- tests/scripts/thread-cert/pcap.py
+++ tests/scripts/thread-cert/pcap.py
@@ -31,7 +31,7 @@
 import struct
 import time
 
-DLT_IEEE802_15_4 = 195
+DLT_IEEE802_15_4_TAP = 283
 PCAP_MAGIC_NUMBER = 0xA1B2C3D4
 PCAP_VERSION_MAJOR = 2
 PCAP_VERSION_MINOR = 4
@@ -54,14 +54,19 @@ class PcapCodec(object):
             0,
             0,
             256,
-            DLT_IEEE802_15_4,
+            DLT_IEEE802_15_4_TAP,
         )
 
     def encode_frame(self, frame, sec, usec):
         """ Returns a pcap encapsulation of the given frame. """
+        pcap_tap_fcs_tlv = struct.pack("<HHL", 0, 1, 1)
+        pcap_tap_channel_tlv = struct.pack("<HHHH", 3, 3, frame[0], 0)
+        length = 4 + len(pcap_tap_fcs_tlv) + len(pcap_tap_channel_tlv)
+        pcap_tap_header = struct.pack("<HH", 0, length)
         frame = frame[1:]
-        length = len(frame)
+        length += len(frame)
         pcap_frame = struct.pack("<LLLL", sec, usec, length, length)
+        pcap_frame += pcap_tap_header + pcap_tap_fcs_tlv + pcap_tap_channel_tlv
         pcap_frame += frame
         return pcap_frame
 
