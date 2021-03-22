9f2060e188ac7d068e49b5e4adf5a83bd6c78c87
gavin <gavin@machinelake.com>
2021-03-22 12:07:08 -0700

    adding missing doxygen tags

--- include/openthread/ping_sender.h
+++ include/openthread/ping_sender.h
@@ -46,6 +46,16 @@ extern "C" {
 #endif
 
 /**
+ * @addtogroup api-ping-sender
+ *
+ * @brief
+ *   This file includes the OpenThread API for the ping sender module.
+ *
+ * @{
+ *
+ */  
+  
+/**
  * This structure represents a ping reply.
  *
  */
