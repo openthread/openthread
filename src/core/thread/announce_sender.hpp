e1be28cb3be21d0ce650603decca741e2d25c7ee
Abtin Keshavarzian <abtink@google.com>
2021-03-29 13:22:00 -0700

    [announce-sender] use trickle to limit number of announce tx
    This commit updates and enhances the `AnnounceSender` feature. When
    this feature is enabled, routers and REEDs periodically send MLE
    Announcements (on all channels from the channel mask in the current
    Active Operational Dataset). This commit updates the implementation to
    use `TrickleTimer` such that a device monitors the number of received
    Announce messages within a cycle and if the number is above a
    redundancy threshold, the device skips sending Announcement in that
    cycle itself. This helps limit the number of Announcement tx in cycle
    in a dense network. The same mechanism is used by both routers and
    REEDs.

--- src/core/thread/announce_sender.hpp
+++ src/core/thread/announce_sender.hpp
@@ -40,6 +40,7 @@
 #include "common/non_copyable.hpp"
 #include "common/notifier.hpp"
 #include "common/timer.hpp"
+#include "common/trickle_timer.hpp"
 #include "mac/mac.hpp"
 
 namespace ot {
@@ -53,6 +54,15 @@ namespace ot {
 class AnnounceSenderBase : public InstanceLocator, private NonCopyable
 {
 protected:
+    enum : uint8_t
+    {
+        /**
+         * This constant defines the special channel value to start from the first channel in the channel mask.
+         *
+         */
+        kChannelIteratorFirst = Mac::ChannelMask::kChannelIteratorFirst,
+    };
+
     /**
      * This constructor initializes the object.
      *
@@ -65,28 +75,34 @@ protected:
     /**
      * This method schedules the MLE Announce transmissions.
      *
-     * This method schedules `aCount` MLE Announcement transmission cycles. Each cycle covers all the channel in
-     * the `aChannelMask`, with `aPeriod` time interval between any two successive MLE Announcement transmissions
-     * (possibly) on different channels from the given mask. The `aJitter` can be used to add a random jitter
-     * of `[-aJitter, aJitter]` to `aPeriod` interval. A zero value for `aCount` indicates non-stop MLE Announcement
-     * transmission cycles.
+     * This method requests @p aCount additional MLE transmission cycles to be scheduled. Each cycle covers all the
+     * channels in the specified channel mask from `GetChannelMask()`, with `GetPeriod()` as the time interval between
+     * any two successive MLE Announcement transmissions (possibly) on different channels from the mask. The
+     * `GetJitter()` value is used to add a random interval from `[-jitter, jitter]` to each period interval.
      *
-     * @param[in]  aChannelMask   The channel mask providing the list of channels to use for transmission.
-     * @param[in]  aCount         The number of transmissions per channel. Zero indicates non-stop transmissions.
-     * @param[in]  aPeriod        The time between two successive MLE Announce transmissions (in milliseconds).
-     * @param[in]  aJitter        Maximum random jitter added to @aPeriod per transmission (in milliseconds).
+     * If a "starting channel" is specified using `SetStartingChannel()`, then the cycle starts with MLE Announce
+     * transmission on the given starting channel. Otherwise the cycle starts with the first channel (with smallest
+     * channel number) in the mask.
+     *
+     * If a previously requested MLE Announce transmission is still ongoing, a subsequent call to this method adds
+     * the @p aCount cycles to the schedule.
+     *
+     * If there is no active MLE Announce transmission, after call to `SendAnnounce()`, the first MLE Announce
+     * transmission happens within a short random interval selected from range `[0, jitter]`.
+     *
+     * @param[in]  aCount     The number of cycles to schedule.
      *
      */
-    void SendAnnounce(Mac::ChannelMask aChannelMask, uint8_t aCount, uint32_t aPeriod, uint16_t aJitter);
+    void SendAnnounce(uint8_t aCount);
 
     /**
      * This method stops the ongoing MLE Announce transmissions.
      *
      */
-    void Stop(void) { mTimer.Stop(); }
+    void Stop(void);
 
     /**
-     * This method indicates whether the latest scheduled MLE Announce transmission is currently in progress or is
+     * This method indicates whether a previously scheduled MLE Announce transmission is currently in progress or is
      * finished.
      *
      * @returns TRUE if the MLE Announce transmission is in progress, FALSE otherwise.
@@ -95,22 +111,75 @@ protected:
     bool IsRunning(void) const { return mTimer.IsRunning(); }
 
     /**
-     * This method gets the period for the latest scheduled MLE Announce transmission (the one in progress or the last
-     * finished one).
+     * This method gets the period interval.
      *
-     * @returns The period interval (in milliseconds) between two successive MLE Announcement transmissions.
+     * @returns The period interval (in milliseconds).
      *
      */
     uint32_t GetPeriod(void) const { return mPeriod; }
 
     /**
-     * This method gets the channel mask for the latest scheduled MLE Announce transmission (the one in progress or the
-     * last finished one).
+     * This method sets the period interval.
      *
-     * @returns A constant reference to channel mask
+     * The period along with jitter value from (`Get/SetJitter()`) determines the interval between two successive MLE
+     * Announcement transmissions (possibly) on different channels from the specified channel mask.
+     *
+     * @param[in] aPeriod   The period interval (in milliseconds).
+     *
+     */
+    void SetPeriod(uint32_t aPeriod) { mPeriod = aPeriod; }
+
+    /**
+     * This method gets the current jitter interval.
+     *
+     * @returns The jitter interval (in milliseconds).
+     *
+     */
+    uint16_t GetJitter(void) const { return mJitter; }
+
+    /**
+     * This method sets the jitter interval.
+     *
+     * @param[in] aJitter  The jitter interval (in milliseconds).
+     *
+     */
+    void SetJitter(uint16_t aJitter) { mJitter = aJitter; }
+
+    /**
+     * This method gets the channel mask.
+     *
+     * @returns The channel mask.
      *
      */
     const Mac::ChannelMask GetChannelMask(void) const { return mChannelMask; }
+
+    /**
+     * This method sets the channel mask.
+     *
+     * @param[in] aChannelMask   The channel mask.
+     *
+     */
+    void SetChannelMask(Mac::ChannelMask aChannelMask);
+
+    /**
+     * This method gets the starting channel, i.e., the first channel in a TX cycle to send MLE Announcement on.
+     *
+     * @returns The current starting channel.
+     *
+     */
+    uint8_t GetStartingChannel(void) const { return mStartingChannel; }
+
+    /**
+     * This method sets the starting channel, i.e., the first channel in a TX cycle to send MLE Announcement on.
+     *
+     * @p aStartingChannel MUST be present in the current channel mask (from `GetChannelMask()`), otherwise it is
+     * ignored and an MLE transmission cycle starts with the first channel (with smallest channel number) in the channel
+     * mask.
+     *
+     * @param[in] aStartingChannel  The starting channel.
+     *
+     */
+    void SetStartingChannel(uint8_t aStartingChannel);
 
     /**
      * This method is the timer handler and must be invoked by sub-class when the timer expires from the `aHandler`
@@ -120,11 +189,14 @@ protected:
     void HandleTimer(void);
 
 private:
+    void SelectStartingChannel(void);
+
     Mac::ChannelMask mChannelMask;
     uint32_t         mPeriod;
     uint16_t         mJitter;
     uint8_t          mCount;
     uint8_t          mChannel;
+    uint8_t          mStartingChannel;
     TimerMilli       mTimer;
 };
 
@@ -147,19 +219,56 @@ public:
      */
     AnnounceSender(Instance &aInstance);
 
+    /**
+     * This method notifies the `AnnounceSender` that a MLE Announcement message was received with a current timestamp
+     * to update its internal state (decide whether or not to skip transmission of MLE Announcement in this cycle).
+     *
+     */
+    void UpdateOnReceivedAnnounce(void);
+
 private:
-    enum
+    enum : uint32_t
     {
-        kRouterTxInterval = OPENTHREAD_CONFIG_ANNOUNCE_SENDER_INTERVAL_ROUTER,
-        kReedTxInterval   = OPENTHREAD_CONFIG_ANNOUNCE_SENDER_INTERVAL_REED,
-        kMinTxPeriod      = 1000, // in ms
-        kMaxJitter        = 500,  // in ms
+        // Specifies the time interval (in milliseconds) between
+        // `AnnounceSender` transmit cycles. With in a cycle, device
+        // sends MLE Announcements on all channels from Active
+        // Dataset's channel mask.
+        kInterval = OPENTHREAD_CONFIG_ANNOUNCE_SENDER_INTERVAL,
+
+        // Specifies the sub-interval (in millisecond) within a cycle
+        // to send MLE announcement messages. We use half of the cycle
+        // interval length for transmission. This ensures that the
+        // transmissions are finished before device needs to make a
+        // decision about the next cycle. This time is divided by the
+        // number of channels to determine the time between two
+        // successive Announce tx (on different channels).
+        kTxInterval = (kInterval / 2 + 1),
     };
 
-    void        CheckState(void);
+    enum : uint16_t
+    {
+        // Specifies the number of MLE Announcement messages that the
+        // device must receive within a cycle interval to skip sending the
+        // Announcement itself. This is used as `mTrickleTimer` redundancy
+        // constant in `AnnounceSender`.
+        kRedundancyConstant = OPENTHREAD_CONFIG_ANNOUNCE_SENDER_REDUNDANCY_CONSTANT,
+
+        // Jitter interval (in milliseconds) applied to the period
+        // between any two successive MLE Announcement transmissions
+        // (possibly) on different channels.
+        kMaxJitter = OPENTHREAD_CONFIG_ANNOUNCE_SENDER_JITTER_INTERVAL,
+    };
+
     void        Stop(void);
     static void HandleTimer(Timer &aTimer);
+    static void HandleTrickleTimer(TrickleTimer &aTimer);
+    void        HandleTrickleTimer(void);
     void        HandleNotifierEvents(Events aEvents);
+    void        HandleRoleChanged(void);
+    void        HandleActiveDatasetChanged(void);
+    void        HandleThreadChannelChanged(void);
+
+    TrickleTimer mTrickleTimer;
 };
 
 #endif // OPENTHREAD_CONFIG_ANNOUNCE_SENDER_ENABLE
