61556eaf088b35dda0e102693bc661b44e8c48d4
Abtin Keshavarzian <abtink@google.com>
2021-04-14 13:15:00 -0700

    [radio] update docs on frame counter when `RADIO_CAPS_TRANSMIT_SEC`
    This commit updates documentations in `radio.h` and adds more detail
    on expected behavior on assigning of the frame counter (on retx or
    first attempt frame) when radio provides `OT_RADIO_CAPS_TRANSMIT_SEC`
    capability (i.e., radio platform layer handles the tx security and
    frame counter management).

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (100)
+#define OPENTHREAD_API_VERSION (101)
 
 /**
  * @addtogroup api-instance
