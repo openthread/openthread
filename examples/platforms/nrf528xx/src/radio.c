openthread/openthread

commit a9d43fac5da39cc108cb3b57eb7df835db611c80

[nrf528xx] set key id and frame counter for retransmissions (#6427)
For nRF52 platforms in order to achieve the best CSL accuracy frame
security processing was moved to the nrf_802154_tx_started() radio
driver callback which is called from the interrupt context when frame
transmission is started by the hardware peripheral.

There is a possibility that a requested transmission will be aborted
before nrf_802154_tx_started() is called. The frame can be then
retransmitted and if nrf_802154_tx_started() is called it will skip
setting key index and frame counter (so they will remain unchanged
with a value equal to whatever is present in RAM).

It seems that we should always set these fields. Moreover, security is
processed only once which is controlled by
mTxInfo.mIsSecurityProcessed member so checking for retransmission is
redundant.

examples/platforms/nrf528xx/src/radio.c
@@ -1222,11 +1222,8 @@ void nrf_802154_tx_started(const uint8_t *aFrame)
 
     sTransmitFrame.mInfo.mTxInfo.mAesKey = &sCurrKey;
 
-    if (!sTransmitFrame.mInfo.mTxInfo.mIsARetx)
-    {
-        otMacFrameSetKeyId(&sTransmitFrame, sKeyId);
-        otMacFrameSetFrameCounter(&sTransmitFrame, sMacFrameCounter++);
-    }
+    otMacFrameSetKeyId(&sTransmitFrame, sKeyId);
+    otMacFrameSetFrameCounter(&sTransmitFrame, sMacFrameCounter++);
 
     processSecurity = true;
 #endif // OPENTHREAD_CONFIG_THREAD_VERSION >= OT_THREAD_VERSION_1_2

commit 23bbde5c96cba146351349c692c8ad3337bf7fac

[nrf528xx] set frame counter and key idx in otPlatRadioTransmit() (#6458)
This change tackles both problems resolved and introduced by #6427.

For direct transmissions, if the first frame transmission fails,
e.g. due to a CCA failure then retransmissions will contain an invalid
frame counter. It causes problems especially for long IP packets - the
receiver drops a fragment but the transmitter will flood it with the
next fragments since it is not aware that the other party dropped a
fragment. We have identified that it's a root cause of the problem
reported in #6337.

Previous solution (remove the check if a frame is retransmitted) may
cause problems with indirect transmissions where radio frames are
created from scratch and then their frame counter would be
overridden. That is why the best place for setting frame counter and
key index in otPlatRadioTransmit().

examples/platforms/nrf528xx/src/radio.c
@@ -509,6 +509,12 @@ otError otPlatRadioTransmit(otInstance *aInstance, otRadioFrame *aFrame)
     }
 
 #if OPENTHREAD_CONFIG_THREAD_VERSION >= OT_THREAD_VERSION_1_2
+    if (otMacFrameIsSecurityEnabled(aFrame) && otMacFrameIsKeyIdMode1(aFrame) && !aFrame->mInfo.mTxInfo.mIsARetx)
+    {
+        otMacFrameSetKeyId(aFrame, sKeyId);
+        otMacFrameSetFrameCounter(aFrame, sMacFrameCounter++);
+    }
+
     if (aFrame->mInfo.mTxInfo.mTxDelay != 0)
     {
         if (!nrf_802154_transmit_raw_at(&aFrame->mPsdu[-1], true, aFrame->mInfo.mTxInfo.mTxDelayBaseTime,
@@ -1221,9 +1227,6 @@ void nrf_802154_tx_started(const uint8_t *aFrame)
              !sTransmitFrame.mInfo.mTxInfo.mIsSecurityProcessed);
 
     sTransmitFrame.mInfo.mTxInfo.mAesKey = &sCurrKey;
-
-    otMacFrameSetKeyId(&sTransmitFrame, sKeyId);
-    otMacFrameSetFrameCounter(&sTransmitFrame, sMacFrameCounter++);
 
     processSecurity = true;
 #endif // OPENTHREAD_CONFIG_THREAD_VERSION >= OT_THREAD_VERSION_1_2