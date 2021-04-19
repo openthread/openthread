3acc41ebccec0d088bef5fee574a07bd93da930f
Handa Wang <handaw@google.com>
2021-04-19 17:01:09 +0800

    minimal test case for reproducing

---
+++ tests/scripts/thread-cert/test_ping.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2021, The OpenThread Authors.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. Neither the name of the copyright holder nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+
+import logging
+import unittest
+
+import pktverify
+from pktverify import packet_verifier, packet_filter
+from pktverify.consts import MA1, MA1g, MA2
+import config
+import thread_cert
+
+# Test description:
+# The purpose of this test is to verify the functionality of ping command.
+#
+# Topology:
+#    ----------------(eth)------------------
+#           |                  |     |
+#          BR1 (Leader) ----- BR2   HOST
+#           |
+#           |
+#          TD
+#
+
+ROUTER_1 = 1
+ROUTER_2 = 2
+
+
+class TestPing(thread_cert.TestCase):
+    USE_MESSAGE_FACTORY = False
+
+    TOPOLOGY = {
+        ROUTER_1: {
+            'name': 'Router_1',
+            'allowlist': [ROUTER_2],
+            'version': '1.2',
+            'router_selection_jitter': 2,
+        },
+        ROUTER_2: {
+            'name': 'Router_2',
+            'allowlist': [ROUTER_1],
+            'version': '1.2',
+            'router_selection_jitter': 2,
+        },
+    }
+
+    def test(self):
+        router1 = self.nodes[ROUTER_1]
+        router2 = self.nodes[ROUTER_2]
+
+        router1.start()
+        self.simulator.go(5)
+        self.assertEqual('leader', router1.get_state())
+
+        router2.start()
+        self.simulator.go(5)
+        self.assertEqual('router', router2.get_state())
+
+        # 3. ROUTER_1 pings ROUTER_2 using RLOC address. ROUTER_1 sends 5 echo
+        # requests and expects 5 responses.
+        self.assertTrue(
+            router1.ping(router2.get_ip6_address(config.ADDRESS_TYPE.RLOC),
+                         count=5,
+                         num_responses=5))
+        self.simulator.go(5)
+
+        self.collect_ipaddrs()
+        self.collect_rloc16s()
+        self.collect_rlocs()
+        self.collect_extra_vars()
+
+    def verify(self, pv: pktverify.packet_verifier.PacketVerifier):
+        pkts = pv.pkts
+        vars = pv.vars
+        pv.summary.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
