c0c4315f64176aa91f2cf0fd844f4ed126c0effb
Handa Wang <handaw@google.com>
2021-04-23 14:15:01 +0800

    MATN-16

--- tests/scripts/thread-cert/pktverify/consts.py
+++ tests/scripts/thread-cert/pktverify/consts.py
@@ -52,6 +52,7 @@ MA6 = Ipv6Addr('ff02::1')
 MAe1 = Ipv6Addr('fd0e::1234:777a:1')
 MAe2 = Ipv6Addr('::')
 MAe3 = Ipv6Addr('cafe::e0ff')
+MAS = [[Ipv6Addr(f"ff0e::{format(j, 'x')}:777a:{format(i, 'x')}") for j in range(16)] for i in range(16)]
 ALL_MPL_FORWARDERS_MA = Ipv6Addr('ff03::fc')
 
 LINK_LOCAL_PREFIX = Bytes("fe80")
