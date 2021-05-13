53b50577d4b50564e08c9daca27e4fa33e735b06
whd <7058128+superwhd@users.noreply.github.com>
2021-05-13 10:27:41 +0800

    [github-actions] fix MATN_16_LargeNumberOfMulticastGroupSubscriptionsToBBR (#6576)

--- tests/scripts/thread-cert/border_router/MATN_02_MLRFirstUse.py
+++ tests/scripts/thread-cert/border_router/MATN_02_MLRFirstUse.py
@@ -167,7 +167,7 @@ class MATN_02_MLRFirstUse(thread_cert.TestCase):
         # 3. BR_1 responds to the multicast registration.
         # BR_1 unicasts an MLR.rsp CoAP response to TD as "2.04 changed".
         # The payload contains "Status TLV: ST_MLR_SUCCESS".
-        pkts.filter_wpan_src64(vars['BR_1']) \
+        pkts.copy().filter_wpan_src64(vars['BR_1']) \
             .filter_ipv6_dst(vars['TD_RLOC']) \
             .filter_coap_ack('/n/mr') \
             .filter(lambda p: p.thread_nm.tlv.status == 0) \
