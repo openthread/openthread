e7c2ffc012072e0b472910e47a82cd32c5f8f591
Handa Wang <handaw@google.com>
2021-06-04 14:19:21 +0800

    update API version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (122)
+#define OPENTHREAD_API_VERSION (123)
 
 /**
  * @addtogroup api-instance
