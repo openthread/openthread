9df5b247708531a2261b14ded56810663da77b59
Sam Kumar <samkumar@berkeley.edu>
2021-06-16 16:00:37 -0700

    Add TCP API for OpenThread
    Co-authored-by: Jonathan Hui <jonhui@google.com>
    Co-authored-by: Abtin Keshavarzian <abtink@google.com>
    Co-authored-by: Sam Kumar <sam_k@apple.com>
    Co-authored-by: Simon Lin <simonlin@google.com>

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (124)
+#define OPENTHREAD_API_VERSION (125)
 
 /**
  * @addtogroup api-instance
