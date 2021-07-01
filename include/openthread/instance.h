c669a28eb98b1cfb2623e0e537c6b22adba023d7
Sam Kumar <samkumar@berkeley.edu>
2021-07-01 15:10:59 -0700

    [tcp] Add recommended receive buffer sizes to the API

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (138)
+#define OPENTHREAD_API_VERSION (139)
 
 /**
  * @addtogroup api-instance
