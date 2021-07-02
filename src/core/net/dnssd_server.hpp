01e2068f72835c0a7f798eca7c7e5dc65c1ee40c
Abtin Keshavarzian <abtink@google.com>
2021-07-02 16:52:01 -0700

    [dnssd-server] typos and smaller style changes

--- src/core/net/dnssd_server.hpp
+++ src/core/net/dnssd_server.hpp
@@ -60,7 +60,7 @@ class Server : public InstanceLocator, private NonCopyable
 {
 public:
     /**
-     * This enumeration specifies a dns-sd query type.
+     * This enumeration specifies a DNS-SD query type.
      *
      */
     enum DnsQueryType : uint8_t
@@ -125,7 +125,7 @@ public:
     void HandleDiscoveredHost(const char *aHostFullName, const otDnssdHostInfo &aHostInfo);
 
     /**
-     * This function aquires the next query in the server.
+     * This method acquires the next query in the server.
      *
      * @param[in] aQuery            The query pointer. Pass nullptr to get the first query.
      *
@@ -135,12 +135,12 @@ public:
     const otDnssdQuery *GetNextQuery(const otDnssdQuery *aQuery) const;
 
     /**
-     * This function aquires the dns-sd query type and name for a specific query.
+     * This method acquires the DNS-SD query type and name for a specific query.
      *
      * @param[in]   aQuery            The query pointer.
      * @param[out]  aNameOutput       The name output buffer.
      *
-     * @returns The dns-sd query type.
+     * @returns The DNS-SD query type.
      *
      */
     static DnsQueryType GetQueryTypeAndName(const otDnssdQuery *aQuery, char (&aName)[Name::kMaxNameSize]);
