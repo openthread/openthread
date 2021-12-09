e708324c3189cf3ac2f33bfc2d10c61a43a4df6f
Eduardo Montoya <edmont@users.noreply.github.com>
2021-12-09 18:12:39 +0100

    [mlr] send BMLR.ntf for de-registration (#7227)
    BBR should send BMLR.ntf to the backbone upon successful
    de-registration as well.

--- src/core/backbone_router/bbr_manager.cpp
+++ src/core/backbone_router/bbr_manager.cpp
@@ -242,6 +242,9 @@ void Manager::HandleMulticastListenerRegistration(const Coap::Message &aMessage,
         if (timeout == 0)
         {
             mMulticastListenersTable.Remove(address);
+
+            // Put successfully de-registered addresses at the end of `addresses`.
+            addresses[kIp6AddressesNumMax - (++successAddressNum)] = address;
         }
         else
         {
