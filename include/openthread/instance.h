330320e9a4f7433d4fabaf1ccf4d09f77136299b
Abtin Keshavarzian <abtink@google.com>
2021-12-29 12:29:41 -0800

    [logging] simplify logging by removing `DEFINE_AS_MACRO_ONLY`
    This commit removes `OPENTHREAD_CONFIG_LOG_DEFINE_AS_MACRO_ONLY`.
    which helps simplify the logging implementation. This feature enabled
    all logging including the platform API to be defined as macros and
    was intended for certain restricted platforms. With the new logging
    model, OpenThread core itself will prepare the entire log line
    instead of platform layer, so this feature is no longer applicable or
    useful.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (180)
+#define OPENTHREAD_API_VERSION (181)
 
 /**
  * @addtogroup api-instance
