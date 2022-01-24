4af585400e1412034ab65f4ad7cf6d6f064f288f
JaneFromSilabs <58004715+JaneFromSilabs@users.noreply.github.com>
2022-01-24 12:49:59 -0500

    [thci] reset and multicast changes (#7340)
    - THCI changes for bbr reset procedure to make a better effort to
      reset and to also make sure to restart the otbr-agent prior to
      attempting a factoryreset to cover the case where the agent cannot
      currently communicate to the rcp.
    
    - THCI change to move from sudo service restart otbr-agent to sudo
      systemctl restart otbr-agent, which is more comprehensive.
    
    - THCI changes for multicast to use ipmaddr in both soc and bbr cases.

--- tools/harness-thci/OpenThread.py
+++ tools/harness-thci/OpenThread.py
@@ -1511,7 +1511,7 @@ class OpenThreadTHCI(object):
 
         start_time = time.time()
         while time.time() < start_time + timeout:
-            time.sleep(0.3)
+            time.sleep(0.5)
             if not self.IsBorderRouter:
                 self._disconnect()
                 self._connect()
@@ -1519,6 +1519,10 @@ class OpenThreadTHCI(object):
                 self.__executeCommand('state', timeout=0.1)
                 break
             except Exception:
+                self.__restartAgentService()
+                time.sleep(2)
+                self.__sendCommand('factoryreset', expectEcho=False)
+                time.sleep(0.5)
                 continue
         else:
             raise AssertionError("Could not connect with OT device {} after reset.".format(self))
@@ -3489,6 +3493,7 @@ class OpenThreadTHCI(object):
         Args:
             sAddr   : str : Multicast address to be subscribed and notified OTA.
         """
+        self._beforeRegisterMulticast(sAddr, timeout)
 
         cmd = 'ipmaddr add ' + str(sAddr)
 
@@ -3651,6 +3656,12 @@ class OpenThread(OpenThreadTHCI, IThci):
         pass
 
     def _deviceAfterReset(self):
+        pass
+
+    def __restartAgentService(self):
+        pass
+
+    def _beforeRegisterMulticast(self, sAddr, timeout):
         pass
 
     def __socRead(self, size=512):
