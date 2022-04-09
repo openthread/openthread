c1f1a0bc43715680196cc9be5bbe2029e1203db5
Sam Kumar <samkumar@berkeley.edu>
2022-04-09 12:39:56 -0700

    [tcp] add TCP circular send buffer API

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (203)
+#define OPENTHREAD_API_VERSION (204)
 
 /**
  * @addtogroup api-instance
