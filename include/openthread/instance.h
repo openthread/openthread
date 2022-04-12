e05d3a58123148d20ef473a3720c9e06147b3ada
Simon Lin <simonlin@google.com>
2022-04-12 22:46:41 +0800

    update api version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (201)
+#define OPENTHREAD_API_VERSION (202)
 
 /**
  * @addtogroup api-instance
