1e17f2d55775a539c9d1ba8dff0d4019a44eb7ea
Tobias <tlifschitz@maper.com.ar>
2022-04-25 16:15:53 -0300

    Increment api version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (204)
+#define OPENTHREAD_API_VERSION (205)
 
 /**
  * @addtogroup api-instance
