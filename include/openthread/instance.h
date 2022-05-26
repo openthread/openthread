99f775b4ec93a40d5f5b1b507cf0e62e2aa4ce12
Hemanth Rao <hemanth.laks@silabs.com>
2022-05-26 19:44:32 +0100

    Increment OPENTHREAD_API_VERSION

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (212)
+#define OPENTHREAD_API_VERSION (213)
 
 /**
  * @addtogroup api-instance
