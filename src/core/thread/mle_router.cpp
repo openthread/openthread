12dfedb28fc11a498341eeb45baa8e8f1bbe2a54
Abtin Keshavarzian <abtink@google.com>
2022-07-07 08:54:14 -0700

    [csl] update CSL on role change to router or leader (#7874)
    This commit adds a call to `Get<Mac::Mac>().UpdateCsl()` when there
    is role change from `SetStateLeader()` or `SetStateRouter()`. This
    ensures to disable CSL at `SubMac` and radio platform.

--- src/core/thread/mle_router.cpp
+++ src/core/thread/mle_router.cpp
@@ -399,6 +399,10 @@ void MleRouter::SetStateRouter(uint16_t aRloc16)
             RemoveNeighbor(child);
         }
     }
+
+#if OPENTHREAD_CONFIG_MAC_CSL_RECEIVER_ENABLE
+    Get<Mac::Mac>().UpdateCsl();
+#endif
 }
 
 void MleRouter::SetStateLeader(uint16_t aRloc16, LeaderStartMode aStartMode)
@@ -439,6 +443,10 @@ void MleRouter::SetStateLeader(uint16_t aRloc16, LeaderStartMode aStartMode)
             RemoveNeighbor(child);
         }
     }
+
+#if OPENTHREAD_CONFIG_MAC_CSL_RECEIVER_ENABLE
+    Get<Mac::Mac>().UpdateCsl();
+#endif
 
     LogNote("Leader partition id 0x%x", mLeaderData.GetPartitionId());
 }
