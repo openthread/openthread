3679ee7994f7881f5bd4de5a6b7f7802b7cead5f
Pawel Wanczyk <Pawel.Wanczyk@nordicsemi.no>
2022-07-19 11:03:18 +0200

    [routing-table] Do not reset LastHeard timestamp when removing Router Link with Link quality 0 (non-existent link).

--- src/core/thread/router_table.cpp
+++ src/core/thread/router_table.cpp
@@ -323,7 +323,11 @@ exit:
 
 void RouterTable::RemoveRouterLink(Router &aRouter)
 {
-    aRouter.SetLinkQualityOut(kLinkQuality0);
+    if (aRouter.GetLinkQualityOut() != 0)
+    {
+        aRouter.SetLinkQualityOut(kLinkQuality0);
+        aRouter.SetLastHeard(TimerMilli::GetNow());
+    }
 
     for (Router *cur = GetFirstEntry(); cur != nullptr; cur = GetNextEntry(cur))
     {
