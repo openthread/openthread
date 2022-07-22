4361a01c59465f77b5da1f23058f7bc1b35575d5
Song Guo <songguo@google.com>
2022-07-22 15:37:25 +0800

    Update test cases

--- tests/unit/test_nat64.cpp
+++ tests/unit/test_nat64.cpp
@@ -28,38 +28,50 @@
 
 #include "border_router/nat64.hpp"
 
+#include "test_platform.h"
+#include "test_util.hpp"
+
 #include "string.h"
 
+#include "common/code_utils.hpp"
+#include "common/debug.hpp"
 #include "common/instance.hpp"
 #include "common/message.hpp"
 #include "net/ip6.hpp"
 
-#include "common/code_utils.hpp"
-#include "common/debug.hpp"
-
-#include "test_platform.h"
-#include "test_util.hpp"
+#if OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
 
 namespace ot {
 namespace BorderRouter {
 
-#if OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
+static ot::Instance *sInstance;
 
-template <size_t N> bool CheckMessage(const Message *aMessage, const uint8_t (&aExpectedMessage)[N])
+void DumpMessageInHex(const char *prefix, const uint8_t *aBuf, size_t aBufLen)
 {
-    uint8_t  readMessage[N];
+    // This function dumps all packets the output of this function can be imported to packet analyser for debugging.
+    printf("%s", prefix);
+    for (uint16_t i = 0; i < aBufLen; i++)
+    {
+        printf("%02x", aBuf[i]);
+    }
+    printf("\n");
+}
+
+bool CheckMessage(const Message &aMessage, const uint8_t *aExpectedMessage, size_t aExpectedMessageLen)
+{
+    uint8_t  readMessage[OPENTHREAD_CONFIG_IP6_MAX_DATAGRAM_LENGTH];
     uint16_t messageLength;
     bool     success = true;
 
-    success       = success && (aMessage->GetLength() == N);
-    messageLength = aMessage->ReadBytes(0, readMessage, N);
-    success       = success && (N == messageLength);
-    success       = success && (memcmp(readMessage, aExpectedMessage, N) == 0);
+    success       = success && (aMessage.GetLength() == aExpectedMessageLen);
+    messageLength = aMessage.ReadBytes(0, readMessage, aMessage.GetLength());
+    success       = success && (aExpectedMessageLen == messageLength);
+    success       = success && (memcmp(readMessage, aExpectedMessage, aExpectedMessageLen) == 0);
 
     if (!success)
     {
         printf("Expected Message\n");
-        for (uint16_t i = 0; i < N; i++)
+        for (uint16_t i = 0; i < aExpectedMessageLen; i++)
         {
             printf("%02x%c", aExpectedMessage[i], " \n"[(i & 0xf) == 0xf]);
         }
@@ -75,14 +87,68 @@ template <size_t N> bool CheckMessage(const Message *aMessage, const uint8_t (&a
     return success;
 }
 
+template <size_t N>
+void TestCase6To4(const char *aTestName,
+                  const uint8_t (&aIp6Message)[N],
+                  Nat64::Result  aResult,
+                  const uint8_t *aOutMessage,
+                  size_t         aOutMessageLen)
+{
+    Message *msg = sInstance->Get<Ip6::Ip6>().NewMessage(0);
+
+    printf("Testing NAT64 6 to 4: %s\n", aTestName);
+
+    VerifyOrQuit(msg != nullptr);
+    SuccessOrQuit(msg->AppendBytes(aIp6Message, N));
+
+    DumpMessageInHex("I ", aIp6Message, N);
+
+    VerifyOrQuit(sInstance->Get<Nat64>().HandleOutgoing(*msg) == aResult);
+
+    if (aOutMessage != nullptr)
+    {
+        DumpMessageInHex("O ", aOutMessage, aOutMessageLen);
+        VerifyOrQuit(CheckMessage(*msg, aOutMessage, aOutMessageLen));
+    }
+
+    printf("  ... PASS\n");
+}
+
+template <size_t N>
+void TestCase4To6(const char *aTestName,
+                  const uint8_t (&aIp4Message)[N],
+                  Nat64::Result  aResult,
+                  const uint8_t *aOutMessage,
+                  size_t         aOutMessageLen)
+{
+    Message *msg = sInstance->Get<Ip6::Ip6>().NewMessage(0);
+
+    printf("Testing NAT64 4 to 6: %s\n", aTestName);
+
+    VerifyOrQuit(msg != nullptr);
+    SuccessOrQuit(msg->AppendBytes(aIp4Message, N));
+
+    DumpMessageInHex("I ", aIp4Message, N);
+
+    VerifyOrQuit(sInstance->Get<Nat64>().HandleIncoming(*msg) == aResult);
+
+    if (aOutMessage != nullptr)
+    {
+        DumpMessageInHex("O ", aOutMessage, aOutMessageLen);
+        VerifyOrQuit(CheckMessage(*msg, aOutMessage, aOutMessageLen));
+    }
+
+    printf("  ... PASS\n");
+}
+
 void TestNat64(void)
 {
     Ip6::Prefix  nat64prefix;
     Ip4::Cidr    nat64cidr;
     Ip6::Address ip6Source;
     Ip6::Address ip6Dest;
-    Instance *   instance = testInitInstance();
-    Nat64        nat64(*instance);
+
+    sInstance = testInitInstance();
 
     {
         const uint8_t ip6Address[] = {0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -91,121 +157,171 @@ void TestNat64(void)
 
         nat64cidr.Set(ip4Address, 32);
         nat64prefix.Set(ip6Address, 96);
-        SuccessOrQuit(nat64.SetIp4Cidr(nat64cidr));
-        SuccessOrQuit(nat64.SetEnabled(true));
-        nat64.SetNat64Prefix(nat64prefix);
+        SuccessOrQuit(sInstance->Get<Nat64>().SetIp4Cidr(nat64cidr));
+        SuccessOrQuit(sInstance->Get<Nat64>().SetEnabled(true));
+        sInstance->Get<Nat64>().SetNat64Prefix(nat64prefix);
     }
 
     {
-        const uint8_t ip6SourceBytes[] = {
-            0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
-        };
-        const uint8_t ip6DestBytes[] = {
-            0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 172, 16, 243, 197,
-        };
-
-        ip6Source.SetBytes(ip6SourceBytes);
-        ip6Dest.SetBytes(ip6DestBytes);
-    }
-
-    {
-        const uint8_t ip6Packet[] = {
+        // fd02::1               fd01::ac10:f3c5       UDP      52     43981 → 4660 Len=4
+        const uint8_t kIp6Packet[] = {
             0x60, 0x08, 0x6e, 0x38, 0x00, 0x0c, 0x11, 0x40, 0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             172,  16,   243,  197,  0xab, 0xcd, 0x12, 0x34, 0x00, 0x0c, 0xe3, 0x31, 0x61, 0x62, 0x63, 0x64,
         };
-        const uint8_t expectedIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
-                                             0x4d, 192,  168,  123,  1,    172,  16,   243,  197,  0xab, 0xcd,
-                                             0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8d, 0x61, 0x62, 0x63, 0x64};
+        // 192.168.123.1         172.16.243.197        UDP      32     43981 → 4660 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
+                                      0x4d, 192,  168,  123,  1,    172,  16,   243,  197,  0xab, 0xcd,
+                                      0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8d, 0x61, 0x62, 0x63, 0x64};
 
-        Message *     msg;
-        Nat64::Result result;
-
-        printf("Testing Nat64 v6 -> v4\n");
-
-        msg = instance->Get<Ip6::Ip6>().NewMessage(0);
-        msg->AppendBytes(ip6Packet, sizeof(ip6Packet));
-
-        result = nat64.HandleOutgoing(*msg);
-        VerifyOrQuit(result == Nat64::Result::kForward);
-        VerifyOrQuit(CheckMessage(msg, expectedIp4Packet));
+        TestCase6To4("good v6 udp datagram", kIp6Packet, Nat64::Result::kForward, kIp4Packet, sizeof(kIp4Packet));
     }
 
     {
-        const uint8_t ip4Packet[]         = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
-                                     0x4d, 172,  16,   243,  197,  192,  168,  123,  1,    0xab, 0xcd,
-                                     0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8d, 0x61, 0x62, 0x63, 0x64};
-        const uint8_t expectedIp6Packet[] = {
+        // 172.16.243.197        192.168.123.1         UDP      32     43981 → 4660 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
+                                      0x4d, 172,  16,   243,  197,  192,  168,  123,  1,    0xab, 0xcd,
+                                      0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8d, 0x61, 0x62, 0x63, 0x64};
+        // fd01::ac10:f3c5       fd02::1               UDP      52     43981 → 4660 Len=4
+        const uint8_t kIp6Packet[] = {
             0x60, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x11, 0x3e, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 172,  16,   243,  197,  0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x01, 0xab, 0xcd, 0x12, 0x34, 0x00, 0x0c, 0xe3, 0x31, 0x61, 0x62, 0x63, 0x64,
         };
 
-        Message *     msg;
-        Nat64::Result result;
-
-        printf("Testing Nat64 v4 -> v6 (mapping exists)\n");
-        msg = instance->Get<Ip6::Ip6>().NewMessage(sizeof(Ip6::Header) - sizeof(Ip4::Header));
-        msg->AppendBytes(ip4Packet, sizeof(ip4Packet));
-
-        result = nat64.HandleIncoming(*msg);
-        VerifyOrQuit(result == Nat64::Result::kForward);
-        VerifyOrQuit(CheckMessage(msg, expectedIp6Packet));
+        TestCase4To6("good v4 udp datagram", kIp4Packet, Nat64::Result::kForward, kIp6Packet, sizeof(kIp6Packet));
     }
 
     {
-        const uint8_t ip4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
-                                     0x4c, 172,  16,   243,  197,  192,  168,  123,  2,    0xab, 0xcd,
-                                     0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8c, 0x61, 0x62, 0x63, 0x64};
-
-        Message *     msg;
-        Nat64::Result result;
-
-        printf("Testing Nat64 v4 -> v6\n");
-
-        msg = instance->Get<Ip6::Ip6>().NewMessage(sizeof(Ip6::Header) - sizeof(Ip4::Header));
-        msg->AppendBytes(ip4Packet, sizeof(ip4Packet));
-
-        result = nat64.HandleIncoming(*msg);
-        VerifyOrQuit(result == Nat64::Result::kDrop);
-    }
-
-    {
-        const uint8_t ip6SourceBytes[] = {
-            0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        // fd02::1               fd01::ac10:f3c5       TCP      64     43981 → 4660 [ACK] Seq=1 Ack=1 Win=1 Len=4
+        const uint8_t kIp6Packet[] = {
+            0x60, 0x08, 0x6e, 0x38, 0x00, 0x18, 0x06, 0x40, 0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 172,  16,   243,  197,  0xab, 0xcd, 0x12, 0x34, 0x87, 0x65, 0x43, 0x21,
+            0x12, 0x34, 0x56, 0x78, 0x50, 0x10, 0x00, 0x01, 0x5f, 0xf8, 0x00, 0x00, 0x61, 0x62, 0x63, 0x64,
         };
-        const uint8_t ip6DestBytes[] = {
-            0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 172, 16, 243, 197,
+        // 192.168.123.1         172.16.243.197        TCP      44     43981 → 4660 [ACK] Seq=1 Ack=1 Win=1 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x2c, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x06, 0xa0,
+                                      0x4c, 192,  168,  123,  1,    172,  16,   243,  197,  0xab, 0xcd,
+                                      0x12, 0x34, 0x87, 0x65, 0x43, 0x21, 0x12, 0x34, 0x56, 0x78, 0x50,
+                                      0x10, 0x00, 0x01, 0x1e, 0x54, 0x00, 0x00, 0x61, 0x62, 0x63, 0x64};
+
+        TestCase6To4("good v6 tcp datagram", kIp6Packet, Nat64::Result::kForward, kIp4Packet, sizeof(kIp4Packet));
+    }
+
+    {
+        // 172.16.243.197        192.168.123.1         TCP      44     43981 → 4660 [ACK] Seq=1 Ack=1 Win=1 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x2c, 0x00, 0x00, 0x00, 0x00, 0x40, 0x06, 0x9f,
+                                      0x4c, 172,  16,   243,  197,  192,  168,  123,  1,    0xab, 0xcd,
+                                      0x12, 0x34, 0x87, 0x65, 0x43, 0x21, 0x12, 0x34, 0x56, 0x78, 0x50,
+                                      0x10, 0x00, 0x01, 0x1e, 0x54, 0x00, 0x00, 0x61, 0x62, 0x63, 0x64};
+        // fd01::ac10:f3c5       fd02::1               TCP      64     43981 → 4660 [ACK] Seq=1 Ack=1 Win=1 Len=4
+        const uint8_t kIp6Packet[] = {
+            0x60, 0x00, 0x00, 0x00, 0x00, 0x18, 0x06, 0x3f, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 172,  16,   243,  197,  0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xab, 0xcd, 0x12, 0x34, 0x87, 0x65, 0x43, 0x21,
+            0x12, 0x34, 0x56, 0x78, 0x50, 0x10, 0x00, 0x01, 0x5f, 0xf8, 0x00, 0x00, 0x61, 0x62, 0x63, 0x64,
         };
 
-        ip6Source.SetBytes(ip6SourceBytes);
-        ip6Dest.SetBytes(ip6DestBytes);
+        TestCase4To6("good v4 tcp datagram", kIp4Packet, Nat64::Result::kForward, kIp6Packet, sizeof(kIp6Packet));
     }
 
     {
-        const uint8_t ip6Packet[] = {
+        // fd02::1         fd01::ac10:f3c5     ICMPv6   52     Echo (ping) request id=0xaabb, seq=1, hop limit=64
+        const uint8_t kIp6Packet[] = {
+            0x60, 0x08, 0x6e, 0x38, 0x00, 0x0c, 0x3a, 0x40, 0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            172,  16,   243,  197,  0x80, 0x00, 0x76, 0x59, 0xaa, 0xbb, 0x00, 0x01, 0x61, 0x62, 0x63, 0x64,
+        };
+        // 192.168.123.1   172.16.243.197      ICMP     32     Echo (ping) request  id=0xaabb, seq=1/256, ttl=63
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x01, 0xa0,
+                                      0x5d, 192,  168,  123,  1,    172,  16,   243,  197,  0x08, 0x00,
+                                      0x88, 0x7c, 0xaa, 0xbb, 0x00, 0x01, 0x61, 0x62, 0x63, 0x64};
+
+        TestCase6To4("good v6 icmp ping request datagram", kIp6Packet, Nat64::Result::kForward, kIp4Packet,
+                     sizeof(kIp4Packet));
+    }
+
+    {
+        // 172.16.243.197        192.168.123.1         ICMP     32     Echo (ping) reply    id=0xaabb, seq=1/256, ttl=63
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x01, 0xa0,
+                                      0x5d, 172,  16,   243,  197,  192,  168,  123,  1,    0x00, 0x00,
+                                      0x90, 0x7c, 0xaa, 0xbb, 0x00, 0x01, 0x61, 0x62, 0x63, 0x64};
+        // fd01::ac10:f3c5       fd02::1               ICMPv6   52     Echo (ping) reply id=0xaabb, seq=1, hop limit=62
+        const uint8_t kIp6Packet[] = {
+            0x60, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x3a, 0x3e, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 172,  16,   243,  197,  0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x01, 0x81, 0x00, 0x75, 0x59, 0xaa, 0xbb, 0x00, 0x01, 0x61, 0x62, 0x63, 0x64,
+        };
+
+        TestCase4To6("good v4 icmp ping response datagram", kIp4Packet, Nat64::Result::kForward, kIp6Packet,
+                     sizeof(kIp6Packet));
+    }
+
+    {
+        // fd02::1               fd01::ac10:f3c5       UDP      52     43981 → 4660 Len=4
+        const uint8_t kIp6Packet[] = {
+            0x60, 0x08, 0x6e, 0x38, 0x00, 0x0c, 0x11, 0x01, 0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            172,  16,   243,  197,  0xab, 0xcd, 0x12, 0x34, 0x00, 0x0c, 0xe3, 0x31, 0x61, 0x62, 0x63, 0x64,
+        };
+
+        TestCase6To4("v6 hop limit reach", kIp6Packet, Nat64::Result::kDrop, nullptr, 0);
+    }
+
+    {
+        // fd02::1               N/A                   IPv6     39     Invalid IPv6 header
+        const uint8_t kIp6Packet[] = {0x60, 0x08, 0x6e, 0x38, 0x00, 0x0c, 0x11, 0x40, 0xfd, 0x02, 0x00, 0x00, 0x00,
+                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xfd, 0x01,
+                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 172,  16,   243};
+
+        TestCase6To4("bad v6 datagram", kIp6Packet, Nat64::Result::kDrop, nullptr, 0);
+    }
+
+    {
+        // 172.16.243.197        192.168.123.1         UDP      32     43981 → 4660 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x01, 0x11, 0xde,
+                                      0x4d, 172,  16,   243,  197,  192,  168,  123,  1,    0xab, 0xcd,
+                                      0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8d, 0x61, 0x62, 0x63, 0x64};
+
+        TestCase4To6("v4 timeout", kIp4Packet, Nat64::Result::kDrop, nullptr, 0);
+    }
+
+    {
+        // 172.16.243.197        N/A                   IPv4     19     [Malformed Packet]
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11,
+                                      0xa0, 0x4c, 172,  16,   243,  197,  192,  168,  123};
+
+        TestCase4To6("bad v4 datagram", kIp4Packet, Nat64::Result::kDrop, nullptr, 0);
+    }
+
+    {
+        // 172.16.243.197        192.168.123.2         UDP      32     43981 → 4660 Len=4
+        const uint8_t kIp4Packet[] = {0x45, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x11, 0xa0,
+                                      0x4c, 172,  16,   243,  197,  192,  168,  123,  2,    0xab, 0xcd,
+                                      0x12, 0x34, 0x00, 0x0c, 0xa1, 0x8c, 0x61, 0x62, 0x63, 0x64};
+
+        TestCase4To6("no v4 mapping", kIp4Packet, Nat64::Result::kDrop, nullptr, 0);
+    }
+
+    {
+        // fd02::2               fd01::ac10:f3c5       UDP      52     43981 → 4660 Len=4
+        const uint8_t kIp6Packet[] = {
             0x60, 0x08, 0x6e, 0x38, 0x00, 0x0c, 0x11, 0x40, 0xfd, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xfd, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             172,  16,   243,  197,  0xab, 0xcd, 0x12, 0x34, 0x00, 0x0c, 0xe3, 0x30, 0x61, 0x62, 0x63, 0x64,
         };
 
-        Message *     msg;
-        Nat64::Result result;
-
-        printf("Testing Nat64 v6 -> v4 (mapping pool exhausted)\n");
-
-        msg = instance->Get<Ip6::Ip6>().NewMessage(0);
-        msg->AppendBytes(ip6Packet, sizeof(ip6Packet));
-
-        result = nat64.HandleOutgoing(*msg);
-        VerifyOrQuit(result == Nat64::Result::kDrop);
+        TestCase6To4("mapping pool exhausted", kIp6Packet, Nat64::Result::kDrop, nullptr, 0);
     }
-}
 
-#endif // OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
+    testFreeInstance(sInstance);
+}
 
 } // namespace BorderRouter
 } // namespace ot
+
+#endif // OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
 
 int main(void)
 {
@@ -213,7 +329,7 @@ int main(void)
     ot::BorderRouter::TestNat64();
     printf("All tests passed\n");
 #else  // OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
-    printf("Nat64 is not enabled\n");
+    printf("NAT64 is not enabled\n");
 #endif // OPENTHREAD_CONFIG_NAT64_MANAGER_ENABLE
     return 0;
 }
