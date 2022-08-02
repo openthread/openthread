4f459244efd2f3121ba0f7bb8a084a8ecf4c817e
Sarah <91494325+sarah-iot@users.noreply.github.com>
2022-08-02 10:00:56 -0700

    Update instance.h

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (230)
+#define OPENTHREAD_API_VERSION (231)
 
 /**
  * @addtogroup api-instance
