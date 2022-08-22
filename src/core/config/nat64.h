3d8f837bdaa448f90d71bd9e88f8d68c2af7d757
Song Guo <songguo@google.com>
2022-08-22 14:56:40 +0800

    Update

--- src/core/config/nat64.h
+++ src/core/config/nat64.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019, The OpenThread Authors.
+ *  Copyright (c) 2022, The OpenThread Authors.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
