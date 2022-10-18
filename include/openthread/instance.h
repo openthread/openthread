f3e0fc8b8e080737c6d058dc0ff7e11c195ff1cc
Jonathan Hui <jonhui@google.com>
2022-10-18 12:29:52 -0700

    bump api version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (251)
+#define OPENTHREAD_API_VERSION (252)
 
 /**
  * @addtogroup api-instance
