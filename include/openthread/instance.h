9e54a279f5c350d033b4f780523bcad11f3c4773
Jonathan Hui <jonhui@google.com>
2022-10-31 15:32:07 -0700

    increment API version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (256)
+#define OPENTHREAD_API_VERSION (257)
 
 /**
  * @addtogroup api-instance
