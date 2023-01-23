c715d861908c6e194e0d2369c5ee6649dd416ea2
Stefan Agner <stefan@agner.ch>
2023-01-23 11:54:05 +0100

    Update include/openthread/dataset.h
    Co-authored-by: Abtin Keshavarzian <abtink@google.com>

--- include/openthread/dataset.h
+++ include/openthread/dataset.h
@@ -597,6 +597,9 @@ otError otDatasetParseTlvs(const otOperationalDatasetTlvs *aDatasetTlvs, otOpera
 /**
  * This function updates an Operational Dataset with information from another Operational Dataset.
  *
+ * This function updates @p aDstDataset by setting all the components that are marked as present in @p aSrcDataset to
+ * the same value. Other components remain unchanged in @p aDataset.
+ * 
  * @param[in]  aSrcDataset  A pointer to dataset with the new/updated information.
  * @param[out] aDstDataset  A pointer to where the dataset will be placed.
  *
