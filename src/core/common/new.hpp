5cb0e74d5ee4f17c1f654ac68c6d87a3cce611ee
Yakun Xu <xyk@google.com>
2023-02-03 12:41:13 +0800

    [test] migrate tests to cmake
    This commit migrate tests not targeting autotool to cmake.
    
    * removed openthread-test-driver
    * removed functional tests from autotool based check
    * corrected file permission of python scripts
    * added --run-directory to specify directory to collect logs and captures
    * get test-ot-test-srp-server pass on POSIX platform

--- src/core/common/new.hpp
+++ src/core/common/new.hpp
@@ -31,8 +31,8 @@
  *   This file defines the new operator used by OpenThread.
  */
 
-#ifndef NEW_HPP_
-#define NEW_HPP_
+#ifndef OT_CORE_COMMON_NEW_HPP_
+#define OT_CORE_COMMON_NEW_HPP_
 
 #include "openthread-core-config.h"
 
@@ -42,4 +42,4 @@
 
 inline void *operator new(size_t, void *p) throw() { return p; }
 
-#endif // NEW_HPP_
+#endif // OT_CORE_COMMON_NEW_HPP_
