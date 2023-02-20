9e0fe231c9f3520f6740961809fa07ebff8987de
Song Guo <songguo@google.com>
2023-02-20 11:58:15 +0800

    Update

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (288)
+#define OPENTHREAD_API_VERSION (289)
 
 /**
  * @addtogroup api-instance
