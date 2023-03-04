1533a304f142ca8366af65b59fa72c0ca8db9542
Abtin Keshavarzian <abtink@google.com>
2023-03-04 11:17:31 -0800

    [tcp] update `otTcpConnect()` doc to indicate currently supported behavior
    This commit updates the documentation for `otTcpConnect()` indicating
    that caller need to wait for `otTcpEstablished` callback indicating
    that TCP connection was established  before it can start sending
    data (e.g., calling `otTcpSendByReference()`).
    
    It also removes the mention of "TCP Fast Open" which is not yet
    supported.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (298)
+#define OPENTHREAD_API_VERSION (299)
 
 /**
  * @addtogroup api-instance
