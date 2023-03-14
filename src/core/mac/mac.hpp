a5d7947e2db6c097aceadfce5df60ede0f4d4627
Doru Gucea <doru-cristian.gucea@nxp.com>
2023-03-14 00:47:22 -0700

    [mac] Allow setting the timeout for receiving a Data Frame
    This setting is targeted for SEDs which may need to decrease the timeout for
    receiving a Data Frame (which follows an ACK with FP bit set) for power saving reasons.
    
    IEEE 802.15.4/2015 specification doesn't enforce a specific timeout value for
    this parameter so the application should be able to configure it.
    
    Signed-off-by: Doru Gucea <doru-cristian.gucea@nxp.com>

--- src/core/mac/mac.hpp
+++ src/core/mac/mac.hpp
@@ -71,8 +71,9 @@ class Neighbor;
 
 namespace Mac {
 
-constexpr uint32_t kDataPollTimeout = 100; ///< Timeout for receiving Data Frame (in msec).
-constexpr uint32_t kSleepDelay      = 300; ///< Max sleep delay when frame is pending (in msec).
+constexpr uint32_t kDataPollTimeout =
+    OPENTHREAD_CONFIG_MAC_DATA_POLL_TIMEOUT; ///< Timeout for receiving Data Frame (in msec).
+constexpr uint32_t kSleepDelay = 300;        ///< Max sleep delay when frame is pending (in msec)
 
 constexpr uint16_t kScanDurationDefault = OPENTHREAD_CONFIG_MAC_SCAN_DURATION; ///< Duration per channel (in msec).
 
