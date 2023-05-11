58561fd2140b1a07ca615c3a236fc20dc0dc0d10
Jonathan Hui <jonhui@google.com>
2023-05-11 11:16:00 -0700

    [alarm] update docs to explicitly require 32-bit alarms

--- include/openthread/platform/alarm-milli.h
+++ include/openthread/platform/alarm-milli.h
@@ -56,6 +56,8 @@ extern "C" {
 /**
  * Set the alarm to fire at @p aDt milliseconds after @p aT0.
  *
+ * For both @p aT0 and @p aDt, the platform MUST support all values in [0, 2^32-1].
+ *
  * @param[in] aInstance  The OpenThread instance structure.
  * @param[in] aT0        The reference time.
  * @param[in] aDt        The time delay in milliseconds from @p aT0.
@@ -71,6 +73,9 @@ void otPlatAlarmMilliStop(otInstance *aInstance);
 
 /**
  * Get the current time.
+ *
+ * The current time MUST represent a free-running timer. When maintaining current time, the time value MUST utilize the
+ * entire range [0, 2^32-1] and MUST NOT wrap before 2^32.
  *
  * @returns The current time in milliseconds.
  */
