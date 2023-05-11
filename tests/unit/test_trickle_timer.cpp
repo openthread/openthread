3ce4d8bef2103f813354a99060cd9bb445cbb311
David Smith <david.smith@mmbnetworks.com>
2023-05-11 15:38:13 -0400

    Trying to fix the pretty issues.

--- tests/unit/test_trickle_timer.cpp
+++ tests/unit/test_trickle_timer.cpp
@@ -113,7 +113,7 @@ public:
         mFiredCounter++;
     }
 
-    uint32_t GetFiredCounter(void) { return mFiredCounter; }
+    uint32_t GetFiredCounter(void) const { return mFiredCounter; }
 
     void ResetFiredCounter(void) { mFiredCounter = 0; }
 
