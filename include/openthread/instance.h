771b9b8ff9df94646261ce1f51d33c0f3a0409c9
Abtin Keshavarzian <abtink@google.com>
2023-06-07 18:37:08 -0700

    [address-resolver] implement ramp-down mechanism for retry delay
    Address resolver retry delay determines how long the device needs to
    wait after a failed address query for a target address to allow a
    next query attempt. This is tracked per target (in `CacheEntry`).
    
    The retry delay starts with an initial value (15 seconds). If
    consecutive queries fail, the retry delay doubles each time, up to a
    maximum value (120 seconds). In the current code, the retry delay is
    only reset to the initial value after a successful address
    resolution.
    
    This commit implements a new mechanism to reduce the retry delay over
    time when there is no activity (no queries for the target address).
    The ramp-down countdown starts after a previous retry delay has
    expired. If no query is requested within the maximum retry delay
    interval (120 seconds), the retry delay is divided by two. This
    process is repeated until the retry delay reaches the initial
    (minimum) value.
    
    If a query is requested during the ramp-down process, the ramp-down is
    stopped. The next actions are determined based on whether the query
    is successful or fails. If the query is successful, the retry delay
    is reset to the initial value. If the query fails, the retry delay
    timeout is applied and the ramp-down process is started again after
    the retry delay duration has expired.
    
    This commit also updates the `test-014-address-resolver`  to validate
    the newly added ramp-down behavior.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (337)
+#define OPENTHREAD_API_VERSION (338)
 
 /**
  * @addtogroup api-instance
