ecb7a9c83829f52324cc2821db5c8e8ee0f780f7
bm9129 <136643535+bm9129@users.noreply.github.com>
2023-06-14 15:46:25 -0400

    Update infra_if.cpp
    Fix missing verification for Null pointer. Otherwise will crash for interfaces without links

--- src/posix/platform/infra_if.cpp
+++ src/posix/platform/infra_if.cpp
@@ -73,7 +73,7 @@ bool otPlatInfraIfHasAddress(uint32_t aInfraIfIndex, const otIp6Address *aAddres
     {
         struct sockaddr_in6 *ip6Addr;
 
-        if (if_nametoindex(addr->ifa_name) != aInfraIfIndex || addr->ifa_addr->sa_family != AF_INET6)
+        if (if_nametoindex(addr->ifa_name) != aInfraIfIndex || addr->ifa_addr == NULL || addr->ifa_addr->sa_family != AF_INET6)
         {
             continue;
         }
@@ -316,7 +316,7 @@ void InfraNetif::CountAddresses(otSysInfraNetIfAddressCounters &aAddressCounters
     {
         in6_addr *in6Addr;
 
-        if (strncmp(addr->ifa_name, mInfraIfName, sizeof(mInfraIfName)) != 0 || addr->ifa_addr->sa_family != AF_INET6)
+        if (strncmp(addr->ifa_name, mInfraIfName, sizeof(mInfraIfName)) != 0 || addr->ifa_addr == NULL || addr->ifa_addr->sa_family != AF_INET6)
         {
             continue;
         }
@@ -348,7 +348,7 @@ bool InfraNetif::HasLinkLocalAddress(void) const
     {
         struct sockaddr_in6 *ip6Addr;
 
-        if (strncmp(addr->ifa_name, mInfraIfName, sizeof(mInfraIfName)) != 0 || addr->ifa_addr->sa_family != AF_INET6)
+        if (strncmp(addr->ifa_name, mInfraIfName, sizeof(mInfraIfName)) != 0 || addr->ifa_addr == NULL || addr->ifa_addr->sa_family != AF_INET6)
         {
             continue;
         }
