79b056add68e197e0fbbcccdfa7b8d95cd008954
gavinmcgovern <81183035+gavinmcgovern@users.noreply.github.com>
2023-06-22 14:59:21 -0700

    [docs] add radio defgroup for proper documentation (#9195)

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (333)
+#define OPENTHREAD_API_VERSION (334)
 
 /**
  * @addtogroup api-instance
