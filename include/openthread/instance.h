63e173366072dfe27d9697c50290bda4f9780da5
Martin Zimmermann <30142883+martinzi@users.noreply.github.com>
2023-07-01 18:44:07 +0200

    [csl] add APIs to retrieve status

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (338)
+#define OPENTHREAD_API_VERSION (339)
 
 /**
  * @addtogroup api-instance
