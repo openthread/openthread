4d13455eb223cef496b0a290641b981456089d7a
Zhanglong Xia <zhanglongxia@google.com>
2023-07-05 16:05:59 +0800

    update for comments

--- src/posix/platform/daemon.cpp
+++ src/posix/platform/daemon.cpp
@@ -75,17 +75,18 @@ void GetFilename(Filename &aFilename, const char *aPattern)
 int Daemon::OutputFormatV(const char *aFormat, va_list aArguments)
 {
     static constexpr char truncatedMsg[] = "(truncated ...)";
-    static_assert(OPENTHREAD_CONFIG_CLI_MAX_LINE_LENGTH < sizeof(truncatedMsg),
-                  "OPENTHREAD_CONFIG_CLI_MAX_LINE_LENGTH is too short!");
     char buf[OPENTHREAD_CONFIG_CLI_MAX_LINE_LENGTH];
     int  rval;
+
+    static_assert(sizeof(truncatedMsg) < OPENTHREAD_CONFIG_CLI_MAX_LINE_LENGTH,
+                  "OPENTHREAD_CONFIG_CLI_MAX_LINE_LENGTH is too short!");
 
     rval = vsnprintf(buf, sizeof(buf), aFormat, aArguments);
     VerifyOrExit(rval >= 0, otLogWarnPlat("Failed to format CLI output: %s", strerror(errno)));
 
     if (rval >= static_cast<int>(sizeof(buf)))
     {
-        rval = static_cast<int>(sizeof(buf));
+        rval = static_cast<int>(sizeof(buf) - 1);
         memcpy(buf + sizeof(buf) - sizeof(truncatedMsg), truncatedMsg, sizeof(truncatedMsg));
     }
 
