fd62216aa856b6078ea78dcc5451f7b3685d6d66
Zhangwx <zhangwenxu@espressif.com>
2023-07-13 05:14:43 +0800

    [border-agent] fix struct typedef for `otBorderAgentId` (#9277)

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (343)
+#define OPENTHREAD_API_VERSION (344)
 
 /**
  * @addtogroup api-instance
