f9554fa522cba7e510c4074354b64105304c7258
Abtin Keshavarzian <abtink@google.com>
2023-07-17 10:37:20 -0700

    [config] define separate project and platform core config header
    This commit updates and enhances the specification of OT core config
    header files. It adds `OPENTHREAD_PLATFORM_CORE_CONFIG_FILE`, which
    can be used by platforms to provide a core config header file name.
    
    The project and platform specific config header files are included in
    the following order:
    
    1. Project specific header (`OPENTHREAD_PROJECT_CORE_CONFIG_FILE`)
    2. Platform specific header (`OPENTHREAD_PLATFORM_CORE_CONFIG_FILE`)
    3. Default config values as specified by `config/{module}.h`
    
    CMake config options `OT_PROJECT_CONFIG` and `OT_PLATFORM_CONFIG` are
    also defined, which can be used to specify project and platform
    config headers. Platforms can define a default config header for
    `OT_PLATFORM_CONFIG`. The existing `OT_CONFIG` CMake option is marked
    as deprecated (with a warning message which recommends use of the new
    configs).
    
    This commit also updates the default simulation and POSIX core config
    headers to remove extra Doxygen-style documentation and ensure that
    all definitions have an `#ifndef` guard check.

--- src/core/openthread-core-config.h
+++ src/core/openthread-core-config.h
@@ -45,12 +45,25 @@
 
 #define OPENTHREAD_CORE_CONFIG_H_IN
 
+/**
+ * Include project and platform specific header files in the following order:
+ *
+ * 1. Project specific header file (`OPENTHREAD_PROJECT_CORE_CONFIG_FILE`)
+ * 2. Platform specific header file (`OPENTHREAD_PLATFORM_CORE_CONFIG_FILE`)
+ * 3. Default config values as specified by `config/{module}.h`
+ *
+ */
+
 #ifdef OPENTHREAD_PROJECT_CORE_CONFIG_FILE
 #include OPENTHREAD_PROJECT_CORE_CONFIG_FILE
 #elif defined(OPENTHREAD_CONFIG_CORE_USER_CONFIG_HEADER_ENABLE)
 // This configuration header file should be provided by the user when
 // OPENTHREAD_CONFIG_CORE_USER_CONFIG_HEADER_ENABLE is defined to 1.
 #include "openthread-core-user-config.h"
+#endif
+
+#ifdef OPENTHREAD_PLATFORM_CORE_CONFIG_FILE
+#include OPENTHREAD_PLATFORM_CORE_CONFIG_FILE
 #endif
 
 #ifndef OPENTHREAD_CONFIG_THREAD_VERSION
