5b196bb5ea5a17abf092c51d3f48a6723d5a3a33
Jake Swensen <jake@swensen.io>
2023-07-18 17:23:30 -0500

    [random] remove mbedtls header from api (#9286)
    When trying to opaquely bind to the public facing API, including
    <mbedtls/ctr_drbg.h> forces an external dependency on mbedtls.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (345)
+#define OPENTHREAD_API_VERSION (346)
 
 /**
  * @addtogroup api-instance
