650e30f81ccfd798409d459a86fb1ed575b5144a
Abtin Keshavarzian <abtink@google.com>
2023-07-26 15:50:40 -0700

    [mle] update Avd trickle timer interval based on router neighbor count (#9307)
    This commit updates the `TrickleTimer` to allow `IntervalMax` and
    `IntervalMin` to be changed while the timer is running. In particular,
    when `IntervalMax` is changed to a value that is shorter than the
    current interval being used by the timer, the timer will adapt the
    new shorter interval and may fire immediately.
    
    A unit test `test_trickle_timer` has been added to validate the
    behavior of `TrickleTimer` in detail. All different scenarios where
    `IntervalMax` or `IntervalMin` are changed are covered by the
    unit test.
    
    The new mechanism to change the trickle timer `IntervalMax` is
    used to update the MLE Advertisement trickle timer. The `IntervalMax`
    is determined based on the number of router neighbors of the device
    with link quality 2 or better. If the device has fewer router neighbors,
    it will use a shorter `IntervalMax`. As new links are established
    with routers, the `IntervalMax` is recalculated and updated on the
    Advertisement trickle timer.
    
    This commit also adds a new test `test-024-mle-adv-imax-change.py` to
    validate updates to the `IntervalMax` value based on the number of
    router neighbors.
    
    Co-authored-by: David Smith <david.smith@mmbnetworks.com>

--- src/core/thread/neighbor_table.cpp
+++ src/core/thread/neighbor_table.cpp
@@ -310,6 +310,13 @@ void NeighborTable::Signal(Event aEvent, const Neighbor &aNeighbor)
 #endif
         break;
 
+#if OPENTHREAD_FTD
+    case kRouterAdded:
+    case kRouterRemoved:
+        Get<RouterTable>().SignalTableChanged();
+        break;
+#endif
+
     default:
         break;
     }
