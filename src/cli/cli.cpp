64896b3249a6a2bbd37cb3193c5e8b4ffccb17bf
Jeff Rhodie <rhodie@google.com>
2023-09-07 01:54:37 +0000

    CLI tags

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -7847,6 +7847,29 @@ template <> otError Interpreter::Process<Cmd("trel")>(Arg aArgs[])
     {
         error = ProcessEnableDisable(aArgs + 1, otTrelIsFilterEnabled, otTrelSetFilterEnabled);
     }
+    /**
+     * @cli trel peers
+     * @code
+     * trel peers
+     * | No  | Ext MAC Address  | Ext PAN Id       | IPv6 Socket Address                              |
+       +-----+------------------+------------------+--------------------------------------------------+
+       |   1 | 5e5785ba3a63adb9 | f0d9c001f00d2e43 | [fe80:0:0:0:cc79:2a29:d311:1aea]:9202            |
+       |   2 | ce792a29d3111aea | dead00beef00cafe | [fe80:0:0:0:5c57:85ba:3a63:adb9]:9203            |
+     * Done
+     * @endcode
+     * @code 
+     * trel peers list
+     * 001 ExtAddr:5e5785ba3a63adb9 ExtPanId:f0d9c001f00d2e43 SockAddr:[fe80:0:0:0:cc79:2a29:d311:1aea]:9202
+       002 ExtAddr:ce792a29d3111aea ExtPanId:dead00beef00cafe SockAddr:[fe80:0:0:0:5c57:85ba:3a63:adb9]:9203
+       >>>>>>> [trel] implement new TREL model using DNS-SD
+     * Done
+     * @endcode
+     * @cparam trel peers [@ca{list}]
+     * @par
+     * Gets the TREL peer table in table or list format.
+     * @sa otTrelInitPeerIterator
+     * @sa otTrelGetNextPeer
+     */
     else if (aArgs[0] == "peers")
     {
         uint16_t           index = 0;
