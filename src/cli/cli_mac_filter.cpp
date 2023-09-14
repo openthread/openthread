f2d6752fe828a9bbabf7c9edfeaf2d4ea24c214e
Jeff Rhodie <rhodie@google.com>
2023-09-14 01:21:29 +0000

    CLI updates

--- src/cli/cli_mac_filter.cpp
+++ src/cli/cli_mac_filter.cpp
@@ -142,7 +142,7 @@ void MacFilter::OutputEntry(const otMacFilterEntry &aEntry)
  * Done
  * @endcode
  * @par api_copy
- * #otLinkFilterAddRssIn
+ * #otLinkFilterSetAddressMode
  */
 template <> otError MacFilter::Process<Cmd("addr")>(Arg aArgs[])
 {
