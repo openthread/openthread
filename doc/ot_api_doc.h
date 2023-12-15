c2d7255b5bc787a3d93a6807972043ec6c697a00
Jeff Rhodie <rhodie@google.com>
2023-12-15 01:44:04 +0000

    config var

--- doc/ot_api_doc.h
+++ doc/ot_api_doc.h
@@ -203,15 +203,15 @@
  * @defgroup config-border-agent             Border Agent
  * @defgroup config-border-router            Border Router
  * @defgroup config-border-routing           Border Routing Manager
- * @defgroup config-channel-maanger          Channel Manager
+ * @defgroup config-channel-manager          Channel Manager
  * @defgroup config-channel-monitor          Channel Monitor
  * @defgroup config-child-supervision        Child Supervision
  * @defgroup config-coap         	     CoAP
  * @defgroup config-commissioner	     Commissioner
  * @defgroup config-crypto                   Crypto Backend Library
  * @defgroup config-dataset-updater          Dataset Updater
- * @defgroup config-dhcp-client              DHCP Client
- * @defgroup config-dhcp-server              DHCP Server
+ * @defgroup config-dhcpv6-client            DHCP Client
+ * @defgroup config-dhcpv6-server            DHCP Server
  * @defgroup config-diag                     DIAG Service
  *
  * @}
