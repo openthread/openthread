22f3ee9e53bd9d40a3d2939d0323e4485aefbfbe
Jeff Rhodie <rhodie@google.com>
2024-02-01 01:19:25 +0000

    coap commands

--- src/cli/cli_coap.cpp
+++ src/cli/cli_coap.cpp
@@ -348,15 +348,15 @@ exit:
  * Done
  * @endcode
  * @cparam coap delete @ca{address} @ca{uri-path} [@ca{type}]
- *   * `address`: IPv6 address of the CoAP server.
-     *  `uri-path`: URI path of the resource.
-     *  `type`:
-          *`con` for Confirmable or `non-con` for Non-confirmable (default).
-     *    * `non-con`: Non-confirmable (default)
-     *    * `block-`: Use this option, followed by the block-wise value,
-     *      if the response should be transferred block-wise. Valid
-     *      values are: `block-16`, `block-32`, `block-64`, `block-128`,
-     *      `block-256`, `block-512`, or `block-124`.
+ *     * `address`: IPv6 address of the CoAP server.
+       *  `uri-path`: URI path of the resource.
+       *  `type`:
+ *            *`con` for Confirmable or `non-con` for Non-confirmable (default).
+ *            * `non-con`: Non-confirmable (default)
+ *            * `block-`: Use this option, followed by the block-wise value,
+ *            * if the response should be transferred block-wise. Valid
+ *            * values are: `block-16`, `block-32`, `block-64`, `block-128`,
+ *            * `block-256`, `block-512`, or `block-124`.
  * @par
  * Gets information about the specified CoAP resource.
  */
