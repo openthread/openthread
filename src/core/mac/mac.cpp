c66d91bdd7268e81402c1064d0e0a87002c7d4f6
Abtin Keshavarzian <abtink@google.com>
2024-03-07 21:39:52 -0800

    [key-manager] update how key guard time is determined and applied (#9871)
    This commit makes changes/fixes to `KeyManager` regarding key switch
    guard time.
    
    Key Rotation Time updates:
    - When the Key Rotation Time changes (due to security policy updates),
      the key switch guard time (`mKeySwitchGuardTime`) is also adjusted.
      It's set to 93% of the Rotation Time (rounded down).
    - Immediately checks if the new rotation time indicates a rotation is
      due and keys are rotated.
    
    New variable `mKeySwitchGuardTimer`:
    - This is reset to the current guard time whenever the key sequence is
      updated.
    - It decrements hourly until reaching zero.
    - Key switch guard comparison is made with this value, aligning the
      implementation with the Thread specification.
    
    `SetCurrentKeySequence()` modification:
    - Now accepts a new input parameter that determines whether to apply
      or ignore the key switch guard when updating the key sequence.
    - During a key rotation check (when the rotation time has passed), the
      key switch guard is ignored and we always move to the next key
      sequence number.
    
    Other changes:
    - Variables handling guard and rotation time now use `uint16_t`
      instead of `uint32_t` to align with security policy definitions.
    - API and CLI command documentation for setting the "key switch guard
      time" emphasize that they are intended for testing purposes.

--- src/core/mac/mac.cpp
+++ src/core/mac/mac.cpp
@@ -1637,7 +1637,7 @@ Error Mac::ProcessReceiveSecurity(RxFrame &aFrame, const Address &aSrcAddr, Neig
 
         if (keySequence > keyManager.GetCurrentKeySequence())
         {
-            keyManager.SetCurrentKeySequence(keySequence);
+            keyManager.SetCurrentKeySequence(keySequence, KeyManager::kApplyKeySwitchGuard);
         }
     }
 
