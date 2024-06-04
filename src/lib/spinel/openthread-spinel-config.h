ea61703252bdba2690425f53028f864e876706e5
Li Cao <irvingcl@google.com>
2024-06-04 14:06:23 +0800

    [spinel] remove including openthread-core-config.h (#10322)
    `openthread-spinel-config.h` is included by many modules like
    `spinel_encoder`, `spinel_decoder`. To use these modules in external
    projects, we need to remove the including of
    `openthread-core-config.h`.

--- src/lib/spinel/openthread-spinel-config.h
+++ src/lib/spinel/openthread-spinel-config.h
@@ -34,8 +34,6 @@
 #ifndef OPENTHREAD_SPINEL_CONFIG_H_
 #define OPENTHREAD_SPINEL_CONFIG_H_
 
-#include "openthread-core-config.h"
-
 /**
  * @def OPENTHREAD_SPINEL_CONFIG_OPENTHREAD_MESSAGE_ENABLE
  *
