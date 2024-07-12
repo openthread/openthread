abc45bd7278b846de14930817a1ba0d202421b8d
Abtin Keshavarzian <abtink@google.com>
2024-07-12 15:27:07 -0700

    [mesh-forwarder] `RemoveMessagesForChild()` to use a predicate fn
    This commit updates `RemoveMessagesForChild()` to use a predicate
    function to determine which messages should be removed. This replaces
    the `Message::SubType` filtering and allows messages matching
    multiple sub-types to be removed together.

--- src/core/thread/mesh_forwarder_ftd.cpp
+++ src/core/thread/mesh_forwarder_ftd.cpp
@@ -266,49 +266,37 @@ exit:
     return error;
 }
 
-void MeshForwarder::RemoveMessages(Child &aChild, Message::SubType aSubType)
+void MeshForwarder::RemoveMessagesForChild(Child &aChild, MessageChecker &aMessageChecker)
 {
     for (Message &message : mSendQueue)
     {
-        if ((aSubType != Message::kSubTypeNone) && (aSubType != message.GetSubType()))
+        if (!aMessageChecker(message))
         {
             continue;
         }
 
         if (mIndirectSender.RemoveMessageFromSleepyChild(message, aChild) != kErrorNone)
         {
-            switch (message.GetType())
-            {
-            case Message::kTypeIp6:
+            const Neighbor *neighbor = nullptr;
+
+            if (message.GetType() == Message::kTypeIp6)
             {
                 Ip6::Header ip6header;
 
                 IgnoreError(message.Read(0, ip6header));
-
-                if (&aChild == Get<NeighborTable>().FindNeighbor(ip6header.GetDestination()))
-                {
-                    message.ClearDirectTransmission();
-                }
-
-                break;
+                neighbor = Get<NeighborTable>().FindNeighbor(ip6header.GetDestination());
             }
-
-            case Message::kType6lowpan:
+            else if (message.GetType() == Message::kType6lowpan)
             {
                 Lowpan::MeshHeader meshHeader;
 
                 IgnoreError(meshHeader.ParseFrom(message));
-
-                if (&aChild == Get<NeighborTable>().FindNeighbor(meshHeader.GetDestination()))
-                {
-                    message.ClearDirectTransmission();
-                }
-
-                break;
+                neighbor = Get<NeighborTable>().FindNeighbor(meshHeader.GetDestination());
             }
 
-            default:
-                break;
+            if (&aChild == neighbor)
+            {
+                message.ClearDirectTransmission();
             }
         }
 
