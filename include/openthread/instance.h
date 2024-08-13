ddbc99b8219bed13f8ba9c9dbd1be6e2b4dcdce5
Zhanglong Xia <zhanglongxia@google.com>
2024-08-13 13:25:05 +0800

    [radio] update the description of `otPlatradioSetChannelTargetPower()` (#10585)
    This commit updates the descrition of the function
    `otPlatRadioSetChannelTargetPower()` to make the function descrition
    more accurate, and updates the implementation of the function based on
    the latest description.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (431)
+#define OPENTHREAD_API_VERSION (432)
 
 /**
  * @addtogroup api-instance
--- tests/unit/test_power_calibration.cpp
+++ tests/unit/test_power_calibration.cpp
@@ -67,8 +67,9 @@ void TestPowerCalibration(void)
 
     SuccessOrQuit(otPlatRadioSetChannelTargetPower(instance, 11, 4999));
     rawPowerSettingLength = sizeof(rawPowerSetting);
-    VerifyOrQuit(otPlatRadioGetRawPowerSetting(instance, 11, rawPowerSetting, &rawPowerSettingLength) ==
-                 OT_ERROR_NOT_FOUND);
+    SuccessOrQuit(otPlatRadioGetRawPowerSetting(instance, 11, rawPowerSetting, &rawPowerSettingLength));
+    VerifyOrQuit(rawPowerSettingLength == 1);
+    VerifyOrQuit(rawPowerSetting[0] == 0x00);
 
     SuccessOrQuit(otPlatRadioSetChannelTargetPower(instance, 11, 5000));
     rawPowerSettingLength = sizeof(rawPowerSetting);
