fdca218d20caca7cc6cd8b4aa0ca8f194db8d0fc
Esko Dijk <esko.dijk@iotconsultancy.nl>
2024-08-15 16:57:39 +0200

    [cli] clarify docs - parameter use for 'dns config' to change service mode only, and add missing CLI outputs.

--- src/cli/cli_dns.cpp
+++ src/cli/cli_dns.cpp
@@ -161,6 +161,9 @@ template <> otError Dns::Process<Cmd("config")>(Arg aArgs[])
      * ResponseTimeout: 5000 ms
      * MaxTxAttempts: 2
      * RecursionDesired: no
+     * ServiceMode: srv_txt_opt
+     * Nat64Mode: allow
+     * TransportProtocol: udp
      * Done
      * @endcode
      * @code
@@ -170,16 +173,20 @@ template <> otError Dns::Process<Cmd("config")>(Arg aArgs[])
      * @code
      * dns config
      * Server: [fd00:0:0:0:0:0:0:2]:53
-     * ResponseTimeout: 3000 ms
+     * ResponseTimeout: 6000 ms
      * MaxTxAttempts: 3
      * RecursionDesired: yes
+     * ServiceMode: srv_txt_opt
+     * Nat64Mode: allow
+     * TransportProtocol: udp
      * Done
      * @endcode
      * @par api_copy
      * #otDnsClientSetDefaultConfig
      * @cparam dns config [@ca{dns-server-IP}] [@ca{dns-server-port}] <!--
      * -->                [@ca{response-timeout-ms}] [@ca{max-tx-attempts}] <!--
-     * -->                [@ca{recursion-desired-boolean}] [@ca{service-mode}]
+     * -->                [@ca{recursion-desired-boolean}] [@ca{service-mode}] <!--
+     * -->                [@ca{protocol}]
      * @par
      * We can leave some of the fields as unspecified (or use value zero). The
      * unspecified fields are replaced by the corresponding OT config option
