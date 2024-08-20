5aeb945cb16642015e586fbce4cd0acbce42d60c
Abtin Keshavarzian <abtink@google.com>
2024-08-20 13:49:40 -0700

    [mle] include Link Margin TLV in Child Update messages
    This commit adds code to include the Link Margin TLV in MLE Child
    Update Request or Response messages sent by a parent to a child. This
    allows the child to learn and update its "link quality out" to its
    parent.
    
    The change is designed to be backward compatible. Child devices
    running older firmware will simply disregard this additional TLV.
    When processing the message, the presence of the Link Margin TLV is
    checked (it is optional).

--- src/core/thread/mle.cpp
+++ src/core/thread/mle.cpp
@@ -3394,6 +3394,7 @@ void Mle::HandleChildUpdateRequest(RxInfo &aRxInfo)
     RxChallenge challenge;
     TlvList     requestedTlvList;
     TlvList     tlvList;
+    uint8_t     linkMarginOut;
 
     SuccessOrExit(error = Tlv::Find<SourceAddressTlv>(aRxInfo.mMessage, sourceAddress));
 
@@ -3435,6 +3436,17 @@ void Mle::HandleChildUpdateRequest(RxInfo &aRxInfo)
         }
 
         SuccessOrExit(error = HandleLeaderData(aRxInfo));
+
+        switch (Tlv::Find<LinkMarginTlv>(aRxInfo.mMessage, linkMarginOut))
+        {
+        case kErrorNone:
+            mParent.SetLinkQualityOut(LinkQualityForLinkMargin(linkMarginOut));
+            break;
+        case kErrorNotFound:
+            break;
+        default:
+            ExitNow(error = kErrorParse);
+        }
 
 #if OPENTHREAD_CONFIG_MAC_CSL_RECEIVER_ENABLE
         {
@@ -3494,6 +3506,7 @@ void Mle::HandleChildUpdateResponse(RxInfo &aRxInfo)
     uint32_t    mleFrameCounter;
     uint16_t    sourceAddress;
     uint32_t    timeout;
+    uint8_t     linkMarginOut;
 
     Log(kMessageReceive, kTypeChildUpdateResponseAsChild, aRxInfo.mMessageInfo.GetPeerAddr());
 
@@ -3614,6 +3627,17 @@ void Mle::HandleChildUpdateResponse(RxInfo &aRxInfo)
 
     default:
         OT_ASSERT(false);
+    }
+
+    switch (Tlv::Find<LinkMarginTlv>(aRxInfo.mMessage, linkMarginOut))
+    {
+    case kErrorNone:
+        mParent.SetLinkQualityOut(LinkQualityForLinkMargin(linkMarginOut));
+        break;
+    case kErrorNotFound:
+        break;
+    default:
+        ExitNow(error = kErrorParse);
     }
 
     aRxInfo.mClass = response.IsEmpty() ? RxInfo::kPeerMessage : RxInfo::kAuthoritativeMessage;
