2ae0dd2dc3c2093d62418a448356b325e162217f
Yakun Xu <xyk@google.com>
2024-09-06 16:59:42 +0800

    [radio] add tx timestamp
    This commit adds tx timestamp so that the pcap callback can get the time
    when a frame was transmitted.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (441)
+#define OPENTHREAD_API_VERSION (442)
 
 /**
  * @addtogroup api-instance
