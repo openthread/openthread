018d183c6fe7e437a742b7a15d464eb3860803c8
Song Guo <songguo@google.com>
2024-09-11 15:19:20 +0800

    Update API version

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -53,7 +53,7 @@ extern "C" {
  * @note This number versions both OpenThread platform and user APIs.
  *
  */
-#define OPENTHREAD_API_VERSION (440)
+#define OPENTHREAD_API_VERSION (441)
 
 /**
  * @addtogroup api-instance
