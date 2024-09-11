c80663d1028975297821c43a324228b84f58dad6
Abtin Keshavarzian <abtink@google.com>
2024-09-11 14:36:05 -0700

    [border-agent] invoke ephemeral key callback on session connection
    This commit updates the Border Agent to invoke the ephemeral key
    callback when a commissioner candidate successfully establishes a
    secure session using the ephemeral key. This can be identified by
    checking the state `otBorderAgentGetState()` and matching it with
    `OT_BORDER_AGENT_STATE_ACTIVE`. This new signal can be used to stop
    advertising the mDNS service "_meshcop-e._udp" earlier (since the
    ephemeral key is one-time use).

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (459)
+#define OPENTHREAD_API_VERSION (460)
 
 /**
  * @addtogroup api-instance
