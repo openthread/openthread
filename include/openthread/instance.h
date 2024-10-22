b644cd97ee529503777dc4d8d87f9a804484c31a
Abtin Keshavarzian <abtink@google.com>
2024-10-22 02:51:45 -0700

    [udp] track netif ID in socket and simplify handling of backbone sockets
    This commit updates `otUdpSocket` to track the associated network
    interface identifier to which the socket is bound. This simplifies
    the code for tracking backbone sockets, particularly how
    `Udp::HandlePayload()` finds a matching socket.
    
    Previously, backbone sockets were placed at the end of the `mSockets`
    linked-list. The code tracked and updated a pointer to the entry
    before the first backbone socket in the list. This commit removes the
    need for this additional tracking and simplifies the implementation.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (456)
+#define OPENTHREAD_API_VERSION (457)
 
 /**
  * @addtogroup api-instance
