b92bd46b602da2700d206cbb76a8009e94b7d41c
Yakun Xu <xyk@google.com>
2024-12-11 13:58:47 +0800

    [api] correct typo of CallbackContext  (#11034)

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (466)
+#define OPENTHREAD_API_VERSION (467)
 
 /**
  * @addtogroup api-instance
