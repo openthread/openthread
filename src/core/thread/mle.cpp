0e1c9a79a3a5df8c31e675d66757cc5444fdf5b6
Abtin Keshavarzian <abtink@google.com>
2025-01-15 11:05:15 -0800

    [style] use `constexpr` instead of anonymous `enum` for constants (#11161)
    This commit updates a few remaining places where anonymous enums were
    used to declare constants, replacing them with `static constexpr`.

--- src/core/thread/mle.cpp
+++ src/core/thread/mle.cpp
@@ -4094,10 +4094,7 @@ void Mle::Log(MessageAction aAction, MessageType aType, const Ip6::Address &aAdd
 
 void Mle::Log(MessageAction aAction, MessageType aType, const Ip6::Address &aAddress, uint16_t aRloc)
 {
-    enum : uint8_t
-    {
-        kRlocStringSize = 17,
-    };
+    static constexpr uint16_t kRlocStringSize = 17;
 
     String<kRlocStringSize> rlocString;
 
