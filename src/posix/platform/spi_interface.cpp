openthread/openthread

commit abadeadfbbd2e71ec31bc957f2aa68c64b52ca72

[posix] introduce `OT_EXIT_RCP_RESET_REQUIRED` for platform reset requests (#11292)
This commit modifies the process exit behavior to introduce a new exit code
`OT_EXIT_RCP_RESET_REQUIRED`. This code signals that a hardware reset is required
by the POSIX platform, rather than being initiated directly by OpenThread itself.

This change is necessary to accommodate scenarios where the platform or a
separate stack (e.g., Wifi/BT) is responsible for managing hardware resets
of integrated chips (like Wifi/BT/Thread trinity chips). Previously, OpenThread
might have attempted to handle resets directly, which is not always appropriate
in these integrated environments.

This commit includes the following changes:
- Adds a new exit code: `OT_EXIT_RCP_RESET_REQUIRED` is now defined to indicate a
  platform-requested hardware reset.
- Makes reset parameters optional: The `gpio-reset-device` and `gpio-reset-line`
  parameters for reset functionality are now optional.
- process dies if gpio-reset-device is not given when TriggerReset().

src/posix/platform/spi_interface.cpp
@@ -127,13 +127,7 @@ otError SpiInterface::Init(ReceiveFrameCallback aCallback, void *aCallbackContex
 
     spiGpioIntDevice   = mRadioUrl.GetValue("gpio-int-device");
     spiGpioResetDevice = mRadioUrl.GetValue("gpio-reset-device");
-    if (!spiGpioIntDevice || !spiGpioResetDevice)
-    {
-        DieNow(OT_EXIT_INVALID_ARGUMENTS);
-    }
 
-    SuccessOrDie(mRadioUrl.ParseUint8("gpio-int-line", spiGpioIntLine));
-    SuccessOrDie(mRadioUrl.ParseUint8("gpio-reset-line", spiGpioResetLine));
     VerifyOrDie(mRadioUrl.ParseUint8("spi-mode", spiMode) != OT_ERROR_INVALID_ARGS, OT_EXIT_INVALID_ARGUMENTS);
     VerifyOrDie(mRadioUrl.ParseUint32("spi-speed", spiSpeed) != OT_ERROR_INVALID_ARGS, OT_EXIT_INVALID_ARGUMENTS);
     VerifyOrDie(mRadioUrl.ParseUint32("spi-reset-delay", spiResetDelay) != OT_ERROR_INVALID_ARGS,
@@ -151,7 +145,17 @@ otError SpiInterface::Init(ReceiveFrameCallback aCallback, void *aCallbackContex
     mSpiAlignAllowance  = spiAlignAllowance;
 
     InitIntPin(spiGpioIntDevice, spiGpioIntLine);
-    InitResetPin(spiGpioResetDevice, spiGpioResetLine);
+
+    if (spiGpioResetDevice)
+    {
+        SuccessOrDie(mRadioUrl.ParseUint8("gpio-reset-line", spiGpioResetLine));
+        InitResetPin(spiGpioResetDevice, spiGpioResetLine);
+    }
+    else
+    {
+        LogNote("gpio-reset-device is not given.");
+    }
+
     InitSpiDev(mRadioUrl.GetPath(), spiMode, spiSpeed);
 
     mReceiveFrameCallback = aCallback;
@@ -301,6 +305,8 @@ exit:
 
 void SpiInterface::TriggerReset(void)
 {
+    VerifyOrDie(mResetGpioValueFd >= 0, OT_EXIT_RCP_RESET_REQUIRED);
+
     // Set Reset pin to low level.
     SetGpioValue(mResetGpioValueFd, 0);
 

src/lib/platform/exit_code.c
@@ -75,6 +75,14 @@ const char *otExitCodeToString(uint8_t aExitCode)
 #endif
         break;
 
+    case OT_EXIT_INVALID_STATE:
+        retval = "InvalidState";
+        break;
+
+    case OT_EXIT_RCP_RESET_REQUIRED:
+        retval = "RcpResetRequired";
+        break;
+
     default:
         assert(false);
         retval = "UnknownExitCode";

src/lib/platform/exit_code.h
@@ -88,6 +88,10 @@ enum
      */
     OT_EXIT_INVALID_STATE = 7,
 
+    /**
+     * RCP chip reset is not able to be done by OT
+     */
+    OT_EXIT_RCP_RESET_REQUIRED = 8,
 };
 
 /**

src/posix/platform/radio_url.cpp
@@ -43,28 +43,30 @@ const char *otSysGetRadioUrlHelpString(void)
     "\n"
 
 #if OPENTHREAD_POSIX_CONFIG_SPINEL_SPI_INTERFACE_ENABLE
-#define OT_SPINEL_SPI_RADIO_URL_HELP_BUS                                                                       \
-    "Protocol=[spinel+spi*]           Specify the Spinel interface as the Spinel SPI interface\n"              \
-    "    spinel+spi://${PATH_TO_SPI_DEVICE}?${Parameters}\n"                                                   \
-    "Parameters:\n"                                                                                            \
-    "    gpio-int-device[=gpio-device-path]\n"                                                                 \
-    "                                  Specify a path to the Linux sysfs-exported GPIO device for the\n"       \
-    "                                  `I̅N̅T̅` pin.\n"                                                     \
-    "    gpio-int-line[=line-offset]\n"                                                                        \
-    "                                  The offset index of `I̅N̅T̅` pin for the associated GPIO device.\n"  \
-    "    gpio-reset-dev[=gpio-device-path]\n"                                                                  \
-    "                                  Specify a path to the Linux sysfs-exported GPIO device for the\n"       \
-    "                                  `R̅E̅S̅` pin.\n"                                                     \
-    "    gpio-reset-line[=line-offset]"                                                                        \
-    "                                  The offset index of `R̅E̅S̅` pin for the associated GPIO device.\n"  \
-    "    spi-mode[=mode]               Specify the SPI mode to use (0-3).\n"                                   \
-    "    spi-speed[=hertz]             Specify the SPI speed in hertz.\n"                                      \
-    "    spi-cs-delay[=usec]           Specify the delay after C̅S̅ assertion, in µsec.\n"                  \
-    "    spi-reset-delay[=ms]          Specify the delay after R̅E̅S̅E̅T̅ assertion, in milliseconds.\n"  \
-    "    spi-align-allowance[=n]       Specify the maximum number of 0xFF bytes to clip from start of\n"       \
-    "                                  MISO frame. Max value is 16.\n"                                         \
-    "    spi-small-packet=[n]          Specify the smallest packet we can receive in a single transaction.\n"  \
-    "                                  (larger packets will require two transactions). Default value is 32.\n" \
+#define OT_SPINEL_SPI_RADIO_URL_HELP_BUS                                                                           \
+    "Protocol=[spinel+spi*]           Specify the Spinel interface as the Spinel SPI interface\n"                  \
+    "    spinel+spi://${PATH_TO_SPI_DEVICE}?${Parameters}\n"                                                       \
+    "Parameters:\n"                                                                                                \
+    "    gpio-int-device[=gpio-device-path]\n"                                                                     \
+    "                                  Specify a path to the Linux sysfs-exported GPIO device for the\n"           \
+    "                                  `I̅N̅T̅` pin.\n"                                                         \
+    "    gpio-int-line[=line-offset]\n"                                                                            \
+    "                                  The offset index of `I̅N̅T̅` pin for the associated GPIO device.\n"      \
+    "    gpio-reset-device[=gpio-device-path]\n"                                                                   \
+    "                                  Specify a path to the Linux sysfs-exported GPIO device for the\n"           \
+    "                                  `R̅E̅S̅` pin. If not specified, the process will direct exist when an\n" \
+    "                                  RCP reset is required.\n"                                                   \
+    "    gpio-reset-line[=line-offset]\n"                                                                          \
+    "                                  The offset index of `R̅E̅S̅` pin for the associated GPIO device. This\n" \
+    "                                  is required if `gpio-reset-device` is specified.\n"                         \
+    "    spi-mode[=mode]               Specify the SPI mode to use (0-3).\n"                                       \
+    "    spi-speed[=hertz]             Specify the SPI speed in hertz.\n"                                          \
+    "    spi-cs-delay[=usec]           Specify the delay after C̅S̅ assertion, in µsec.\n"                      \
+    "    spi-reset-delay[=ms]          Specify the delay after R̅E̅S̅E̅T̅ assertion, in milliseconds.\n"      \
+    "    spi-align-allowance[=n]       Specify the maximum number of 0xFF bytes to clip from start of\n"           \
+    "                                  MISO frame. Max value is 16.\n"                                             \
+    "    spi-small-packet=[n]          Specify the smallest packet we can receive in a single transaction.\n"      \
+    "                                  (larger packets will require two transactions). Default value is 32.\n"     \
     "\n"
 #else
 #define OT_SPINEL_SPI_RADIO_URL_HELP_BUS

commit bfeac7616d57005fb90f5eb68778557729661bf8

[posix] fix GPIO interrupt pin error when SPI init
the error was introduced in https://github.com/openthread/openthread/pull/11292

src/posix/platform/spi_interface.cpp
@@ -128,6 +128,8 @@ otError SpiInterface::Init(ReceiveFrameCallback aCallback, void *aCallbackContex
     spiGpioIntDevice   = mRadioUrl.GetValue("gpio-int-device");
     spiGpioResetDevice = mRadioUrl.GetValue("gpio-reset-device");
 
+    VerifyOrDie(spiGpioIntDevice, OT_EXIT_INVALID_ARGUMENTS);
+    SuccessOrDie(mRadioUrl.ParseUint8("gpio-int-line", spiGpioIntLine));
     VerifyOrDie(mRadioUrl.ParseUint8("spi-mode", spiMode) != OT_ERROR_INVALID_ARGS, OT_EXIT_INVALID_ARGUMENTS);
     VerifyOrDie(mRadioUrl.ParseUint32("spi-speed", spiSpeed) != OT_ERROR_INVALID_ARGS, OT_EXIT_INVALID_ARGUMENTS);
     VerifyOrDie(mRadioUrl.ParseUint32("spi-reset-delay", spiResetDelay) != OT_ERROR_INVALID_ARGS,