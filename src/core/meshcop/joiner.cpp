openthread/openthread

commit f19548b4b19c6919bc24c90d603babe5d6dadbbc

[joiner] check for non-zero joiner UDP port before electing a joiner router (#9445)
An invalid Discovery Response that carries no Joiner UDP Port TLV will
be accepted by the joiner, but we shouldn't qualify it as a valid
joiner router since we can only open a connection with a non-zero
port.

src/core/meshcop/joiner.cpp
@@ -261,7 +261,7 @@ void Joiner::HandleDiscoverResult(Mle::DiscoverScanner::ScanResult *aResult)
 {
     VerifyOrExit(mState == kStateDiscover);
 
-    if (aResult != nullptr)
+    if (aResult != nullptr && aResult->mJoinerUdpPort > 0)
     {
         SaveDiscoveredJoinerRouter(*aResult);
     }

commit 648f29a78f91e8aa68434c3ccd74b811ad56b40c

[joiner] keep discovering when UDP port is bad
This commit fixes the issue that the joiner starts joining process
immediately when an invalid Joiner UDP Port is encountered. This issue
was introduced by #9445 which attempts to ignore joiner routers having
an invalid Joiner UDP Port.

src/core/meshcop/joiner.cpp
@@ -249,7 +249,7 @@ void Joiner::HandleDiscoverResult(Mle::DiscoverScanner::ScanResult *aResult)
 {
     VerifyOrExit(mState == kStateDiscover);
 
-    if (aResult != nullptr && aResult->mJoinerUdpPort > 0)
+    if (aResult != nullptr)
     {
         SaveDiscoveredJoinerRouter(*aResult);
     }
@@ -270,8 +270,10 @@ void Joiner::SaveDiscoveredJoinerRouter(const Mle::DiscoverScanner::ScanResult &
 {
     uint8_t       priority;
     bool          doesAllowAny;
-    JoinerRouter *end = GetArrayEnd(mJoinerRouters);
+    JoinerRouter *end;
     JoinerRouter *entry;
+
+    VerifyOrExit(aResult.mJoinerUdpPort > 0);
 
     doesAllowAny = AsCoreType(&aResult.mSteeringData).PermitsAllJoiners();
 
@@ -283,6 +285,8 @@ void Joiner::SaveDiscoveredJoinerRouter(const Mle::DiscoverScanner::ScanResult &
 
     // We keep the list sorted based on priority. Find the place to
     // add the new result.
+
+    end = GetArrayEnd(mJoinerRouters);
 
     for (entry = &mJoinerRouters[0]; entry < end; entry++)
     {