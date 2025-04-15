51e88850b9a2cfa2158c60edf139befd0979c1f1
Abtin Keshavarzian <abtink@google.com>
2025-04-15 11:34:22 -0700

    [dns-client] use `DecompressRecordData()` for broader record type support
    This commit updates the DNS client to use `DecompressRecordData()`
    helper method when processing `QueryRecord()` responses for
    arbitrary record types.
    
    This enables decompression of embedded DNS names within the received
    record data for a wider range of record types. In particular, name
    decompression is now supported for PTR, CNAME, DNAME, NS, SRV, SOA,
    MX, RP, AFSDB, RT, PX, KX, and NSEC records.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (501)
+#define OPENTHREAD_API_VERSION (502)
 
 /**
  * @addtogroup api-instance
