c0cffb2b8cca1a5c73ed784d9b4f0e9100529834
Yang Song <yangsongcn@google.com>
2025-05-08 01:21:43 +0000

    Signal an error immediately when no upstream DNS server available

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (505)
+#define OPENTHREAD_API_VERSION (506)
 
 /**
  * @addtogroup api-instance
