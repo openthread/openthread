d28ced5a9a67b550524236a9b2956bd145040b98
Esko Dijk <esko.dijk@iotconsultancy.nl>
2025-05-14 10:12:33 +0200

    [ble][tcat] add BLE 'non-advertising' state, TCAT active/standby states, and timed activation (#11802)
    This adds a BLE 'non-advertising' state in which BLE is enabled for connections, but not advertising.
    - If the TCAT agent is not started and BLE is started, BLE advertising will be enabled by default.
    - If the TCAT agent is started, then BLE advertising is under control of the TCAT agent - depending on its state.
    
    The TCAT agent, while started, can be now in standby mode or in active mode. In standby, it doesn't
    advertise and doesn't allow incoming TCAT connections. In active mode, it allows both advertising and incoming
    connections. Timed activation of the active mode is also possible. CLI commands are added to test the new modes and
    timed activation.
    
    Existing architecture is not changed (BLE-Secure is in control and has all APIs to access TCAT agent).
    It also fixes some (API) documentation and clarifies this where needed. Also, an error-value bugfix is done.
    
    The TcatAgent is now added to instance.hpp/cpp, because that is required to use a timer from within
    TcatAgent.
    
    Log messages of TcatAgent are shortened and more often try to use an identical constant string value.
    This saves memory in the binary, due to string re-use.
    
    The "join callback" is clarified and extended to support callback to the application when the TCAT Commissioner
    instructs the TCAT Device to try join a Thread network, or leave a Thread network.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (538)
+#define OPENTHREAD_API_VERSION (539)
 
 /**
  * @addtogroup api-instance
