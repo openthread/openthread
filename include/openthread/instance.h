f116849f2e93a981e0fd74762e1e388183269ebf
Jeff Rhodie <rhodie@google.com>
2025-05-15 01:07:42 +0000

    minor syntax update in command desc

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (507)
+#define OPENTHREAD_API_VERSION (508)
 
 /**
  * @addtogroup api-instance
