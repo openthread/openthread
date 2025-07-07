567c00217b056b714b1792dab60ae012c5c7a061
Abtin Keshavarzian <abtink@google.com>
2025-07-07 13:08:51 -0700

    [routing-manager] new API to get infrastructure interface info (#11667)
    This commit introduces `otBorderRoutingGetInfraIfInfo()` to get the
    interface index and running state of the configured infrastructure
    interface. A corresponding CLI command is also added to retrieve this
    information.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (515)
+#define OPENTHREAD_API_VERSION (516)
 
 /**
  * @addtogroup api-instance
