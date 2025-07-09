d2d644d470193ad37871431fce38666ef44bcce1
Abtin Keshavarzian <abtink@google.com>
2025-07-09 13:16:37 -0700

    [mdns] change `SetEnabled()` to be idempotent (#11690)
    This commit changes the `SetEnabled()` API to return `kErrorNone` when
    the component is already in the requested state (enabled or
    disabled). Previously, the method would return `kErrorAlready` in
    this scenario. Making this API idempotent simplifies caller logic, as
    they no longer need to handle the `kErrorAlready` case.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (518)
+#define OPENTHREAD_API_VERSION (519)
 
 /**
  * @addtogroup api-instance
