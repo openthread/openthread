12a741c7642bdc1e482deeeafe432c998357eeec
Abtin Keshavarzian <abtink@google.com>
2025-07-23 15:18:36 -0700

    [history-tracker] add client/server for remote query
    This commit introduces a client/server mechanism to the History
    Tracker module. This allows a device to query history information
    from another device over the Thread network using TMF messages.
    
    The new functionality is composed of three main parts:
    
    - Server (`HistoryTracker::Server`): This component is responsible for
      handling incoming TMF query requests (`h/qy`). It collects the
      requested local history entries (e.g., Network Info), formats them
      into TLVs, and sends them back to the requester in one or more TMF
      answer messages (`h/an`). It can fragment large responses into
      multiple messages.
    
    - Client (`HistoryTracker::Client`): This provides a new public API
      (`otHistoryTrackerQueryNetInfo`) to send a query to a remote
      device. It handles sending the request and processing the received
      answer(s), passing the retrieved history entries to the user via a
      callback. A function to cancel an ongoing query
      (`otHistoryTrackerCancelQuery`) is also added.
    
    - TLVs (`history_tracker_tlvs`): New TLVs are defined for the
      query/answer protocol, including `RequestTlv` to specify the query
      parameters, `AnswerTlv` to manage multi-message responses,
      `NetworkInfoTlv` to carry the data, and `QueryIdTlv` to correlate
      requests and responses.
    
    A new CLI command, `history query netinfo`, is added to use the new
    client API. The existing `history netinfo` output logic is refactored
    into helper methods to be shared by both the local and remote history
    commands.
    
    The new feature can be enabled/disabled using two new configuration
    flags:
    - `OPENTHREAD_CONFIG_HISTORY_TRACKER_SERVER_ENABLE`
    - `OPENTHREAD_CONFIG_HISTORY_TRACKER_CLIENT_ENABLE`

--- src/core/instance/instance.hpp
+++ src/core/instance/instance.hpp
@@ -145,6 +145,8 @@
 #include "utils/channel_monitor.hpp"
 #include "utils/heap.hpp"
 #include "utils/history_tracker.hpp"
+#include "utils/history_tracker_client.hpp"
+#include "utils/history_tracker_server.hpp"
 #include "utils/jam_detector.hpp"
 #include "utils/link_metrics_manager.hpp"
 #include "utils/mesh_diag.hpp"
@@ -704,6 +706,12 @@ private:
 
 #if OPENTHREAD_CONFIG_HISTORY_TRACKER_ENABLE
     HistoryTracker::Local mHistoryTrackerLocal;
+#if OPENTHREAD_CONFIG_HISTORY_TRACKER_SERVER_ENABLE
+    HistoryTracker::Server mHistoryTrackerServer;
+#endif
+#if OPENTHREAD_CONFIG_HISTORY_TRACKER_CLIENT_ENABLE
+    HistoryTracker::Client mHistoryTrackerClient;
+#endif
 #endif
 
 #if OPENTHREAD_CONFIG_LINK_METRICS_MANAGER_ENABLE
@@ -1025,7 +1033,16 @@ template <> inline Utils::MeshDiag &Instance::Get(void) { return mMeshDiag; }
 #endif
 
 #if OPENTHREAD_CONFIG_HISTORY_TRACKER_ENABLE
+
 template <> inline HistoryTracker::Local &Instance::Get(void) { return mHistoryTrackerLocal; }
+
+#if OPENTHREAD_CONFIG_HISTORY_TRACKER_SERVER_ENABLE
+template <> inline HistoryTracker::Server &Instance::Get(void) { return mHistoryTrackerServer; }
+#endif
+
+#if OPENTHREAD_CONFIG_HISTORY_TRACKER_CLIENT_ENABLE
+template <> inline HistoryTracker::Client &Instance::Get(void) { return mHistoryTrackerClient; }
+#endif
 #endif
 
 #if OPENTHREAD_CONFIG_LINK_METRICS_MANAGER_ENABLE
