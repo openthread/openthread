6f0993065ef422030c3e89c1b407185a65d9751c
Abtin Keshavarzian <abtink@google.com>
2025-10-10 12:59:33 -0700

    [border-agent] introduce `TxtData` class (#12005)
    This commit introduces a new `MeshCoP::BorderAgent::TxtData` class to
    encapsulate the logic for preparing the Border Agent's MeshCoP
    service TXT data.
    
    The TXT data generation logic is moved from the `BorderAgent::Manager`
    into the new `TxtData` class. This refactoring improves modularity
    and maintainability and prepares for the future addition of a MeshCoP
    TXT data parser.

--- src/core/instance/instance.hpp
+++ src/core/instance/instance.hpp
@@ -87,6 +87,7 @@
 #include "mac/wakeup_tx_scheduler.hpp"
 #include "meshcop/border_agent.hpp"
 #include "meshcop/border_agent_tracker.hpp"
+#include "meshcop/border_agent_txt_data.hpp"
 #include "meshcop/commissioner.hpp"
 #include "meshcop/dataset_manager.hpp"
 #include "meshcop/dataset_updater.hpp"
@@ -590,6 +591,7 @@ private:
 
 #if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE
     MeshCoP::BorderAgent::Manager mBorderAgentManager;
+    MeshCoP::BorderAgent::TxtData mBorderAgentTxtData;
 #endif
 
 #if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE && OPENTHREAD_CONFIG_BORDER_AGENT_EPHEMERAL_KEY_ENABLE
@@ -1034,6 +1036,7 @@ template <> inline MeshCoP::DatasetUpdater &Instance::Get(void) { return mDatase
 
 #if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE
 template <> inline MeshCoP::BorderAgent::Manager &Instance::Get(void) { return mBorderAgentManager; }
+template <> inline MeshCoP::BorderAgent::TxtData &Instance::Get(void) { return mBorderAgentTxtData; }
 #endif
 
 #if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE && OPENTHREAD_CONFIG_BORDER_AGENT_EPHEMERAL_KEY_ENABLE
