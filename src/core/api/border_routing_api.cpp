5ce409830b8314bf48879d6075f330280e208ebb
Yang Song <yangsongcn@google.com>
2025-11-20 00:12:25 +0800

    [cli] add new cli br nat64prefixtable command (#12090)
    This commit introduces a new API
    `otBorderRoutingGetNextNat64PrefixEntry`, which allows iterating
    through the table of RA-discovered NAT64 prefixes on the
    infrastructure link.
    
    The `br nat64prefixtable command` is added to the CLI to display the
    contents of the RA-discovered NAT64 prefix table.

--- src/core/api/border_routing_api.cpp
+++ src/core/api/border_routing_api.cpp
@@ -238,6 +238,18 @@ otError otBorderRoutingGetNextRouterEntry(otInstance                         *aI
     return AsCoreType(aInstance).Get<BorderRouter::RxRaTracker>().GetNextRouterEntry(*aIterator, *aEntry);
 }
 
+#if OPENTHREAD_CONFIG_NAT64_BORDER_ROUTING_ENABLE
+otError otBorderRoutingGetNextNat64PrefixEntry(otInstance                         *aInstance,
+                                               otBorderRoutingPrefixTableIterator *aIterator,
+                                               otBorderRoutingNat64PrefixEntry    *aEntry)
+{
+    AssertPointerIsNotNull(aIterator);
+    AssertPointerIsNotNull(aEntry);
+
+    return AsCoreType(aInstance).Get<BorderRouter::RxRaTracker>().GetNextNat64PrefixEntry(*aIterator, *aEntry);
+}
+#endif
+
 otError otBorderRoutingGetNextRdnssAddrEntry(otInstance                         *aInstance,
                                              otBorderRoutingPrefixTableIterator *aIterator,
                                              otBorderRoutingRdnssAddrEntry      *aEntry)
