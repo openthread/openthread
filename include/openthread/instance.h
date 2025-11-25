ee87c769c90ec7b5b855fc84e417b7e3d44b3a26
Abtin Keshavarzian <abtink@google.com>
2025-11-25 12:50:27 -0800

    [mdns] add validation for names in browser and resolver APIs
    This commit introduces validation for names provided to mDNS browser,
    resolver, and querier start and stop functions.
    
    New `ValidateNamesIn()` overloads are added to check the validity of
    service types, instance names, and host names passed to APIs like
    `otMdnsStartBrowser()`, `otMdnsStartSrvResolver()`, etc.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (553)
+#define OPENTHREAD_API_VERSION (554)
 
 /**
  * @addtogroup api-instance
