85fd01fd906b51ecb6ad857fcb347c2f4c8aef47
Abtin Keshavarzian <abtink@google.com>
2025-12-16 13:01:48 -0800

    [border-agent] move border agent ephemeral key APIs to a separate header
    This change moves all public APIs related to the Border Agent's
    ephemeral key feature out of `border_agent.h` and into a new,
    dedicated header file `border_agent_ephemeral_key.h`.
    
    The corresponding C API implementations are also moved from
    `border_agent_api.cpp` into a new dedicated
    `border_agent_ephemeral_key_api.cpp` file.
    
    This improves the organization and modularity of the public API,
    making the codebase easier to navigate and understand.

--- include/openthread/instance.h
+++ include/openthread/instance.h
@@ -52,7 +52,7 @@ extern "C" {
  *
  * @note This number versions both OpenThread platform and user APIs.
  */
-#define OPENTHREAD_API_VERSION (561)
+#define OPENTHREAD_API_VERSION (562)
 
 /**
  * @addtogroup api-instance
