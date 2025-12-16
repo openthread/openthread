9e22ff87e8ef3bf0403a7796d4c60083669d5c29
Abtin Keshavarzian <abtink@google.com>
2025-12-16 10:54:34 -0800

    [cli] move border agent commands to a separate module
    This change moves the CLI implementation for the Border Agent from
    the main `cli.cpp` into a new dedicated `Ba` class within
    `cli_ba.cpp` and `cli_ba.hpp`.
    
    The new `Ba` module is integrated into the main `Interpreter` class,
    following the same pattern used by other CLI modules such as `Bbr`
    and `Br`. This improves code organization and modularity, making the
    CLI codebase easier to navigate and maintain.
    
    The functionality of the `ba` commands remains unchanged.

---
+++ src/cli/cli_ba.hpp
@@ -0,0 +1,96 @@
+/*
+ *  Copyright (c) 2025, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ *   This file contains definitions for CLI to Border Agent.
+ */
+
+#ifndef CLI_BA_HPP_
+#define CLI_BA_HPP_
+
+#include "openthread-core-config.h"
+
+#include <openthread/border_agent.h>
+
+#include "cli/cli_config.h"
+#include "cli/cli_utils.hpp"
+
+#if OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE
+
+namespace ot {
+namespace Cli {
+
+/**
+ * Implements the Border Agent CLI interpreter.
+ */
+class Ba : private Utils
+{
+public:
+    /**
+     * Constructor
+     *
+     * @param[in]  aInstance            The OpenThread Instance.
+     * @param[in]  aOutputImplementer   An `OutputImplementer`.
+     */
+    Ba(otInstance *aInstance, OutputImplementer &aOutputImplementer)
+        : Utils(aInstance, aOutputImplementer)
+    {
+    }
+
+    /**
+     * Processes a CLI sub-command.
+     *
+     * @param[in]  aArgs     An array of command line arguments.
+     *
+     * @retval OT_ERROR_NONE              Successfully executed the CLI command.
+     * @retval OT_ERROR_PENDING           The CLI command was successfully started but final result is pending.
+     * @retval OT_ERROR_INVALID_COMMAND   Invalid or unknown CLI command.
+     * @retval OT_ERROR_INVALID_ARGS      Invalid arguments.
+     * @retval ...                        Error during execution of the CLI command.
+     */
+    otError Process(Arg aArgs[]);
+
+private:
+    using Command = CommandEntry<Ba>;
+
+    template <CommandId kCommandId> otError Process(Arg aArgs[]);
+
+    void OutputBorderAgentCounters(const otBorderAgentCounters &aCounters);
+#if OPENTHREAD_CONFIG_BORDER_AGENT_EPHEMERAL_KEY_ENABLE
+    static void HandleBorderAgentEphemeralKeyStateChange(void *aContext);
+    void        HandleBorderAgentEphemeralKeyStateChange(void);
+#endif
+};
+
+} // namespace Cli
+} // namespace ot
+
+#endif // OPENTHREAD_CONFIG_BORDER_AGENT_ENABLE
+
+#endif // CLI_BA_HPP_
