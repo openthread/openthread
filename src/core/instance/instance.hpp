db506d6c15cc767a322c63f539272fa5842616b1
Abtin Keshavarzian <abtink@google.com>
2026-01-21 15:17:44 -0800

    [joiner] introduce `Seeker` to handle discovery and candidate tracking
    This change introduces a new `Seeker` class to encapsulate the logic
    for discovering and prioritizing Joiner Router candidates.
    
    The `Joiner` class is updated to use the `Seeker` to perform the
    discovery scan. The `Joiner` provides a callback `EvaluateScanResult`
    to the `Seeker` to filter and evaluate scan results based on
    Steering Data, preserving the existing behavior.
    
    This change separates the discovery mechanism from the `Joiner` state
    machine. This simplifies the `Joiner` implementation and facilitates
    future enhancements to the joining process.
    
    This commit makes to changes to how the Joiner Router candidates are
    tracked or prioritized.

--- src/core/instance/instance.hpp
+++ src/core/instance/instance.hpp
@@ -100,6 +100,7 @@
 #include "meshcop/joiner_router.hpp"
 #include "meshcop/meshcop_leader.hpp"
 #include "meshcop/network_name.hpp"
+#include "meshcop/seeker.hpp"
 #include "net/dhcp6_client.hpp"
 #include "net/dhcp6_server.hpp"
 #include "net/dhcp6_types.hpp"
@@ -617,6 +618,7 @@ private:
 #endif
 
 #if OPENTHREAD_CONFIG_JOINER_ENABLE
+    MeshCoP::Seeker mSeeker;
     MeshCoP::Joiner mJoiner;
 #endif
 
@@ -959,6 +961,8 @@ template <> inline Dnssd &Instance::Get(void) { return mDnssd; }
 #endif
 
 #if OPENTHREAD_CONFIG_JOINER_ENABLE
+template <> inline MeshCoP::Seeker &Instance::Get(void) { return mSeeker; }
+
 template <> inline MeshCoP::Joiner &Instance::Get(void) { return mJoiner; }
 #endif
 
