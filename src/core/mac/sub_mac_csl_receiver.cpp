319cd69d263cb166ccaf0c2544937983669e5e0f
Abtin Keshavarzian <abtink@google.com>
2026-07-31 11:35:03 -0700

    [radio] add `Radio::ConvertUncertaintyToUsec()` helper
    This commit introduces `Radio::kUncertaintyUnit` constant and
    `Radio::ConvertUncertaintyToUsec()` helper function to convert CSL
    uncertainty (in units of 10 microseconds) to microseconds.
    
    It updates `SubMac`, `Mle`, and `CslAccuracy` to use the helper,
    eliminating manual `* 10` calculations and redundant internal
    constants.

--- src/core/mac/sub_mac_csl_receiver.cpp
+++ src/core/mac/sub_mac_csl_receiver.cpp
@@ -261,7 +261,8 @@ void SubMac::GetCslWindowEdges(uint32_t &aAhead, uint32_t &aAfter)
     elapsed = curTime - mCslLastSync.GetValue();
 
     semiWindow = DetermineClockDrift(elapsed);
-    semiWindow += mCslParentAccuracy.GetUncertaintyInMicrosec() + Get<Radio::Radio>().GetCslUncertainty() * 10;
+    semiWindow +=
+        Radio::ConvertUncertaintyToUsec(mCslParentAccuracy.GetUncertainty() + Get<Radio::Radio>().GetCslUncertainty());
 
     aAhead = Min(semiPeriod, semiWindow + kMinReceiveOnAhead + kCslReceiveTimeAhead);
     aAfter = Min(semiPeriod, semiWindow + kMinReceiveOnAfter);
